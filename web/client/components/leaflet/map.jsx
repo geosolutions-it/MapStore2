b95b07ba0361ab8cd76ef8082e8b97a3e7ad365c
Lorenzo Natali <lorenzo.natali@geo-solutions.it>
2015-07-28 14:27:44 +0200

    fixed indentation eslint. close #18

--- web/client/components/leaflet/map.jsx
+++ web/client/components/leaflet/map.jsx
@@ -9,35 +9,35 @@ var L = require('leaflet');
 var React = require('react');
 
 var LeafletMap = React.createClass({
-  propTypes: {
-    id: React.PropTypes.string,
-    center: React.PropTypes.object,
-    zoom: React.PropTypes.number
-  },
-  getDefaultProps() {
-      return {
-          id: 'map'
-      };
+    propTypes: {
+        id: React.PropTypes.string,
+        center: React.PropTypes.object,
+        zoom: React.PropTypes.number
     },
-  getInitialState() {
-    return {
-      map: null
-    };
-  },
-  componentDidMount() {
-    var map = L.map(this.props.id).setView([this.props.center.lat, this.props.center.lng],
-      this.props.zoom);
-    L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
-          attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-      }).addTo(map);
-    this.map = map;
-  },
-  componentWillUnmount() {
-    this.map.remove();
-  },
-  render() {
-    return <div id={this.props.id}></div>;
-  }
+    getDefaultProps() {
+        return {
+          id: 'map'
+        };
+    },
+    getInitialState() {
+        return {
+            map: null
+        };
+    },
+    componentDidMount() {
+        var map = L.map(this.props.id).setView([this.props.center.lat, this.props.center.lng],
+          this.props.zoom);
+        L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
+              attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+          }).addTo(map);
+        this.map = map;
+    },
+    componentWillUnmount() {
+        this.map.remove();
+    },
+    render() {
+        return <div id={this.props.id}></div>;
+    }
 });
 
 module.exports = LeafletMap;
