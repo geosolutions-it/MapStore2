cb637e69ad2b854fdca27e353b136d5813ed0b52
Lorenzo Natali <lorenzo.natali@geo-solutions.it>
2015-07-29 12:03:14 +0200

    fixes to legacy conversion. renamed test file names

--- web/client/utils/ConfigUtils.js
+++ web/client/utils/ConfigUtils.js
@@ -7,10 +7,11 @@ var ConfigUtils = {
         var center = mapConfig.center;
         var zoom = mapConfig.zoom;
         var maxExtent = mapConfig.maxExtent;
-        var epsg900913 = new Proj4js.Proj('EPSG:900913');
+        var projection = mapConfig.projection;
+        var epsgMap = new Proj4js.Proj(projection);
         var epsg4326 = new Proj4js.Proj('EPSG:4326');
         var xy = new Proj4js.Point(center);
-        Proj4js.transform(epsg900913, epsg4326, xy);
+        Proj4js.transform(epsgMap, epsg4326, xy);
         const latLng = {lat: xy.y, lng: xy.x};
         return {
             latLng: latLng,
