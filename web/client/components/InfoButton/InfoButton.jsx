16f53a13284228de5bbec0623f2c4f575059287d
Mirco Bertelli <mircobe87@gmail.com>
2015-08-07 13:04:17 +0200

    new about/info/credit component
     - added Bootstrap and React-Bootstrap dependencies
     - implemented a new "about/info/credit" component;
     - updated the viewer to show an example of this new component

---
+++ web/client/components/InfoButton/InfoButton.jsx
@@ -0,0 +1,94 @@
+/**
+ * Copyright 2015, GeoSolutions Sas.
+ * All rights reserved.
+ *
+ * This source code is licensed under the BSD-style license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+var React = require('react');
+var BootstrapReact = require('react-bootstrap');
+var Modal = BootstrapReact.Modal;
+var Button = BootstrapReact.Button;
+var Glyphicon = BootstrapReact.Glyphicon;
+
+/**
+ * A button to show a simple information window.
+ * Component's properies:
+ *  - id: {string}            custom identifier for this component
+ *  - title: {string|element} title of the window shown in its header
+ *  - body: {string|element}  content of the window
+ *  - style: {object}         a css-like hash to define the style on the component
+ *  - glyphicon: {string}    bootstrap glyphicon name
+ *  - text: {string|element}  text content for the button
+ *  - btnSize: {string}       bootstrap button size ('large', 'small', 'xsmall')
+ *
+ * Note: the button will not be never empty, it will show at least the text (default or custom)
+ */
+var About = React.createClass({
+    propTypes: {
+        id: React.PropTypes.string,
+        title: React.PropTypes.oneOfType([React.PropTypes.string, React.PropTypes.element]),
+        body: React.PropTypes.oneOfType([React.PropTypes.string, React.PropTypes.element]),
+        style: React.PropTypes.object,
+        glyphicon: React.PropTypes.string,
+        text: React.PropTypes.oneOfType([React.PropTypes.string, React.PropTypes.element]),
+        hiddenText: React.PropTypes.bool,
+        btnSize: React.PropTypes.oneOf(['large', 'medium', 'small', 'xsmall'])
+    },
+    getDefaultProps() {
+        return {
+            id: undefined,
+            title: "Info",
+            body: "",
+            style: undefined,
+            glyphicon: undefined,
+            text: "Info",
+            hiddenText: false,
+            btnSize: 'medium'
+        };
+    },
+    getInitialState() {
+        return {
+            isVisible: false
+        };
+    },
+    render() {
+        return (
+            <div
+                id={this.props.id}
+                style={this.props.style}>
+                <Button
+                    bsStyle="info"
+                    bsSize={this.props.btnSize}
+                    onClick={this.open}>
+                    {this.props.glyphicon ? <Glyphicon glyph={this.props.glyphicon}/> : ""}
+                    {!this.props.hiddenText && this.props.glyphicon ? "\u00A0" : ""}
+                    {!(this.props.hiddenText && this.props.glyphicon) ? this.props.text : ""}
+                </Button>
+
+                <Modal
+                    show={this.state.isVisible}
+                    onHide={this.close}
+                    bsStyle="info">
+
+                    <Modal.Header closeButton>
+                        <Modal.Title>{this.props.title}</Modal.Title>
+                    </Modal.Header>
+                    <Modal.Body>{this.props.body}</Modal.Body>
+                </Modal>
+            </div>
+        );
+    },
+    close() {
+        this.setState({
+            isVisible: false
+        });
+    },
+    open() {
+        this.setState({
+            isVisible: true
+        });
+    }
+});
+
+module.exports = About;
--- web/client/examples/viewer/app.jsx
+++ web/client/examples/viewer/app.jsx
@@ -7,6 +7,8 @@
  */
 var React = require('react');
 var MapViewController = require('../../components/Map/MapViewController');
+var InfoButton = require('../../components/InfoButton/InfoButton');
+
 var ConfigUtils = require('../../utils/ConfigUtils');
 
 var Api = require('../../api/MapConfigDAO');
@@ -15,5 +17,48 @@ Api.get("../data/mapStoreConfig.json").then( function(legacyConfig) {
     const mapId = "map";
     // convert from legacy
     const conf = ConfigUtils.convertFromLegacy(legacyConfig);
-    React.render(<MapViewController id={mapId} config={conf}/>, document.body);
+    React.render(<MapViewController id={mapId} config={conf}/>, document.getElementById("mapContainer"));
 });
+
+React.render(<InfoButton
+    text="About"
+    title="About this app..."
+    glyphicon="info-sign"
+    body={
+        <div>
+            <h1>MapStore 2</h1>
+            <p>
+                MapStore 2 is a framework to build web mapping applications using
+                standard mapping libraries, such as <a href="http://openlayers.org/">OpenLayers 3</a> and <a href="http://leafletjs.com/">Leaflet</a>.
+            </p>
+            <p>MapStore 2 has several example applications:</p>
+            <ul>
+                <li>
+                    MapViewer is a simple viewer of preconfigured maps (optionally
+                    stored in a database using GeoStore)
+                </li>
+                <li>
+                    MapPublisher has been developed to create, save and share in a
+                    simple and intuitive way maps and mashups created selecting
+                    contents by server like OpenStreetMap, Google Maps, MapQuest or
+                    specific servers provided by your organization or third party.
+                    For more information check the <a href="https://github.com/geosolutions-it/MapStore2/wiki">MapStore wiki</a>.
+                </li>
+            </ul>
+            <h2>License</h2>
+            <p>
+                MapStore 2 is Free and Open Source software, it is based on
+                OpenLayers 3, Leaflet and ReactJS, and is licensed under the
+                Simplified BSD License.
+            </p>
+            <h2>Contributing</h2>
+            <p>We welcome contributions in any form:</p>
+            <ul>
+                <li>pull requests for new features</li>
+                <li>pull requests for bug fixes</li>
+                <li>pull requests for documentation</li>
+                <li>funding for any combination of the above</li>
+            </ul>
+            <p>For more information check <a href="https://github.com/geosolutions-it/MapStore2/blob/master/CONTRIBUTING.md">this</a> page.</p>
+        </div>
+    }/>, document.getElementById("aboutContainer"));
