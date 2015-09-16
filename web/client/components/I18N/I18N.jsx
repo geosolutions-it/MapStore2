1a382a882b95aa609af1317cfdfdc4669f51c481
Mirco Bertelli <mircobe87@gmail.com>
2015-09-16 16:29:56 +0200

    Added Intl polyfill

--- web/client/components/I18N/I18N.jsx
+++ web/client/components/I18N/I18N.jsx
@@ -5,5 +5,16 @@
  * This source code is licensed under the BSD-style license found in the
  * LICENSE file in the root directory of this source tree.
  */
-module.exports.Message = require('./Message');
-module.exports.HTML = require('./HTML');
+
+if (!global.Intl) {
+    require.ensure([
+        'intl'
+    ], function(require) {
+        require('intl');
+        module.exports.Message = require('./Message');
+        module.exports.HTML = require('./HTML');
+    });
+} else {
+    module.exports.Message = require('./Message');
+    module.exports.HTML = require('./HTML');
+}
