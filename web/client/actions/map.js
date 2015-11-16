82e4ef9136f92bb1894232e9452cc8dff0c39c70
Mauro Bartolomeoli <maurobartolomeoli@gmail.com>
2015-11-16 12:18:18 +0100

    Added graticule layer support for ol3

--- web/client/actions/map.js
+++ web/client/actions/map.js
@@ -10,6 +10,8 @@ const CHANGE_MAP_VIEW = 'CHANGE_MAP_VIEW';
 const CLICK_ON_MAP = 'CLICK_ON_MAP';
 const CHANGE_MOUSE_POINTER = 'CHANGE_MOUSE_POINTER';
 const CHANGE_ZOOM_LVL = 'CHANGE_ZOOM_LVL';
+const PAN_TO = 'PAN_TO';
+const ZOOM_TO_EXTENT = 'ZOOM_TO_EXTENT';
 const CHANGE_MAP_CRS = 'CHANGE_MAP_CRS';
 
 
@@ -54,15 +56,34 @@ function changeZoomLevel(zoomLvl, mapStateSource) {
     };
 }
 
+function panTo(center) {
+    return {
+        type: PAN_TO,
+        center
+    };
+}
+
+function zoomToExtent(extent, crs) {
+    return {
+        type: ZOOM_TO_EXTENT,
+        extent,
+        crs
+    };
+}
+
 module.exports = {
     CHANGE_MAP_VIEW,
     CLICK_ON_MAP,
     CHANGE_MOUSE_POINTER,
     CHANGE_ZOOM_LVL,
+    PAN_TO,
+    ZOOM_TO_EXTENT,
     CHANGE_MAP_CRS,
     changeMapView,
     clickOnMap,
     changeMousePointer,
     changeZoomLevel,
-    changeMapCrs
+    changeMapCrs,
+    zoomToExtent,
+    panTo
 };
--- web/client/reducers/map.js
+++ web/client/reducers/map.js
@@ -6,9 +6,12 @@
  * LICENSE file in the root directory of this source tree.
  */
 
-var {CHANGE_MAP_VIEW, CHANGE_MOUSE_POINTER, CHANGE_ZOOM_LVL, CHANGE_MAP_CRS} = require('../actions/map');
+var {CHANGE_MAP_VIEW, CHANGE_MOUSE_POINTER,
+    CHANGE_ZOOM_LVL, CHANGE_MAP_CRS, ZOOM_TO_EXTENT, PAN_TO} = require('../actions/map');
 
 var assign = require('object-assign');
+var MapUtils = require('../utils/MapUtils');
+var CoordinatesUtils = require('../utils/CoordinatesUtils');
 
 function mapConfig(state = null, action) {
     switch (action.type) {
@@ -33,6 +36,25 @@ function mapConfig(state = null, action) {
             return assign({}, state, {
                 projection: action.crs
             });
+        case ZOOM_TO_EXTENT: {
+            const zoom = MapUtils.getZoomForExtent(action.extent, state.size, 0, 21);
+            const center = CoordinatesUtils.reproject(
+                MapUtils.getCenterForExtent(action.extent, action.crs),
+                action.crs, 'EPSG:4326');
+            return assign({}, state, {
+                zoom,
+                center
+            });
+        }
+        case PAN_TO: {
+            const center = CoordinatesUtils.reproject(
+                action.center,
+                action.center.crs,
+                'EPSG:4326');
+            return assign({}, state, {
+                center
+            });
+        }
         default:
             return state;
     }
