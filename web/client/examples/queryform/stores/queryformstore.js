bbe4c044e69563aed0ccda45a87a2ebb726c73d4
tobia <tobia.dipisa@geo-solutions.it>
2016-02-03 17:43:50 +0100

    Providing the initial state for the query form example

--- web/client/examples/queryform/stores/queryformstore.js
+++ web/client/examples/queryform/stores/queryformstore.js
@@ -2,10 +2,39 @@ var {createStore, combineReducers} = require('redux');
 
 var queryForm = require('../../../reducers/queryform');
 
+const initialState = {
+    filterFields: [
+        {
+            rowId: 0,
+            attribute: null,
+            operator: null,
+            value: null,
+            exception: null
+        }
+    ],
+    attributes: [
+        {
+            id: "ListAttribute",
+            type: "list",
+            values: [
+                "value1",
+                "value2",
+                "value3",
+                "value4",
+                "value5"
+            ]
+        },
+        {
+            id: "DateAttribute",
+            type: "date"
+        }
+    ]
+};
+
  // reducers
 const reducers = combineReducers({
     queryForm
 });
 
 // export the store with the given reducers
-module.exports = createStore(reducers, {});
+module.exports = createStore(reducers, {queryForm: initialState});
