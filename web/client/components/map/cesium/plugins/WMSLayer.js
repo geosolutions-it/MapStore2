c5b96d3c64a00a807c6e47edfe4f3a7703d888c7
Mauro Bartolomeoli <maurobartolomeoli@gmail.com>
2016-02-05 12:06:39 +0100

    Fixes #423: improvements to the CesiumJS Map component

--- web/client/components/map/cesium/plugins/WMSLayer.js
+++ web/client/components/map/cesium/plugins/WMSLayer.js
@@ -16,21 +16,35 @@ function getWMSURLs( urls ) {
     return urls.map((url) => url.split("\?")[0]);
 }
 
+
+function splitUrl(originalUrl) {
+    let url = originalUrl;
+    let queryString = "";
+    if (originalUrl.indexOf('?') !== -1) {
+        url = originalUrl.substring(0, originalUrl.indexOf('?') + 1);
+        if (originalUrl.indexOf('%') !== -1) {
+            url = decodeURIComponent(url);
+        }
+        queryString = originalUrl.substring(originalUrl.indexOf('?') + 1);
+    }
+    return {url, queryString};
+}
+
 function WMSProxy(proxy) {
     this.proxy = proxy;
 }
 
 WMSProxy.prototype.getURL = function(resource) {
-    let url = resource;
-    let queryString = "";
-    if (resource.indexOf('?') !== -1) {
-        url = resource.substring(0, resource.indexOf('?') + 1);
-        if (url.indexOf('%') !== -1) {
-            url = decodeURIComponent(url);
-        }
-        queryString = resource.substring(resource.indexOf('?') + 1);
-    }
+    let {url, queryString} = splitUrl(resource);
     return this.proxy + encodeURIComponent(url + queryString);
+};
+
+function NoProxy() {
+}
+
+NoProxy.prototype.getURL = function(resource) {
+    let {url, queryString} = splitUrl(resource);
+    return url + queryString;
 };
 
 function wmsToCesiumOptions(options) {
@@ -43,24 +57,48 @@ function wmsToCesiumOptions(options) {
             useCORS = proxyUrl.useCORS || [];
             proxyUrl = proxyUrl.url;
         }
-        const isCORS = useCORS.reduce((found, current) => found || options.url.indexOf(current) === 0, false);
+        let url = options.url;
+        if (isArray(url)) {
+            url = url[0];
+        }
+        const isCORS = useCORS.reduce((found, current) => found || url.indexOf(current) === 0, false);
         proxy = !isCORS && proxyUrl;
     }
     // NOTE: can we use opacity to manage visibility?
     return assign({
         url: "{s}",
         subdomains: getWMSURLs(isArray(options.url) ? options.url : [options.url]),
-        proxy: proxy && new WMSProxy(proxy),
+        proxy: proxy && new WMSProxy(proxy) || new NoProxy(),
         layers: options.name,
-        parameters: {
+        parameters: assign({
             styles: options.style || "",
             format: options.format || 'image/png',
             transparent: options.transparent !== undefined ? options.transparent : true,
             opacity: opacity
-        }
-    }, options.params || {});
+        }, options.params || {})
+    });
+}
+
+
+function updateUrl(originalUrl, params) {
+    let {url, queryString} = splitUrl(originalUrl);
+    let parts = queryString.split('&').reduce((previous, current) => {
+        const [key, value] = current.split('=');
+        return assign({}, previous, {[key]: value});
+    }, {});
+    Object.keys(params).forEach((key) => {
+        parts[key] = encodeURIComponent(params[key]);
+    });
+    return url + Object.keys(parts).map((key) => key + '=' + parts[key]).join('&');
 }
 
 Layers.registerType('wms', (options) => {
-    return new Cesium.WebMapServiceImageryProvider(wmsToCesiumOptions(options));
+    let layer = new Cesium.WebMapServiceImageryProvider(wmsToCesiumOptions(options));
+    layer.updateParams = (params) => {
+        const provider = layer._tileProvider;
+
+        provider._url = updateUrl(provider._url, params);
+        provider._urlParts[1] = updateUrl(provider._urlParts[1], params);
+    };
+    return layer;
 });
