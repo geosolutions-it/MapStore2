68eead963f28e97e582a8b0ccd13aa0ca393eef0
Matteo Papi <mat.papi@gmail.com>
2016-02-12 17:44:13 +0100

    Mobile FullScreen Support

--- web/client/examples/viewer/components/FullScreen.jsx
+++ web/client/examples/viewer/components/FullScreen.jsx
@@ -19,40 +19,32 @@ var full = React.createClass({
                     <Button id="enter-full-screen-button" active={this.state.active_full_button} onClick = {() => {this.enterFullscreen(); }}><Glyphicon glyph="resize-full"/>ON</Button>
                     <Button id="exit-full-screen-button" active={this.state.active_resize_button} onClick = {() => {this.exitFullscreen(); }} ><Glyphicon glyph="resize-small" />OFF</Button>
                 </ButtonGroup>
-        		);
+                );
     },
-    enterFullscreen () {
+    enterFullscreen() {
         var docElm = document.documentElement;
         if (docElm.requestFullscreen) {
             docElm.requestFullscreen();
-        }
-        else if (docElm.msRequestFullscreen) {
+        } else if (docElm.msRequestFullscreen) {
             docElm.msRequestFullscreen();
-        }
-        else if (docElm.mozRequestFullScreen) {
+        } else if (docElm.mozRequestFullScreen) {
             docElm.mozRequestFullScreen();
-        }
-        else if (docElm.webkitRequestFullScreen) {
+        } else if (docElm.webkitRequestFullScreen) {
             docElm.webkitRequestFullScreen();
         }
-        return false;
         this.setState({ active_full_button: true, active_resize_button: false, activeKey: this.state.activeKey });
     },
-    exitFullscreen () {
+    exitFullscreen() {
         if (document.exitFullscreen) {
             document.exitFullscreen();
-        }
-        else if (document.msExitFullscreen) {
+        } else if (document.msExitFullscreen) {
             document.msExitFullscreen();
-        }
-        else if (document.mozCancelFullScreen) {
+        } else if (document.mozCancelFullScreen) {
             document.mozCancelFullScreen();
-        }
-        else if (document.webkitCancelFullScreen) {
+        } else if (document.webkitCancelFullScreen) {
             document.webkitCancelFullScreen();
         }
-        return false;
-        this.setState({ active_full_button: false, active_resize_button:true, activeKey: this.state.activeKey });
+        this.setState( { active_full_button: false, active_resize_button: true, activeKey: this.state.activeKey } );
     }
 });
 
