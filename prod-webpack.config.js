a79b2da4e54b475248d6d834ce2efe1bfb005922
Mauro Bartolomeoli <maurobartolomeoli@gmail.com>
2016-02-26 11:39:55 +0100

    Fixed #470: removed mapstore-commons.js

--- prod-webpack.config.js
+++ prod-webpack.config.js
@@ -1,7 +1,6 @@
 var webpackConfig = require('./webpack.config.js');
 var path = require("path");
 var UglifyJsPlugin = require("webpack/lib/optimize/UglifyJsPlugin");
-var CommonsChunkPlugin = require("webpack/lib/optimize/CommonsChunkPlugin");
 var DefinePlugin = require("webpack/lib/DefinePlugin");
 var NormalModuleReplacementPlugin = require("webpack/lib/NormalModuleReplacementPlugin");
 
@@ -14,7 +13,6 @@ webpackConfig.plugins = [
         'NODE_ENV': '"production"'
       }
     }),
-    new CommonsChunkPlugin("commons", "mapstore-commons.js"),
     new NormalModuleReplacementPlugin(/leaflet$/, path.join(__dirname, "web", "client", "libs", "leaflet")),
     new NormalModuleReplacementPlugin(/openlayers$/, path.join(__dirname, "web", "client", "libs", "openlayers")),
     new NormalModuleReplacementPlugin(/proj4$/, path.join(__dirname, "web", "client", "libs", "proj4")),
