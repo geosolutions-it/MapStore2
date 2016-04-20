6923bd8bfb45f7b04d94109c00ff4bd1b4b4f98f
mbarto <maurobartolomeoli@gmail.com>
2016-04-20 09:23:11 +0200

    Fixes to the Print MapPreview zoom calculation (#529)

--- web/client/utils/MapUtils.js
+++ web/client/utils/MapUtils.js
@@ -190,6 +190,7 @@ module.exports = {
     getGoogleMercatorScales,
     getGoogleMercatorScale,
     getZoomForExtent,
+    defaulGetZoomForExtent,
     getCenterForExtent,
     getResolutions,
     getScales
