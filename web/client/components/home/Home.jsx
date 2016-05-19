330efd7d772b70106d33d249a4414d7af1e05004
Mauro Bartolomeoli <maurobartolomeoli@gmail.com>
2016-05-19 11:57:19 +0200

    Restored mobile Home button

--- web/client/components/home/Home.jsx
+++ web/client/components/home/Home.jsx
@@ -7,14 +7,45 @@
  */
 const React = require('react');
 
+const {Glyphicon, Tooltip} = require('react-bootstrap');
+const ToggleButton = require('../../components/buttons/ToggleButton');
+const Message = require('../../components/I18N/Message');
+
 const Home = React.createClass({
-    propTypes: {},
+    propTypes: {
+        isPanel: React.PropTypes.bool,
+        help: React.PropTypes.object,
+        changeHelpText: React.PropTypes.func,
+        changeHelpwinVisibility: React.PropTypes.func
+    },
+    contextTypes: {
+        router: React.PropTypes.object,
+        messages: React.PropTypes.object
+    },
     getDefaultProps() {
-        return {};
+        return {
+            isPanel: false,
+            icon: <Glyphicon glyph="home"/>
+        };
     },
     render() {
-        return (<span/>);
+        let tooltip = <Tooltip id="toolbar-home-button">{<Message msgId="gohome"/>}</Tooltip>;
+        return (
+            <ToggleButton
+                {...this.props}
+                id="home-button"
+                isButton={true}
+                pressed={false}
+                glyphicon="home"
+                helpText={<Message msgId="helptexts.gohome"/>}
+                onClick={this.goHome}
+                tooltip={tooltip}
+                tooltipPlace="left"
+                />
+        );
+    },
+    goHome() {
+        this.context.router.push("/");
     }
 });
-
 module.exports = Home;
