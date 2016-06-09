04632e544a875b0d8027a2ba389ead8f58705c0f
tobia <tobia.dipisa@geo-solutions.it>
2016-06-09 13:26:01 +0200

    Fixing geometry settings for wfs 2.0 and adding pagination support in filter utils

--- web/client/utils/__tests__/FilterUtils-test.js
+++ web/client/utils/__tests__/FilterUtils-test.js
@@ -106,4 +106,103 @@ describe('FilterUtils', () => {
         let filter = FilterUtils.toCQLFilter("ft_name_test", filterObj);
         expect(filter).toExist();
     });
+
+    it('Check for pagination wfs 1.1.0', () => {
+        let filterObj = {
+            pagination: {
+                startIndex: 1,
+                maxFeatures: 20
+            },
+            spatialField: {
+                attribute: "the_geom",
+                geometry: {
+                    center: [1, 1],
+                    coordinates: [
+                        [1, 2],
+                        [2, 3],
+                        [3, 4],
+                        [4, 5],
+                        [5, 6]
+                    ],
+                    extent: [
+                        1, 2, 3, 4, 5
+                    ],
+                    projection: "EPSG:4326",
+                    radius: 1,
+                    type: "Polygon"
+                },
+                method: "BBOX",
+                operation: "INTERSECTS"
+            }
+        };
+
+        let filter = FilterUtils.toOGCFilter("ft_name_test", filterObj, "1.1.0");
+        expect(filter.indexOf('maxFeatures="20"') !== -1).toBe(true);
+        expect(filter.indexOf('startIndex="1"') !== -1).toBe(true);
+    });
+
+    it('Check for pagination wfs 2.0', () => {
+        let filterObj = {
+            pagination: {
+                startIndex: 1,
+                maxFeatures: 20
+            },
+            spatialField: {
+                attribute: "the_geom",
+                geometry: {
+                    center: [1, 1],
+                    coordinates: [
+                        [1, 2],
+                        [2, 3],
+                        [3, 4],
+                        [4, 5],
+                        [5, 6]
+                    ],
+                    extent: [
+                        1, 2, 3, 4, 5
+                    ],
+                    projection: "EPSG:4326",
+                    radius: 1,
+                    type: "Polygon"
+                },
+                method: "BBOX",
+                operation: "INTERSECTS"
+            }
+        };
+
+        let filter = FilterUtils.toOGCFilter("ft_name_test", filterObj);
+        expect(filter.indexOf('count="20"') !== -1).toBe(true);
+        expect(filter.indexOf('startIndex="1"') !== -1).toBe(true);
+    });
+
+    it('Check for no pagination', () => {
+        let filterObj = {
+            spatialField: {
+                attribute: "the_geom",
+                geometry: {
+                    center: [1, 1],
+                    coordinates: [
+                        [1, 2],
+                        [2, 3],
+                        [3, 4],
+                        [4, 5],
+                        [5, 6]
+                    ],
+                    extent: [
+                        1, 2, 3, 4, 5
+                    ],
+                    projection: "EPSG:4326",
+                    radius: 1,
+                    type: "Polygon"
+                },
+                method: "BBOX",
+                operation: "INTERSECTS"
+            }
+        };
+
+        let filter = FilterUtils.toOGCFilter("ft_name_test", filterObj);
+        expect(filter.indexOf('count="20"') !== -1).toBe(false);
+        expect(filter.indexOf('maxFeatures="20"') !== -1).toBe(false);
+        expect(filter.indexOf('startIndex="1"') !== -1).toBe(false);
+    });
 });
