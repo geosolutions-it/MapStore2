6349e3dc0df313c47ae87d05af9eae1e0b1c530a
Lorenzo Pini <lore.pini@tin.it>
2016-06-23 10:36:27 +0200

    Style MeasureResults

--- web/client/components/mapcontrols/measure/MeasureResults.jsx
+++ web/client/components/mapcontrols/measure/MeasureResults.jsx
@@ -7,8 +7,9 @@
  */
 
 const React = require('react');
-const {Panel} = require('react-bootstrap');
+const {Panel, Glyphicon} = require('react-bootstrap');
 const Draggable = require('react-draggable');
+const Dialog = require('../../misc/Dialog');
 
 const Message = require('../../../plugins/locale/Message');
 const ReactIntl = require('react-intl');
@@ -40,7 +41,10 @@ const MeasureResults = React.createClass({
         separatePanel: React.PropTypes.bool,
         title: React.PropTypes.object,
         panelStyle: React.PropTypes.object,
-        panelClassName: React.PropTypes.string
+        panelClassName: React.PropTypes.string,
+        style: React.PropTypes.object,
+        withPanelAsContainer: React.PropTypes.bool,
+        closeGlyph: React.PropTypes.string
     },
     getDefaultProps() {
         return {
@@ -63,7 +67,9 @@ const MeasureResults = React.createClass({
                 position: "absolute",
                 overflow: "auto"
             },
-            panelClassName: "drawer-menu-panel"
+            panelClassName: "drawer-menu-panel",
+            withPanelAsContainer: true,
+            closeGlyph: ""
         };
     },
     shouldComponentUpdate(nextProps) {
@@ -84,25 +90,40 @@ const MeasureResults = React.createClass({
     },
     renderHeader() {
         return (
-            <span>
-                <Message msgId="measureComponent.title" />
-                <button onClick={this.onModalHiding} className="close"><span>×</span></button>
+            <span role="header">
+                <span className="measure-panel-title"><Message msgId="measureComponent.title" /></span>
+                <button onClick={this.onModalHiding} className="close">{this.props.closeGlyph ? <Glyphicon glyph={this.props.closeGlyph}/> : <span>×</span>}</button>
             </span>
+        );
+    },
+    renderBody() {
+        let decimalFormat = {style: "decimal", minimumIntegerDigits: 1, maximumFractionDigits: 2, minimumFractionDigits: 2};
+        return (
+            <div className="panel-body" role="body">
+                <p><span>{this.props.lengthLabel}: </span><span id="measure-len-res"><FormattedNumber key="len" {...decimalFormat} value={measureUtils.getFormattedLength(this.props.uom.length.unit, this.props.measurement.len)} /> {this.props.uom.length.label}</span></p>
+                <p><span>{this.props.areaLabel}: </span><span id="measure-area-res"><FormattedNumber key="area" {...decimalFormat} value={measureUtils.getFormattedArea(this.props.uom.area.unit, this.props.measurement.area)} /> {this.props.uom.area.label}</span></p>
+                <p><span>{this.props.bearingLabel}: </span><span id="measure-bearing-res">{measureUtils.getFormattedBearingValue(this.props.measurement.bearing)}</span></p>
+            </div>
         );
     },
     render() {
         if (this.props.lineMeasureEnabled || this.props.areaMeasureEnabled || this.props.bearingMeasureEnabled ) {
-            let decimalFormat = {style: "decimal", minimumIntegerDigits: 1, maximumFractionDigits: 2, minimumFractionDigits: 2};
+            // compatibility with old theme
+            if (this.props.withPanelAsContainer) {
+                return (
+                    <Draggable key={"drawer-menu--item-collapse-measures"}>
+                        <Panel header={this.renderHeader()} style={this.props.panelStyle} className={this.props.panelClassName}>
+                            {this.renderBody()}
+                        </Panel>
+                    </Draggable>
+                );
+            }
+            // else use the new theme
             return (
-                <Draggable key={"drawer-menu--item-collapse-measures"}>
-                    <Panel header={this.renderHeader()} style={this.props.panelStyle} className={this.props.panelClassName}>
-                        <div className="panel-body">
-                            <p><span>{this.props.lengthLabel}: </span><span id="measure-len-res"><FormattedNumber key="len" {...decimalFormat} value={measureUtils.getFormattedLength(this.props.uom.length.unit, this.props.measurement.len)} /> {this.props.uom.length.label}</span></p>
-                            <p><span>{this.props.areaLabel}: </span><span id="measure-area-res"><FormattedNumber key="area" {...decimalFormat} value={measureUtils.getFormattedArea(this.props.uom.area.unit, this.props.measurement.area)} /> {this.props.uom.area.label}</span></p>
-                            <p><span>{this.props.bearingLabel}: </span><span id="measure-bearing-res">{measureUtils.getFormattedBearingValue(this.props.measurement.bearing)}</span></p>
-                        </div>
-                    </Panel>
-                </Draggable>
+                <Dialog id="mapstore-measure-panel" style={this.props.style}>
+                    {this.renderHeader()}
+                    {this.renderBody()}
+                </Dialog>
             );
         }
         return null;
