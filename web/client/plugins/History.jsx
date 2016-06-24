5b305ef47e8bdbc6f7f7dda897272114da337926
Lorenzo Pini <lore.pini@tin.it>
2016-06-24 16:00:50 +0200

    Various layout fixes

--- web/client/plugins/History.jsx
+++ web/client/plugins/History.jsx
@@ -21,7 +21,6 @@ const {undo, redo} = ActionCreators;
 const UndoButton = connect((state) => {
     let mapHistory = state.map && state.map.past && {past: state.map.past, future: state.map.future} || {past: [], future: []};
     return {
-        btnConfig: {disabled: (mapHistory.past.length > 0) ? false : true},
         disabled: (mapHistory.past.length > 0) ? false : true
     };
 }, {
@@ -31,7 +30,6 @@ const UndoButton = connect((state) => {
 const RedoButton = connect((state) => {
     let mapHistory = state.map && state.map.past && {past: state.map.past, future: state.map.future} || {past: [], future: []};
     return {
-        btnConfig: {disabled: (mapHistory.future.length > 0) ? false : true},
         disabled: (mapHistory.future.length > 0) ? false : true
     };
 }, {
