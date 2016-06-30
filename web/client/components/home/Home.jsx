50171a0af3732759060afe2d5871fe32e6fee64f
Lorenzo Natali <offtherailz@gmail.com>
2016-06-30 17:22:29 +0200

    fix #776. Move home to omnibar (#777)

--- web/client/components/home/Home.jsx
+++ web/client/components/home/Home.jsx
@@ -7,16 +7,12 @@
  */
 const React = require('react');
 
-const {Glyphicon, Tooltip} = require('react-bootstrap');
-const ToggleButton = require('../../components/buttons/ToggleButton');
+const {Glyphicon, OverlayTrigger, Tooltip, Button} = require('react-bootstrap');
 const Message = require('../../components/I18N/Message');
 
 const Home = React.createClass({
     propTypes: {
-        isPanel: React.PropTypes.bool,
-        help: React.PropTypes.object,
-        changeHelpText: React.PropTypes.func,
-        changeHelpwinVisibility: React.PropTypes.func
+        icon: React.PropTypes.node
     },
     contextTypes: {
         router: React.PropTypes.object,
@@ -24,24 +20,23 @@ const Home = React.createClass({
     },
     getDefaultProps() {
         return {
-            isPanel: false,
             icon: <Glyphicon glyph="home"/>
         };
     },
     render() {
         let tooltip = <Tooltip id="toolbar-home-button">{<Message msgId="gohome"/>}</Tooltip>;
         return (
-            <ToggleButton
+            <OverlayTrigger overlay={tooltip}>
+            <Button
                 {...this.props}
                 id="home-button"
-                isButton={true}
-                pressed={false}
-                glyphicon="home"
-                helpText={<Message msgId="helptexts.gohome"/>}
+                className="square-button"
+                bsStyle="primary"
                 onClick={this.goHome}
                 tooltip={tooltip}
                 tooltipPlace="left"
-                />
+                >{this.props.icon}</Button>
+        </OverlayTrigger>
         );
     },
     goHome() {
