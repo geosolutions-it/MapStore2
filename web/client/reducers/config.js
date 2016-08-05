14943b48504197c52d0d04799093dcbe99eaa300
Lorenzo Natali <lorenzo.natali@geo-solutions.it>
2016-08-05 17:23:32 +0200

    fixed map size lost when reload config (before pan)

--- web/client/reducers/config.js
+++ web/client/reducers/config.js
@@ -14,12 +14,13 @@ var ConfigUtils = require('../utils/ConfigUtils');
 function mapConfig(state = null, action) {
     switch (action.type) {
         case MAP_CONFIG_LOADED:
+            let size = (state && state.map && state.map.present && state.map.present.size) || (state && state.map && state.map.size);
+
             let hasVersion = (action.config.version && action.config.version >= 2);
             // we get from the configuration what will be used as the initial state
             let mapState = (action.legacy && !hasVersion) ? ConfigUtils.convertFromLegacy(action.config) : ConfigUtils.normalizeConfig(action.config.map);
-            if (action.mapId) {
-                mapState.map = assign({}, mapState.map, {mapId: action.mapId});
-            }
+
+            mapState.map = assign({}, mapState.map, {mapId: action.mapId, size});
             // we store the map initial state for future usage
             return assign({}, mapState, {mapInitialConfig: mapState.map});
         case MAP_CONFIG_LOAD_ERROR:
