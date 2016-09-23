57aa12c8131519cfc1aa4be04138f0c2d9df9a98
saidarao <saidarao121@gmail.com>
2016-09-23 15:10:34 +0200

    browser check condition for Firefox mobile added

--- web/client/examples/featuregrid/app.jsx
+++ web/client/examples/featuregrid/app.jsx
@@ -5,50 +5,49 @@
  * This source code is licensed under the BSD-style license found in the
  * LICENSE file in the root directory of this source tree.
  */
-const React = require('react');
-const ReactDOM = require('react-dom');
-
-const {Provider} = require('react-redux');
-
-// initializes Redux store
-var store = require('./stores/featuregridstore');
-
-const {loadMapConfig} = require('../../actions/config');
-const {changeBrowserProperties} = require('../../actions/browser');
-const {loadLocale} = require('../../actions/locale');
-
-const ConfigUtils = require('../../utils/ConfigUtils');
-const LocaleUtils = require('../../utils/LocaleUtils');
-const url = require('url');
-
-// reads parameter(s) from the url
-const urlQuery = url.parse(window.location.href, true).query;
-
-// get configuration file url (defaults to config.json on the app folder)
-const { configUrl, legacy } = ConfigUtils.getConfigurationOptions(urlQuery, 'config', 'json');
-
-// dispatch an action to load the configuration from the config.json file
-store.dispatch(loadMapConfig(configUrl, legacy));
-
-store.dispatch(changeBrowserProperties(ConfigUtils.getBrowserProperties()));
-
-const FeatureGrid = require('./containers/FeatureGrid');
-
-// we spread the store to the all application
-// wrapping it with a Provider component
-const FeatureGridApp = React.createClass({
-    render() {
-        return (
-        <Provider store={store}>
-            <FeatureGrid/>
-        </Provider>);
-    }
-});
-
-let locale = LocaleUtils.getUserLocale();
-store.dispatch(loadLocale('../../translations', locale));
-
 const startApp = () => {
+    const React = require('react');
+    const ReactDOM = require('react-dom');
+
+    const {Provider} = require('react-redux');
+
+    // initializes Redux store
+    var store = require('./stores/featuregridstore');
+
+    const {loadMapConfig} = require('../../actions/config');
+    const {changeBrowserProperties} = require('../../actions/browser');
+    const {loadLocale} = require('../../actions/locale');
+
+    const ConfigUtils = require('../../utils/ConfigUtils');
+    const LocaleUtils = require('../../utils/LocaleUtils');
+    const url = require('url');
+
+    // reads parameter(s) from the url
+    const urlQuery = url.parse(window.location.href, true).query;
+
+    // get configuration file url (defaults to config.json on the app folder)
+    const { configUrl, legacy } = ConfigUtils.getConfigurationOptions(urlQuery, 'config', 'json');
+
+    // dispatch an action to load the configuration from the config.json file
+    store.dispatch(loadMapConfig(configUrl, legacy));
+
+    store.dispatch(changeBrowserProperties(ConfigUtils.getBrowserProperties()));
+
+    const FeatureGrid = require('./containers/FeatureGrid');
+
+    // we spread the store to the all application
+    // wrapping it with a Provider component
+    const FeatureGridApp = React.createClass({
+        render() {
+            return (
+            <Provider store={store}>
+                <FeatureGrid/>
+            </Provider>);
+        }
+    });
+
+    let locale = LocaleUtils.getUserLocale();
+    store.dispatch(loadLocale('../../translations', locale));
     // Renders the application, wrapped by the Redux Provider to connect the store to components
     ReactDOM.render(<FeatureGridApp/>, document.getElementById('container'));
 };
--- web/client/examples/print/app.jsx
+++ web/client/examples/print/app.jsx
@@ -5,56 +5,55 @@
  * This source code is licensed under the BSD-style license found in the
  * LICENSE file in the root directory of this source tree.
  */
-const React = require('react');
-const ReactDOM = require('react-dom');
-
-const {Provider} = require('react-redux');
-
-// initializes Redux store
-var store = require('./stores/store');
-
-const {loadMapConfig} = require('../../actions/config');
-const {changeBrowserProperties} = require('../../actions/browser');
-const {loadLocale} = require('../../actions/locale');
-
-const ConfigUtils = require('../../utils/ConfigUtils');
-const LocaleUtils = require('../../utils/LocaleUtils');
-const url = require('url');
-
-// reads parameter(s) from the url
-const urlQuery = url.parse(window.location.href, true).query;
-
-// get configuration file url (defaults to config.json on the app folder)
-const { configUrl, legacy } = ConfigUtils.getConfigurationOptions(urlQuery, 'config', 'json');
-
-const {loadPrintCapabilities} = require('../../actions/print');
-
-// dispatch an action to load the configuration from the config.json file
-store.dispatch(loadMapConfig(configUrl, legacy));
-
-store.dispatch(changeBrowserProperties(ConfigUtils.getBrowserProperties()));
-
-ConfigUtils.loadConfiguration().then(() => {
-    store.dispatch(loadPrintCapabilities(ConfigUtils.getConfigProp('printUrl')));
-});
-
-const Print = require('./containers/Print');
-
-// we spread the store to the all application
-// wrapping it with a Provider component
-const PrintApp = React.createClass({
-    render() {
-        return (
-        <Provider store={store}>
-            <Print/>
-        </Provider>);
-    }
-});
-
-let locale = LocaleUtils.getUserLocale();
-store.dispatch(loadLocale('../../translations', locale));
-
 const startApp = () => {
+    const React = require('react');
+    const ReactDOM = require('react-dom');
+
+    const {Provider} = require('react-redux');
+
+    // initializes Redux store
+    var store = require('./stores/store');
+
+    const {loadMapConfig} = require('../../actions/config');
+    const {changeBrowserProperties} = require('../../actions/browser');
+    const {loadLocale} = require('../../actions/locale');
+
+    const ConfigUtils = require('../../utils/ConfigUtils');
+    const LocaleUtils = require('../../utils/LocaleUtils');
+    const url = require('url');
+
+    // reads parameter(s) from the url
+    const urlQuery = url.parse(window.location.href, true).query;
+
+    // get configuration file url (defaults to config.json on the app folder)
+    const { configUrl, legacy } = ConfigUtils.getConfigurationOptions(urlQuery, 'config', 'json');
+
+    const {loadPrintCapabilities} = require('../../actions/print');
+
+    // dispatch an action to load the configuration from the config.json file
+    store.dispatch(loadMapConfig(configUrl, legacy));
+
+    store.dispatch(changeBrowserProperties(ConfigUtils.getBrowserProperties()));
+
+    ConfigUtils.loadConfiguration().then(() => {
+        store.dispatch(loadPrintCapabilities(ConfigUtils.getConfigProp('printUrl')));
+    });
+
+    const Print = require('./containers/Print');
+
+    // we spread the store to the all application
+    // wrapping it with a Provider component
+    const PrintApp = React.createClass({
+     render() {
+         return (
+         <Provider store={store}>
+             <Print/>
+         </Provider>);
+     }
+    });
+
+    let locale = LocaleUtils.getUserLocale();
+    store.dispatch(loadLocale('../../translations', locale));
     // Renders the application, wrapped by the Redux Provider to connect the store to components
     ReactDOM.render(<PrintApp/>, document.getElementById('container'));
 };
