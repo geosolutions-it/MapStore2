6cab6c649f99e195dfdf5b141ae78f71dd4a78f9
Sandro Mani <manisandro@gmail.com>
2016-10-17 11:56:58 +0200

    Fix typo (#1158)

--- web/client/utils/MapUtils.js
+++ web/client/utils/MapUtils.js
@@ -151,7 +151,7 @@ function defaultGetZoomForExtent(extent, mapSize, minZoom, maxZoom, dpi, mapReso
         return diff > previous.diff ? previous : {diff: diff, zoom: index};
     }, {diff: Number.POSITIVE_INFINITY, zoom: 0});
 
-    return Math.max(0, zoom, Math.min(zoom, maxZoom));
+    return Math.max(0, Math.min(zoom, maxZoom));
 }
 
 /**
