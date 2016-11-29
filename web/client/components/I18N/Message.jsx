8b1669232af9bffb4b0e087275f16620252bb5a0
Sandro Mani <manisandro@gmail.com>
2016-11-29 14:36:31 +0100

    Fix Intl not defined error on firefox mobile

--- web/client/components/I18N/Message.jsx
+++ web/client/components/I18N/Message.jsx
@@ -6,6 +6,9 @@
  * LICENSE file in the root directory of this source tree.
  */
 const React = require('react');
+if (!global.Intl) {
+    require('intl');
+}
 const ReactIntl = require('react-intl');
 
 const FormattedMessage = ReactIntl.FormattedMessage;
