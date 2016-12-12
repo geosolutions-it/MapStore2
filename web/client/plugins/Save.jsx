ae14a59114779f7d2471ee17c28b1960139707f2
Riccardo Mari <rmari76@gmail.com>
2016-12-12 11:45:38 +0100

    Small fixes about layer params and print (qgis and mapserver case)

--- web/client/plugins/Save.jsx
+++ web/client/plugins/Save.jsx
@@ -100,7 +100,9 @@ const Save = React.createClass({
                         type: layer.type,
                         url: layer.url,
                         bbox: layer.bbox,
-                        visibility: layer.visibility
+                        visibility: layer.visibility,
+                        singleTile: layer.singleTile || false,
+                        ...assign({}, layer.params ? {params: layer.params} : {})
                     };
                 });
                 // Groups are ignored, as they already are defined in the layers
