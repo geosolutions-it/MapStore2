ba4b2f4b749abbd3215830007e01793d656fc9ff
Mauro Bartolomeoli <maurobartolomeoli@gmail.com>
2016-12-19 15:59:57 +0100

    Fixes #1357: removed drawermenu dependency from query panel

--- web/client/plugins/DrawerMenu.jsx
+++ web/client/plugins/DrawerMenu.jsx
@@ -23,8 +23,7 @@ const {partialRight} = require('lodash');
 
 const Menu = connect((state) => ({
     show: state.controls.drawer && state.controls.drawer.enabled,
-    activeKey: state.controls.drawer && state.controls.drawer.menu,
-    width: (state.controls.queryPanel && state.controls.queryPanel.enabled) ? 700 : 300
+    activeKey: state.controls.drawer && state.controls.drawer.menu
 }), {
     onToggle: toggleControl.bind(null, 'drawer', null),
     onChoose: partialRight(setControlProperty.bind(null, 'drawer', 'menu'), true),
