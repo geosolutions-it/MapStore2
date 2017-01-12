2ddaa5181632a49bdf8153fce1343c1b5427a122
Lorenzo Natali <lorenzo.natali@geo-solutions.it>
2017-01-12 16:18:29 +0100

    fixed event capture for new filter and map move

--- web/client/components/data/featuregrid/DockedFeatureGrid.jsx
+++ web/client/components/data/featuregrid/DockedFeatureGrid.jsx
@@ -116,14 +116,14 @@ const DockedFeatureGrid = React.createClass({
     },
     shouldComponentUpdate(nextProps) {
         return Object.keys(this.props).reduce((prev, prop) => {
-            if ( !prev && (prop !== 'map' && this.props[prop] === nextProps[prop])) {
+            if ( !prev && prop !== 'map' && prop !== 'columnsDef' && this.props[prop] !== nextProps[prop])) {
                 return true;
             }
             return prev;
         }, false);
     },
     componentWillUpdate(nextProps) {
-        if (!nextProps.loadingGrid && !this.props.isNew && nextProps.isNew) {
+        if (!nextProps.loadingGrid && this.props.isNew && !nextProps.isNew) {
             this.setState({searchN: this.state.searchN + 1});
         }
         if (!nextProps.loadingGrid && nextProps.pagination && (nextProps.dataSourceOptions !== this.props.dataSourceOptions)) {
