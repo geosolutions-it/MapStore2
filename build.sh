7fb0c461b5a7f20b5275bfee6b0893fd008bf664
Fernando Ribeiro <fernandinand@gmail.com>
2017-02-23 15:02:18 +0100

    #1475 Changes required on PR comments

--- build.sh
+++ build.sh
@@ -6,7 +6,3 @@ npm run compile
 npm run lint
 npm test
 mvn clean install
-
-# TODO: Test folder for reference docs build
-chmod +x genDocs
-./genDocs web/client/components/mapcontrols/mouseposition/
