7f4b1306821ab8adf4d5b203021ed795454b2994
diegosanmartino <diego.sanmartino@gmail.com>
2017-03-08 11:16:25 +0100

    used Message

--- web/client/plugins/drawer/Menu.jsx
+++ web/client/plugins/drawer/Menu.jsx
@@ -8,6 +8,7 @@
 var React = require('react');
 var {Glyphicon, Button, OverlayTrigger, Tooltip} = require('react-bootstrap');
 var Sidebar = require('react-sidebar').default;
+var Message = require('../../components/I18N/Message');
 
 var Menu = React.createClass({
     propTypes: {
@@ -62,9 +63,9 @@ var Menu = React.createClass({
             const button = (<Button key={child.props.eventKey} bsSize="large" className={(child.props.buttonConfig && child.props.buttonConfig.buttonClassName) ? child.props.buttonConfig.buttonClassName : "square-button"} onClick={this.props.onChoose.bind(null, child.props.eventKey)} bsStyle={this.props.activeKey === child.props.eventKey ? 'default' : 'primary'}>
                         {child.props.glyph ? <Glyphicon glyph={child.props.glyph} /> : child.props.icon}
                     </Button>);
-            const tooltip = <Tooltip key={"tooltip." + child.props.eventKey} id={"tooltip." + child.props.eventKey}>{child.props.buttonConfig.tooltip || ''}</Tooltip>;
+            const tooltip = <Tooltip key={"tooltip." + child.props.eventKey} id={"tooltip." + child.props.eventKey}><Message msgId={child.props.buttonConfig.tooltip}/></Tooltip>;
             return child.props.buttonConfig.tooltip ? (
-                <OverlayTrigger placement={"top"} key={"overlay-trigger." + child.props.eventKey}
+                <OverlayTrigger placement={"bottom"} key={"overlay-trigger." + child.props.eventKey}
                     overlay={tooltip}>
                     {button}
                 </OverlayTrigger>
