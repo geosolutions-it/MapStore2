e7b9ef8b4ba17d7ca1c2b31fd2a69c62c9f6ee45
Lorenzo Natali <lorenzo.natali@geo-solutions.it>
2017-04-06 18:00:18 +0200

    Set optional drawer menu for share ( see #1632)
     - Documented drawer menu plugin
     - Added a forceDrawer and a hide option
     - Improved share to manage forceDrawer option

--- web/client/components/share/ShareEmbed.jsx
+++ web/client/components/share/ShareEmbed.jsx
@@ -15,10 +15,10 @@
 const React = require('react');
 const CopyToClipboard = require('react-copy-to-clipboard');
 const Message = require('../../components/I18N/Message');
-const {Glyphicon, Col, Grid, Row, Tooltip, Button} = require('react-bootstrap');
+const {Glyphicon, Col, Grid, Row, Tooltip, Button, Checkbox} = require('react-bootstrap');
 const OverlayTrigger = require('../misc/OverlayTrigger');
 
-
+const url = require('url');
 // css required
 require('./share.css');
 
@@ -27,11 +27,11 @@ const ShareEmbed = React.createClass({
         shareUrl: React.PropTypes.string
     },
     getInitialState() {
-        return {copied: false};
+        return {copied: false, forceDrawer: false};
     },
   render() {
 
-      const codeEmbedded = "<iframe style=\"border: none;\" height=\"400\" width=\"600\" src=\"" + this.props.shareUrl + "\"></iframe>";
+      const codeEmbedded = "<iframe style=\"border: none;\" height=\"400\" width=\"600\" src=\"" + this.generateUrl(this.props.shareUrl) + "\"></iframe>";
       const tooltip = (<Tooltip placement="bottom" className="in" id="tooltip-bottom" style={{zIndex: 2001}}>
                            {this.state.copied ? <Message msgId="share.msgCopiedUrl"/> : <Message msgId="share.msgToCopyUrl"/>}
                        </Tooltip>);
@@ -44,11 +44,16 @@ const ShareEmbed = React.createClass({
                       </OverlayTrigger>);
       return (
           <div className="input-link">
+
+
               <Grid className="embed-box" fluid={true}>
                   <Row key="title">
                         <h4>
                            <Message msgId="share.embeddedLinkTitle"/>
                         </h4>
+                        <Checkbox checked={this.state.forceDrawer} onChange={() => this.setState({forceDrawer: !this.state.forceDrawer})}>
+                            <Message msgId="share.forceDrawer"/>
+                         </Checkbox>
                     </Row>
                     <Row key="data" className="row-button">
                         <Col key="textarea" xs={10} sm={10} md={10}><textarea name="description" rows="6" value={codeEmbedded} enabled="false" readOnly /></Col>
@@ -59,6 +64,14 @@ const ShareEmbed = React.createClass({
                 </Grid>
           </div>
       );
+  },
+  generateUrl() {
+      const parsed = url.parse(this.props.shareUrl, true);
+      if (this.state.forceDrawer) {
+          parsed.query.forceDrawer = true;
+      }
+      return url.format(parsed);
+
   }
 });
 
