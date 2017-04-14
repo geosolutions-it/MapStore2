0b28643fc814770f92727cbcd87bd1c016c3d593
Mauro Bartolomeoli <maurobartolomeoli@gmail.com>
2017-04-14 16:44:05 +0200

    Added method to get GML geometry to FilterUtils

--- web/client/utils/FilterUtils.jsx
+++ web/client/utils/FilterUtils.jsx
@@ -7,6 +7,8 @@
  * LICENSE file in the root directory of this source tree.
  */
 
+const {isArray} = require('lodash');
+
 const normalizeVersion = (version) => {
     if (!version) {
         return "2.0";
@@ -385,31 +387,20 @@ const FilterUtils = {
         gmlPolygon += '</gml:Polygon>';
         return gmlPolygon;
     },
-
-    processOGCSpatialFilter: function(version, objFilter, nsplaceholder) {
-        let ogc = ogcSpatialOperator[objFilter.spatialField.operation].startTag;
-        ogc +=
-            propertyTagReference[nsplaceholder].startTag +
-                objFilter.spatialField.attribute +
-            propertyTagReference[nsplaceholder].endTag;
-
-        switch (objFilter.spatialField.operation) {
-            case "INTERSECTS":
-            case "DWITHIN":
-            case "WITHIN":
-            case "CONTAINS": {
-                switch (objFilter.spatialField.geometry.type) {
+    processOGCGeometry: function(version, geometry) {
+        let ogc = '';
+        switch (geometry.type) {
                     case "Point":
-                        ogc += this.getGmlPointElement(objFilter.spatialField.geometry.coordinates,
-                            objFilter.spatialField.geometry.projection || "EPSG:4326", version);
+                ogc += this.getGmlPointElement(geometry.coordinates,
+                    geometry.projection || "EPSG:4326", version);
                         break;
                     case "MultiPoint":
-                        ogc += '<gml:MultiPoint srsName="' + (objFilter.spatialField.geometry.projection || "EPSG:4326") + '">';
+                ogc += '<gml:MultiPoint srsName="' + (geometry.projection || "EPSG:4326") + '">';
 
                         // //////////////////////////////////////////////////////////////////////////
                         // Coordinates of a MultiPoint are an array of positions
                         // //////////////////////////////////////////////////////////////////////////
-                        objFilter.spatialField.geometry.coordinates.forEach((element) => {
+                geometry.coordinates.forEach((element) => {
                             let point = element;
                             if (point) {
                                 ogc += "<gml:pointMember>";
@@ -421,19 +412,19 @@ const FilterUtils = {
                         ogc += '</gml:MultiPoint>';
                         break;
                     case "Polygon":
-                        ogc += this.getGmlPolygonElement(objFilter.spatialField.geometry.coordinates,
-                            objFilter.spatialField.geometry.projection || "EPSG:4326", version);
+                ogc += this.getGmlPolygonElement(geometry.coordinates,
+                    geometry.projection || "EPSG:4326", version);
                         break;
                     case "MultiPolygon":
                         const multyPolygonTagName = version === "2.0" ? "MultiSurface" : "MultiPolygon";
                         const polygonMemberTagName = version === "2.0" ? "surfaceMembers" : "polygonMember";
 
-                        ogc += '<gml:' + multyPolygonTagName + ' srsName="' + (objFilter.spatialField.geometry.projection || "EPSG:4326") + '">';
+                ogc += '<gml:' + multyPolygonTagName + ' srsName="' + (geometry.projection || "EPSG:4326") + '">';
 
                         // //////////////////////////////////////////////////////////////////////////
                         // Coordinates of a MultiPolygon are an array of Polygon coordinate arrays
                         // //////////////////////////////////////////////////////////////////////////
-                        objFilter.spatialField.geometry.coordinates.forEach((element) => {
+                geometry.coordinates.forEach((element) => {
                             let polygon = element;
                             if (polygon) {
                                 ogc += "<gml:" + polygonMemberTagName + ">";
@@ -447,6 +438,21 @@ const FilterUtils = {
                     default:
                         break;
                 }
+        return ogc;
+    },
+    processOGCSpatialFilter: function(version, objFilter, nsplaceholder) {
+        let ogc = ogcSpatialOperator[objFilter.spatialField.operation].startTag;
+        ogc +=
+            propertyTagReference[nsplaceholder].startTag +
+                objFilter.spatialField.attribute +
+            propertyTagReference[nsplaceholder].endTag;
+
+        switch (objFilter.spatialField.operation) {
+            case "INTERSECTS":
+            case "DWITHIN":
+            case "WITHIN":
+            case "CONTAINS": {
+                ogc += this.processOGCGeometry(version, objFilter.spatialField.geometry);
 
                 if (objFilter.spatialField.operation === "DWITHIN") {
                     ogc += '<' + nsplaceholder + ':Distance units="m">' + (objFilter.spatialField.geometry.distance || 0) + '</' + nsplaceholder + ':Distance>';
@@ -555,12 +561,12 @@ const FilterUtils = {
         let gmlPoint = '<gml:Point srsDimension="2"';
 
         gmlPoint += srsName ? ' srsName="' + srsName + '">' : '>';
-
+        const normalizedCoords = coordinates.length && isArray(coordinates[0]) ? coordinates : [coordinates];
         // ///////////////////////////////////////////////////////////////////////////////////////////////////////
         // Array of LinearRing coordinate array. The first element in the array represents the exterior ring.
         // Any subsequent elements represent interior rings (or holes).
         // ///////////////////////////////////////////////////////////////////////////////////////////////////////
-        coordinates.forEach((element) => {
+        normalizedCoords.forEach((element) => {
             let coords = element.map((coordinate) => {
                 return coordinate[0] + " " + coordinate[1];
             });
