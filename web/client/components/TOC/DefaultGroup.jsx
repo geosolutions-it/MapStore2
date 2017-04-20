9f32047778eb33f3fa3195fbce0c2209b8b26fc1
Mauro Bartolomeoli <maurobartolomeoli@gmail.com>
2017-04-20 16:20:55 +0200

    Fixes #1752: TOC by level group className

--- web/client/components/TOC/DefaultGroup.jsx
+++ web/client/components/TOC/DefaultGroup.jsx
@@ -18,6 +18,7 @@ var DefaultGroup = React.createClass({
         style: React.PropTypes.object,
         sortableStyle: React.PropTypes.object,
         onToggle: React.PropTypes.func,
+        level: React.PropTypes.number,
         onSort: React.PropTypes.func,
         propertiesChangeHandler: React.PropTypes.func,
         groupVisibilityCheckbox: React.PropTypes.bool,
@@ -34,20 +35,21 @@ var DefaultGroup = React.createClass({
             sortableStyle: {},
             propertiesChangeHandler: () => {},
             groupVisibilityCheckbox: false,
-            visibilityCheckType: "glyph"
+            visibilityCheckType: "glyph",
+            level: 1
         };
     },
     render() {
         let {children, onToggle, ...other } = this.props;
         return (
-            <Node className="toc-default-group" sortableStyle={this.props.sortableStyle} style={this.props.style} type="group" {...other}>
+            <Node className={"toc-default-group toc-group-" + this.props.level} sortableStyle={this.props.sortableStyle} style={this.props.style} type="group" {...other}>
                 { this.props.groupVisibilityCheckbox &&
                   <VisibilityCheck
                             key="visibility"
                             checkType={this.props.visibilityCheckType}
                             propertiesChangeHandler={this.props.propertiesChangeHandler}/>}
                 <GroupTitle onClick={this.props.onToggle}/>
-                <GroupChildren onSort={this.props.onSort} position="collapsible">
+                <GroupChildren level={this.props.level + 1} onSort={this.props.onSort} position="collapsible">
                     {this.props.children}
                 </GroupChildren>
             </Node>
