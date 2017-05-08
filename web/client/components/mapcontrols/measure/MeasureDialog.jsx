45d3c5e49f6993c4ad718b0cd4ec46cae6fbf80d
Lorenzo Natali <lorenzo.natali@geo-solutions.it>
2017-05-08 12:32:54 +0200

    Fix #1776. Move measure into burger menu
    The dialog for measure is now in the burger menu. This fixes #1776.
    
    The old plugin will be kept in MeasurePanel + MeasureResult plugin and deprecated.
    All the duplicated functionalities are now merged in a unique component.

---
+++ web/client/components/mapcontrols/measure/MeasureDialog.jsx
@@ -0,0 +1,45 @@
+/*
+ * Copyright 2017, GeoSolutions Sas.
+ * All rights reserved.
+ *
+ * This source code is licensed under the BSD-style license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+const React = require('react');
+const {Glyphicon} = require('react-bootstrap');
+const MeasureComponent = require('./MeasureComponent');
+const Message = require('../../I18N/Message');
+const Dialog = require('../../misc/Dialog');
+
+
+const MeasureDialog = React.createClass({
+    propTypes: {
+        show: React.PropTypes.bool,
+        closeGlyph: React.PropTypes.string,
+        onClose: React.PropTypes.func
+    },
+    getDefaultProps() {
+        return {
+            show: false,
+            closeGlyph: "1-close"
+        };
+    },
+    onClose() {
+        this.props.onClose(false);
+    },
+    render() {
+        return this.props.show ? (<Dialog>
+            <div key="header" role="header">
+                <Glyphicon glyph="1-ruler"/>&nbsp;<Message key="title" msgId="measureComponent.Measure"/>
+                <button key="close" onClick={this.onClose} className="close">{this.props.closeGlyph ? <Glyphicon glyph={this.props.closeGlyph}/> : <span>×</span>}</button>
+            </div>
+            <div key="body" className="panel-body" role="body">
+            <MeasureComponent id="measure-panel" style={{
+                    minWidth: "500px"
+                }}{...this.props}/>
+            </div>
+        </Dialog>) : null;
+    }
+});
+module.exports = MeasureDialog;
---
+++ web/client/components/mapcontrols/measure/__tests__/MeasureDialog-test.jsx
@@ -0,0 +1,47 @@
+/**
+ * Copyright 2015, GeoSolutions Sas.
+ * All rights reserved.
+ *
+ * This source code is licensed under the BSD-style license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+var expect = require('expect');
+
+var React = require('react');
+var ReactDOM = require('react-dom');
+const ReactTestUtils = require('react-addons-test-utils');
+var MeasureDialog = require('../MeasureDialog');
+
+
+describe("test the MeasureComponent", () => {
+    beforeEach((done) => {
+        document.body.innerHTML = '<div id="container"></div>';
+        setTimeout(done);
+    });
+
+    afterEach((done) => {
+        ReactDOM.unmountComponentAtNode(document.getElementById("container"));
+        document.body.innerHTML = '';
+        setTimeout(done);
+    });
+
+    it('test component creation', () => {
+        let measurement = {};
+        const mc = ReactDOM.render(<MeasureDialog show={true} measurement={measurement}/>, document.getElementById("container"));
+        expect(mc).toExist();
+    });
+    it('test close', () => {
+        let measurement = {};
+        const handlers = {
+            onClose() {}
+        };
+        let spy = expect.spyOn(handlers, "onClose");
+        const mc = ReactDOM.render(<MeasureDialog show={true} onClose={handlers.onClose} measurement={measurement}/>, document.getElementById("container"));
+        expect(mc).toExist();
+        const dom = ReactDOM.findDOMNode(mc);
+        const closeBtn = dom.getElementsByClassName('close')[0];
+        expect(closeBtn).toExist();
+        ReactTestUtils.Simulate.click(closeBtn);
+        expect(spy.calls.length).toBe(1);
+    });
+});
