44ddae0fd313c642945ab5e8f848655d32aa151a
Mauro Bartolomeoli <maurobartolomeoli@gmail.com>
2017-09-06 10:56:14 +0200

    Fixes #2023: improvements to the footer toolbar tools

--- web/client/plugins/MousePosition.jsx
+++ web/client/plugins/MousePosition.jsx
@@ -9,6 +9,8 @@ const React = require('react');
 
 const {connect} = require('react-redux');
 const {mapSelector, projectionDefsSelector} = require('../selectors/map');
+const Message = require('../components/I18N/Message');
+const {Tooltip} = require('react-bootstrap');
 const {createSelector} = require('reselect');
 
 const assign = require('object-assign');
@@ -49,6 +51,8 @@ const selector = createSelector([
 const MousePositionButton = connect((state) => ({
     pressed: state.mousePosition && state.mousePosition.enabled,
     active: state.mousePosition && state.mousePosition.enabled,
+    tooltip: <Tooltip><Message msgId="showMousePositionCoordinates"/></Tooltip>,
+    tooltipPlace: 'left',
     pressedStyle: "success active",
     defaultStyle: "primary",
     glyphicon: "mouse",
