2c13fa3d043f37b8f55c240d2df9513d09dd2ed0
Matteo V <matteo.velludini.geosolutions@gmail.com>
2017-09-18 12:12:10 +0200

    Fixes #2182 "create new map" error on loading the map (#2188)

--- web/client/product/pages/MapViewer.jsx
+++ web/client/product/pages/MapViewer.jsx
@@ -1,27 +1,19 @@
-const PropTypes = require('prop-types');
-/**
- * Copyright 2016, GeoSolutions Sas.
+/*
+ * Copyright 2017, GeoSolutions Sas.
  * All rights reserved.
  *
  * This source code is licensed under the BSD-style license found in the
  * LICENSE file in the root directory of this source tree.
  */
 const React = require('react');
-
-require('../assets/css/viewer.css');
-
+const PropTypes = require('prop-types');
 const {connect} = require('react-redux');
-
 const url = require('url');
 const urlQuery = url.parse(window.location.href, true).query;
-
-const ConfigUtils = require('../../utils/ConfigUtils');
+const MapViewerCmp = require('../components/viewer/MapViewerCmp');
 const {loadMapConfig} = require('../../actions/config');
 const {initMap} = require('../../actions/map');
-
-const MapViewer = require('../../containers/MapViewer');
-
-let oldLocation;
+const MapViewerContainer = require('../../containers/MapViewer');
 
 class MapViewerPage extends React.Component {
     static propTypes = {
@@ -30,38 +22,21 @@ class MapViewerPage extends React.Component {
         loadMapConfig: PropTypes.func,
         onInit: PropTypes.func,
         plugins: PropTypes.object,
+        wrappedComponent: PropTypes.oneOfType([PropTypes.object, PropTypes.func]),
         location: PropTypes.object
     };
 
     static defaultProps = {
-        mode: 'desktop'
+        mode: 'desktop',
+        plugins: {},
+        match: {
+            params: {}
+        },
+        wrappedContainer: MapViewerContainer
     };
 
-    componentWillMount() {
-        if (this.props.match.params.mapId && oldLocation !== this.props.location) {
-            oldLocation = this.props.location;
-            if (!ConfigUtils.getDefaults().ignoreMobileCss) {
-                if (this.props.mode === 'mobile') {
-                    require('../assets/css/mobile.css');
-                }
-            }
-
-            // VMap = require('../components/viewer/Map')(this.props.params.mapType);
-
-            // TODO let's put this into a utils since it will be needed in login section
-            let mapId = this.props.match.params.mapId === '0' ? null : this.props.match.params.mapId;
-            let config = urlQuery && urlQuery.config || null;
-            const {configUrl} = ConfigUtils.getConfigUrl({mapId, config});
-            this.props.onInit();
-            this.props.loadMapConfig(configUrl, mapId);
-        }
-    }
-
     render() {
-        return (<MapViewer
-            plugins={this.props.plugins}
-            params={this.props.match.params}
-            />);
+        return (<MapViewerCmp {...this.props} />);
     }
 }
 
