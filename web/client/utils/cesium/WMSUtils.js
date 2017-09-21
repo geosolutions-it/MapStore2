81827143e78e4edc0547dbf85f2612d79a38d8a9
kappu <kappu72gmail.com>
2017-09-21 10:57:28 +0200

    Fixed cesium

--- web/client/utils/cesium/WMSUtils.js
+++ web/client/utils/cesium/WMSUtils.js
@@ -7,7 +7,7 @@
  */
 
 const WMSUtils = {
-    PARAM_OPTIONS: ["layers", "styles", "style", "format", "transparent", "version", "tiled", "opacity", "zindex", "srs", "singletile", "_v_", "filterObj" ]
+    PARAM_OPTIONS: ["layers", "styles", "style", "format", "transparent", "version", "tiled", "opacity", "zindex", "srs", "singletile", "_v_", "filterobj" ]
 };
 
 module.exports = WMSUtils;
