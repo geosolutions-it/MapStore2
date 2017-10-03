ad3d001ea012f7f51cd34a30b799c56f9a96beaf
mbarto <maurobartolomeoli@gmail.com>
2017-10-03 14:35:10 +0200

    Fixes #2229: supported dimensions with no default values in WMS getcapabilities parsing (#2257)

---
+++ web/client/utils/__tests__/CatalogUtils-test.js
@@ -0,0 +1,123 @@
+/*
+ * Copyright 2017, GeoSolutions Sas.
+ * All rights reserved.
+ *
+ * This source code is licensed under the BSD-style license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+const expect = require('expect');
+
+const CatalogUtils = require('../CatalogUtils');
+
+describe('Test the CatalogUtils', () => {
+    it('wms dimensions without values', () => {
+        const records = CatalogUtils.getCatalogRecords('wms', {
+            records: [{
+                Dimension: [{
+                    $: {
+                        name: 'time'
+                    }
+                }]
+            }]
+        }, {});
+        expect(records.length).toBe(1);
+        expect(records[0].dimensions.length).toBe(1);
+        expect(records[0].dimensions[0].values.length).toBe(0);
+    });
+
+    it('wms dimensions with values', () => {
+        const records = CatalogUtils.getCatalogRecords('wms', {
+            records: [{
+                Dimension: [{
+                    $: {
+                        name: 'time'
+                    },
+                    _: '1,2'
+                }]
+            }]
+        }, {});
+        expect(records.length).toBe(1);
+        expect(records[0].dimensions.length).toBe(1);
+        expect(records[0].dimensions[0].values.length).toBe(2);
+    });
+
+    it('wmts', () => {
+        const records = CatalogUtils.getCatalogRecords('wmts', {
+            records: [{}]
+        }, {});
+        expect(records.length).toBe(1);
+    });
+
+    it('wmts with tilematrix', () => {
+        const records = CatalogUtils.getCatalogRecords('wmts', {
+            records: [{
+                "ows:WGS84BoundingBox": {
+                    "ows:LowerCorner": "-180.0 -90.0",
+                    "ows:UpperCorner": "180.0 90.0"
+                },
+                TileMatrixSetLink: [{
+                    TileMatrixSet: 'EPSG:4326',
+                    TileMatrixSetLimits: {
+                        TileMatrixSetLimits: [{
+                            TileMatrix: 'EPSG:4326:0',
+                            MinTileCol: 0,
+                            MaxTileCol: 10,
+                            MinTileRow: 0,
+                            MaxTileRow: 10
+                        }]
+                    }
+                }],
+                TileMatrixSet: [{
+                    "ows:Identifier": "EPSG:4326",
+                    "ows:SupportedCRS": "EPSG:4326"
+                }]
+            }]
+        }, {});
+        expect(records.length).toBe(1);
+
+    });
+
+    it('csw empty', () => {
+        const records = CatalogUtils.getCatalogRecords('csw', {
+            records: [{}]
+        }, {});
+        expect(records.length).toBe(1);
+    });
+
+    it('csw with DC URI', () => {
+        const records = CatalogUtils.getCatalogRecords('csw', {
+            records: [{
+                dc: {
+                    URI: [{
+                        name: "thumbnail",
+                        value: "http://thumb"
+                    }, {
+                        name: "wms",
+                        protocol: "OGC:WMS-1.1.1-http-get-map",
+                        value: "http://geoserver"
+                    }]
+                }
+            }]
+        }, {});
+        expect(records.length).toBe(1);
+    });
+
+    it('csw with DC references', () => {
+        const records = CatalogUtils.getCatalogRecords('csw', {
+            records: [{
+                dc: {
+                    references: [{
+                        name: "thumbnail",
+                        scheme: "WWW:LINK-1.0-http--image-thumbnail",
+                        value: "http://thumb"
+                    }, {
+                        name: "wms",
+                        scheme: "OGC:WMS-1.1.1-http-get-map",
+                        value: "http://geoserver"
+                    }]
+                }
+            }]
+        }, {});
+        expect(records.length).toBe(1);
+    });
+});
