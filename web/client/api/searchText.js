60ca119883e3d4a0dca67a4fc8a320a5de547e10
Matteo V <matteo.velludini.geosolutions@gmail.com>
2017-10-09 12:20:11 +0200

    Improvements for pluggable data editor (#2269)

--- web/client/api/searchText.js
+++ web/client/api/searchText.js
@@ -10,33 +10,36 @@ const assign = require('object-assign');
 const GeoCodeUtils = require('../utils/GeoCodeUtils');
 const {generateTemplateString} = require('../utils/TemplateUtils');
 
+const defaultFromTextToFilter = ({searchText, staticFilter, blacklist, item, queriableAttributes, predicate} ) => {
+    // split into words and remove blacklisted words
+    const staticFilterParsed = generateTemplateString(staticFilter || "")(item);
+    let searchWords = searchText.split(" ").filter(w => w).filter( w => blacklist.indexOf(w.toLowerCase()) < 0 );
+
+    // if the array searchWords is empty, then use the full searchText
+    if (searchWords.length === 0 ) {
+        searchWords = !!searchText ? [searchText] : [];
+    }
+    let filter;
+    if (searchWords.length > 0 ) {
+        filter = "(".concat( searchWords.map( (w) => queriableAttributes.map( attr => `${attr} ${predicate} '%${w.replace("'", "''")}%'`).join(" OR ")).join(') AND (')).concat(")");
+    }
+
+    filter = filter ? filter.concat(staticFilterParsed) : staticFilterParsed || null;
+    return filter;
+};
 let Services = {
     nominatim: (searchText, options = {}) =>
         require('./Nominatim')
         .geocode(searchText, options)
         .then( res => GeoCodeUtils.nominatimToGeoJson(res.data)),
-    wfs: (searchText, {url, typeName, queriableAttributes = [], outputFormat = "application/json", predicate = "ILIKE", staticFilter = "", blacklist = [], item, ...params }) => {
-        // split into words and remove blacklisted words
-        const staticFilterParsed = generateTemplateString(staticFilter || "")(item);
-        let searchWords = searchText.split(" ").filter(w => w).filter( w => blacklist.indexOf(w.toLowerCase()) < 0 );
-
-        // if the array searchWords is empty, then use the full searchText
-        if (searchWords.length === 0 ) {
-            searchWords = !!searchText ? [searchText] : [];
-        }
-        let filter;
-        if (searchWords.length > 0 ) {
-            filter = "(".concat( searchWords.map( (w) => queriableAttributes.map( attr => `${attr} ${predicate} '%${w.replace("'", "''")}%'`).join(" OR ")).join(') AND (')).concat(")");
-        }
-
-        filter = filter ? filter.concat(staticFilterParsed) : staticFilterParsed || null;
-
+    wfs: (searchText, {url, typeName, queriableAttributes = [], outputFormat = "application/json", predicate = "ILIKE", staticFilter = "", blacklist = [], item, fromTextToFilter = defaultFromTextToFilter, ...params }) => {
+        const filter = fromTextToFilter({searchText, staticFilter, blacklist, item, queriableAttributes, predicate});
         return WFS
             .getFeatureSimple(url, assign({
                 maxFeatures: 10,
                 typeName,
                 outputFormat,
-                // create a filter like : `(ATTR ilike  '%word1%') AND (ATTR ilike '%word2%')`
+                // create a filter like : `(ATTR ilike '%word1%') AND (ATTR ilike '%word2%')`
                 cql_filter: filter
             }, params))
             .then( response => response.features );
