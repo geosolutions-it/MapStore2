57af8bd1052f3a5be985c0a4b7b1214cddeb0a55
Lorenzo Natali <lorenzo.natali@geo-solutions.it>
2017-10-12 15:04:47 +0200

    updated file headers

--- web/client/components/misc/enhancers/loadingState.jsx
+++ web/client/components/misc/enhancers/loadingState.jsx
@@ -5,6 +5,7 @@
  * This source code is licensed under the BSD-style license found in the
  * LICENSE file in the root directory of this source tree.
  */
+
 const React = require('react');
 const {branch} = require('recompose');
 const ContainerDimensions = require('react-container-dimensions').default;
