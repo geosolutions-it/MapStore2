0dbfb074ee1dfccd475b3b954090aa108dbc4de3
Lorenzo Natali <lorenzo.natali@geo-solutions.it>
2017-11-17 16:53:49 +0100

    fixed mapinfo selector in toc

--- web/client/plugins/TOC.jsx
+++ web/client/plugins/TOC.jsx
@@ -20,7 +20,7 @@ const {groupsSelector, layersSelector, selectedNodesSelector, layerFilterSelecto
 const {mapSelector, mapNameSelector} = require('../selectors/map');
 const {currentLocaleSelector} = require("../selectors/locale");
 const {widgetBuilderAvailable} = require('../selectors/controls');
-const {generalInfoFormatSelector} = require("../selectors/mapInfo");
+const {generalInfoFormatSelector} = require("../selectors/mapinfo");
 
 const LayersUtils = require('../utils/LayersUtils');
 const mapUtils = require('../utils/MapUtils');
