b2f4c5be3a76b1a5565cd7602244556cc16fdd44
stefano bovio <stefano.bovio@geo-solutions.it>
2017-12-07 12:22:01 +0100

    Added tooltip on layer/group title (#2456)

--- web/client/components/TOC/fragments/GroupTitle.jsx
+++ web/client/components/TOC/fragments/GroupTitle.jsx
@@ -10,6 +10,8 @@ const React = require('react');
 const PropTypes = require('prop-types');
 const StatusIcon = require('./StatusIcon');
 const {isObject} = require('lodash');
+const {Tooltip} = require('react-bootstrap');
+const OverlayTrigger = require('../../misc/OverlayTrigger');
 
 class GroupTitle extends React.Component {
     static propTypes = {
@@ -17,7 +19,8 @@ class GroupTitle extends React.Component {
         onClick: PropTypes.func,
         onSelect: PropTypes.func,
         style: PropTypes.object,
-        currentLocale: PropTypes.string
+        currentLocale: PropTypes.string,
+        tooltip: PropTypes.bool
     };
 
     static inheritedPropTypes = ['node'];
@@ -28,13 +31,21 @@ class GroupTitle extends React.Component {
         currentLocale: 'en-US',
         style: {
 
-        }
+        },
+        tooltip: false
     };
 
     render() {
         let expanded = this.props.node.expanded !== undefined ? this.props.node.expanded : true;
         const groupTitle = isObject(this.props.node.title) ? this.props.node.title[this.props.currentLocale] || this.props.node.title.default || this.props.node.name : this.props.node.title || this.props.node.name;
-        return (
+        return this.props.tooltip ? (
+            <OverlayTrigger placement="top" overlay={(<Tooltip id={"tooltip-layer-group"}>{groupTitle}</Tooltip>)}>
+                <div style={this.props.style}>
+                    <span className="toc-group-title" onClick={ this.props.onSelect ? (e) => this.props.onSelect(this.props.node.id, 'group', e.ctrlKey) : () => {}}>{groupTitle}</span><StatusIcon onClick={() => this.props.onClick(this.props.node.id, expanded)} expanded={expanded} node={this.props.node}/>
+                </div>
+            </OverlayTrigger>
+        ) :
+        (
             <div style={this.props.style}>
                 <span className="toc-group-title" onClick={ this.props.onSelect ? (e) => this.props.onSelect(this.props.node.id, 'group', e.ctrlKey) : () => {}}>{groupTitle}</span><StatusIcon onClick={() => this.props.onClick(this.props.node.id, expanded)} expanded={expanded} node={this.props.node}/>
             </div>
