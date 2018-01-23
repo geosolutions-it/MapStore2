3eff8fee76ff78f1c09bc1a7801b2c345220c4b4
stefano bovio <stefano.bovio@geo-solutions.it>
2018-01-23 16:16:10 +0100

    Fixes #2541 Theme Switcher fixed (#2548)

--- web/client/components/app/StandardRouter.jsx
+++ web/client/components/app/StandardRouter.jsx
@@ -72,8 +72,7 @@ class StandardRouter extends React.Component {
                         </ConnectedRouter>
                     </Localized>) :
                         (<span><div className="_ms2_init_spinner _ms2_init_center"><div></div></div>
-                        <div className="_ms2_init_text _ms2_init_center">Loading</div></span>
-                    )}
+                        <div className="_ms2_init_text _ms2_init_center">Loading MapStore</div></span>)}
                 </Theme>
                 <Debug/>
             </div>
