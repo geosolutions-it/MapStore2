5f160363f60064135fb9bf1fabf4d7c45833456a
kappu <kappu72gmail.com>
2018-02-21 17:25:21 +0100

    Closes geosolutions-it/austrocontrol-C125#26

--- web/client/utils/FileFormatUtils.js
+++ web/client/utils/FileFormatUtils.js
@@ -3,8 +3,8 @@ const formats = [{
         outputFormat: "shape-zip",
         extension: "zip"
     }, {
-            outputFormat: "csv",
-            extension: "csv"
+        outputFormat: "csv",
+        extension: "csv"
     }, {
         outputFormat: "excel",
         extension: "xls"
@@ -17,6 +17,48 @@ const formats = [{
     }, {
         outputFormat: "dxf-zip",
         extension: "zip"
+    }, {
+        outputFormat: "application/vnd.google-earth.kml+xml",
+        extension: "kml"
+    }, {
+        outputFormat: "application/json",
+        extension: "json"
+    }, {
+        outputFormat: "gml3",
+        extension: "gml"
+    }, {
+        outputFormat: "GML2",
+        extension: "gml"
+    }, {
+        outputFormat: "application/vnd.googlxml",
+        extension: "kml"
+    }, {
+        outputFormat: "OGR-CSV",
+        extension: "csv"
+    }, {
+        outputFormat: "OGR-FileGDB",
+        extension: "gdb"
+    }, {
+        outputFormat: "OGR-GPKG",
+        extension: "gpkg"
+    }, {
+        outputFormat: "OGR-KML",
+        extension: "kml"
+    }, {
+        outputFormat: "OGR-MIF",
+        extension: "mif"
+    }, {
+        outputFormat: "OGR-TAB",
+        extension: "tab"
+    }, {
+        outputFormat: "SHAPE-ZIP",
+        extension: "zip"
+    }, {
+        outputFormat: "gml32",
+        extension: "gml"
+    }, {
+        outputFormat: "application/x-gpk",
+        extension: "gpk"
     }
 ];
 
