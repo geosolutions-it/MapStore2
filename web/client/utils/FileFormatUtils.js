65499017c8630e3a4e9411654c80177923415982
Andrea <kappu72@gmail.com>
2018-02-27 14:08:00 +0100

    Closes geosolutions-it/austrocontrol-C125#26 (#2632)
    * Closes geosolutions-it/austrocontrol-C125#26
    
    * Fixes on Mauro's comment
    
    * Fixed split search pattern

--- web/client/utils/FileFormatUtils.js
+++ web/client/utils/FileFormatUtils.js
@@ -1,10 +1,16 @@
 const {head} = require('lodash');
+
+const getFormatByName = (outF) => {
+    const extension = outF.split(/[^\w]/)[1];
+    return extension ? {outputFormat: outF, extension: extension.toLowerCase()} : undefined;
+
+};
 const formats = [{
         outputFormat: "shape-zip",
         extension: "zip"
     }, {
-            outputFormat: "csv",
-            extension: "csv"
+        outputFormat: "csv",
+        extension: "csv"
     }, {
         outputFormat: "excel",
         extension: "xls"
@@ -17,10 +23,52 @@ const formats = [{
     }, {
         outputFormat: "dxf-zip",
         extension: "zip"
+    }, {
+        outputFormat: "application/vnd.google-earth.kml+xml",
+        extension: "kml"
+    }, {
+        outputFormat: "application/json",
+        extension: "json"
+    }, {
+        outputFormat: "gml3",
+        extension: "gml"
+    }, {
+        outputFormat: "GML2",
+        extension: "gml"
+    }, {
+        outputFormat: "application/vnd.googlxml",
+        extension: "kml"
+    }, {
+        outputFormat: "OGR-CSV",
+        extension: "csv"
+    }, {
+        outputFormat: "OGR-FileGDB",
+        extension: "gdb"
+    }, {
+        outputFormat: "OGR-GPKG",
+        extension: "gpkg"
+    }, {
+        outputFormat: "OGR-KML",
+        extension: "kml"
+    }, {
+        outputFormat: "OGR-MIF",
+        extension: "mif"
+    }, {
+        outputFormat: "OGR-TAB",
+        extension: "tab"
+    }, {
+        outputFormat: "SHAPE-ZIP",
+        extension: "zip"
+    }, {
+        outputFormat: "gml32",
+        extension: "gml"
+    }, {
+        outputFormat: "application/x-gpk",
+        extension: "gpk"
     }
 ];
 
 module.exports = {
     formats,
-    getByOutputFormat: (outF) => head(formats.filter(format => format.outputFormat === outF))
+    getByOutputFormat: (outF) => head(formats.filter(format => format.outputFormat === outF)) || getFormatByName(outF)
 };
