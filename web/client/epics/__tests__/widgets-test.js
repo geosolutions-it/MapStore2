406480356423bbf751b795a3e9e68ee6f095382d
Lorenzo Natali <offtherailz@gmail.com>
2018-02-28 14:04:41 +0100

    Fix #2615. Avoid widgets clear while saving (#2649)

---
+++ web/client/epics/__tests__/widgets-test.js
@@ -0,0 +1,129 @@
+/*
+ * Copyright 2018, GeoSolutions Sas.
+ * All rights reserved.
+ *
+ * This source code is licensed under the BSD-style license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+var expect = require('expect');
+const { testEpic, addTimeoutEpic, TEST_TIMEOUT } = require('./epicTestUtils');
+
+const {
+    clearWidgetsOnLocationChange
+} = require('../widgets');
+const {
+    CLEAR_WIDGETS
+} = require('../../actions/widgets');
+const {
+    savingMap,
+    mapCreated
+} = require('../../actions/maps');
+const {
+    configureMap
+} = require('../../actions/config');
+const { LOCATION_CHANGE } = require('react-router-redux');
+
+describe('widgets Epics', () => {
+    it('clearWidgetsOnLocationChange triggers CLEAR_WIDGETS on LOCATION_CHANGE', (done) => {
+        const checkActions = actions => {
+            expect(actions.length).toBe(1);
+            const action = actions[0];
+            expect(action.type).toBe(CLEAR_WIDGETS);
+            done();
+        };
+        let count = 0;
+        testEpic(clearWidgetsOnLocationChange,
+            1,
+            [configureMap(), { type: LOCATION_CHANGE, payload: {
+                pathname: "newPath"
+            }}],
+            checkActions,
+            () => {
+                return count++
+                    ? {
+                        routing: {
+                            location: "new"
+                        }
+                    }
+                    : {
+                        routing: {
+                            location: "old"
+                        }
+                    };
+            });
+    });
+    it('clearWidgetsOnLocationChange stops CLEAR_WIDGETS triggers if saving', (done) => {
+        const checkActions = actions => {
+            expect(actions.length).toBe(1);
+            const action = actions[0];
+            expect(action.type).toBe(TEST_TIMEOUT);
+            done();
+        };
+        let count = 0;
+        testEpic(addTimeoutEpic(clearWidgetsOnLocationChange, 20),
+            1,
+            [
+                configureMap(),
+                savingMap(),
+                {
+                    type: LOCATION_CHANGE,
+                    payload: {
+                        pathname: "newPath"
+                    }
+                }
+            ],
+            checkActions,
+            () => {
+                return count++
+                    ? {
+                        routing: {
+                            location: "new"
+                        }
+                    }
+                    : {
+                        routing: {
+                            location: "old"
+                        }
+                    };
+            });
+    });
+    it('clearWidgetsOnLocationChange restores CLEAR_WIDGETS triggers after save completed', (done) => {
+        const checkActions = actions => {
+            expect(actions.length).toBe(1);
+            const action = actions[0];
+            expect(action.type).toBe(CLEAR_WIDGETS);
+            done();
+        };
+        let count = 0;
+        testEpic(clearWidgetsOnLocationChange,
+            1,
+            [configureMap(),
+            savingMap(),
+            {
+                type: LOCATION_CHANGE,
+                payload: {
+                    pathname: "newPath"
+                }
+            },
+            mapCreated(),
+            {
+                type: LOCATION_CHANGE, payload: {
+                    pathname: "newPath"
+                }
+            }],
+            checkActions,
+            () => {
+                return count++
+                    ? {
+                        routing: {
+                            location: "new"
+                        }
+                    }
+                    : {
+                        routing: {
+                            location: "old"
+                        }
+                    };
+            });
+    });
+});
