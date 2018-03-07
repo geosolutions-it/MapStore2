12c9405115768a81b9e17531f3953758f996f0c6
Lorenzo Natali <lorenzo.natali@geo-solutions.it>
2018-03-07 18:44:55 +0100

    add partial support for cesium

---
+++ web/client/components/map/plugins/cesium.js
@@ -0,0 +1,15 @@
+/**
+* Copyright 2016, GeoSolutions Sas.
+* All rights reserved.
+*
+* This source code is licensed under the BSD-style license found in the
+* LICENSE file in the root directory of this source tree.
+*/
+
+module.exports = () => {
+    require('../cesium/plugins/index');
+    return {
+        Map: require('../cesium/Map'),
+        Layer: require('../cesium/Layer')
+    };
+};
