7232bd5117bcc87c43a10a308c5d1bc6e532041d
amusso01 <eandram@hotmail.it>
2018-03-13 12:53:46 +0100

    coding style correction

--- web/client/components/misc/__tests__/Dialog-test.jsx
+++ web/client/components/misc/__tests__/Dialog-test.jsx
@@ -12,18 +12,18 @@ const Dialog = require('../Dialog');
 
 describe("Dialog component", () => {
     beforeEach((done) => {
-       document.body.innerHTML = '<div id="container"></div>';
-       setTimeout(done);
+        document.body.innerHTML = '<div id="container"></div>';
+        setTimeout(done);
     });
-    afterEach ((done ) => {
-       ReactDOM.unmountComponentAtNode(document.getElementById("container"));
-       document.body.innerHTML = '';
-       setTimeout(done);
+    afterEach((done ) => {
+        ReactDOM.unmountComponentAtNode(document.getElementById("container"));
+        document.body.innerHTML = '';
+        setTimeout(done);
     });
     it('Dialog loading process for local vector', () => {
         ReactDOM.render(<Dialog id={"mapstore-shapefile-upload"} />, document.getElementById("container"));
         const container = document.getElementById("container");
-        const loader = container.querySelector('.sk-circle-wrapper .sk-fade-in')
+        const loader = container.querySelector('.sk-circle-wrapper .sk-fade-in');
         expect(loader).toExist;
     });
 });
