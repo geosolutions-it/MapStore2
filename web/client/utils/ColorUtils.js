6276150974f45825415c60e03362efa007d87c24
Andrea <kappu72@gmail.com>
2018-03-19 10:29:12 +0100

    C125 annotations (#2751)
    * Fix issue on editing style in vector import
    
    * Fixed style of loaded vector layers
    
    * fixes on Matteo's comments
    
    * Fixed lint error

--- web/client/utils/ColorUtils.js
+++ web/client/utils/ColorUtils.js
@@ -5,7 +5,8 @@
   * This source code is licensed under the BSD-style license found in the
   * LICENSE file in the root directory of this source tree.
   */
-
+const tinycolor = require("tinycolor2");
+const {toNumber} = require("lodash");
 /**
  * Porting of various MapStore(1) utilities for random/color scale generations
  * @name ColorUtils
@@ -206,6 +207,17 @@ const ColorUtils = {
             g: rgb[1],
             b: rgb[2]
         };
+    },
+    /**
+    * convert any valid css color to rgba str
+    * @param (String) color any valid css color
+    * @param (number) opacity 0 - 1 alpha value
+    * @return (String) rgba string
+    */
+    colorToRgbaStr: (color = "#0000FF", alpha) => {
+        const c = tinycolor(color);
+        return c.setAlpha(toNumber(alpha !== undefined ? alpha : c.getAlpha())).toRgbString();
     }
+
 };
 module.exports = ColorUtils;
