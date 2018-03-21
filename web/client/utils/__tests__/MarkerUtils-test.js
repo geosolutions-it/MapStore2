f4030726ccc6e23ce2ee5b4c5a6fed7a9230b9d7
kappu <kappu72gmail.com>
2018-03-21 13:14:15 +0100

    Removed unused var

--- web/client/utils/__tests__/MarkerUtils-test.js
+++ web/client/utils/__tests__/MarkerUtils-test.js
@@ -6,7 +6,7 @@
  * LICENSE file in the root directory of this source tree.
  */
 const expect = require('expect');
-const marker = require('../../test-resources/markerIcon.png');
+
 
 const {
     extraMarkers,
