5adff12f3ab4112259b752b9d6b8406c8461f151
Lorenzo Natali <lorenzo.natali@geo-solutions.it>
2018-04-05 15:33:05 +0200

    Avoid animation of chart type selector

--- web/client/components/widgets/builder/wizard/chart/ChartType.jsx
+++ web/client/components/widgets/builder/wizard/chart/ChartType.jsx
@@ -8,6 +8,7 @@
 
 
 const React = require('react');
+const { shouldUpdate } = require('recompose');
 const SideGrid = require('../../../../misc/cardgrids/SideGrid');
 const Message = require('../../../../I18N/Message');
 const sampleData = require('../../../enhancers/sampleChartData');
@@ -38,7 +39,9 @@ const ITEMS = [{
     description: <Message msgId={`widgets.chartType.${type}.description`} />,
     caption: <Message msgId={`widgets.chartType.${type}.caption`} />
 }));
-module.exports = ({ onSelect = () => { }, onNextPage = () => { }, types = ITEMS, type} = {}) => (<Row>
+module.exports = shouldUpdate(
+    ({ types, type }, { types: nextTypes, type: nextType}) => type !== nextType && types !== nextTypes
+)(({ onSelect = () => { }, onNextPage = () => { }, types = ITEMS, type} = {}) => (<Row>
     <StepHeader key="title" title={<Message msgId="widgets.selectChartType.title" />} />
     <SideGrid
         key="content"
@@ -59,4 +62,4 @@ module.exports = ({ onSelect = () => { }, onNextPage = () => { }, types = ITEMS,
                      />)
             }))} />
 </Row>
-    );
+    ));
