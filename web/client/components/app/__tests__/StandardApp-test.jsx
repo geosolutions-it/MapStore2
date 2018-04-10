0ae1e3ad1f46ad2c9f4f29d3840bccbb34e3d2f9
Lorenzo Natali <offtherailz@gmail.com>
2018-04-10 11:14:46 +0200

    Fixed #2809. Moved utility function in the proper place (#2810)

--- web/client/components/app/__tests__/StandardApp-test.jsx
+++ web/client/components/app/__tests__/StandardApp-test.jsx
@@ -87,6 +87,7 @@ describe('StandardApp', () => {
     it('creates a default app and reads initialState from localConfig', (done) => {
         const store = (plugins, storeOpts) => {
             expect(storeOpts.initialState.defaultState.test).toExist();
+            expect(storeOpts.initialState.defaultState.testMode).toBe('EXPRESSION_MODE_desktop');
             done();
             return {
                 dispatch() {
@@ -105,6 +106,29 @@ describe('StandardApp', () => {
         const app = ReactDOM.render(<StandardApp appStore={store} storeOpts={storeOpts}/>, document.getElementById("container"));
         expect(app).toExist();
     });
+    it('creates a default app and reads initialState with mode', (done) => {
+        const store = (plugins, storeOpts) => {
+            expect(storeOpts.initialState.defaultState.testMode).toBe('EXPRESSION_MODE_TEST');
+            done();
+            return {
+                dispatch() {
+                }
+            };
+        };
+
+        const storeOpts = {
+            initialState: {
+                defaultState: {
+                    test: "NOTHING"
+
+                },
+                mobile: {}
+            }
+        };
+        const app = ReactDOM.render(<StandardApp mode={'TEST'} appStore={store} storeOpts={storeOpts} />, document.getElementById("container"));
+        expect(app).toExist();
+    });
+
 
     it('creates a default app and renders the given component', () => {
         const store = () => ({
