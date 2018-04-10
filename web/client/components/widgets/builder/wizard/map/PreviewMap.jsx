6a23c8fd2ab87d48b21102a7a0b72ce120cc6b39
Lorenzo Natali <lorenzo.natali@geo-solutions.it>
2018-04-10 17:52:16 +0200

    Fix #2794. Update mapStateSource on preview change

--- web/client/components/widgets/builder/wizard/map/PreviewMap.jsx
+++ web/client/components/widgets/builder/wizard/map/PreviewMap.jsx
@@ -11,6 +11,9 @@ const { compose, withHandlers} = require('recompose');
 
 module.exports = compose(
     withHandlers({
-        onMapViewChanges: ({onChange = () => {}}) => map => onChange('map', map)
+        onMapViewChanges: ({onChange = () => {}}) => map => {
+            onChange('map', map);
+            onChange('mapStateSource', map.mapStateSource);
+        }
     })
 )(require('../../../widget/MapView'));
