44c2bb55a4bd1b8d1c96f60dc6df24581c9ec6c4
kappu <kappu72gmail.com>
2018-04-14 19:43:07 +0200

    Fixed tests

--- web/client/components/manager/rulesmanager/rulesgrid/enhancers/__tests__/triggerFetch-test.js
+++ web/client/components/manager/rulesmanager/rulesgrid/enhancers/__tests__/triggerFetch-test.js
@@ -9,16 +9,30 @@
 const expect = require('expect');
 const triggerFetch = require('../triggerFetch');
 const Rx = require("rxjs");
-const ConfigUtils = require("../../../../../../utils/ConfigUtils");
-ConfigUtils.setConfigProp("geoFenceUrl", "base/web/client/test-resources/");
+const axios = require('../../../../../../libs/ajax');
+const triggerInterceptors = (config) => {
+    if (config.url.indexOf("geofence/rest/rules/count") !== -1) {
+        config.url = "base/web/client/test-resources/geofence/rest/rules/count";
+    }
+    return config;
+};
+
 describe('rulegrid triggerFetch', () => {
     it('get count', (done) => {
+        const inter = axios.interceptors.request.use(triggerInterceptors);
         const onLoad = ({pages, rowsCount}) => {
             expect(pages).toEqual({});
             expect(rowsCount).toBe(10);
             done();
         };
-        const prop$ = Rx.Observable.of({version: 0, filters: {}, setLoading: () => {}, onLoad, onLoadError: () => { }});
-        triggerFetch(prop$).subscribe(() => {});
+        const onLoadError = () => {};
+        const prop$ = Rx.Observable.of({version: 0, filters: {}, setLoading: () => {}, onLoad, onLoadError});
+        triggerFetch(prop$).subscribe({
+            next: () => {},
+            error: () => {},
+            complete: () => {
+                axios.interceptors.request.eject(inter);
+            }
+          });
     });
 });
