d1acf512daf646196c446c8ab811e6849fc8601d
Lorenzo Natali <lorenzo.natali@geo-solutions.it>
2018-05-11 17:58:10 +0200

    fix #2888 switch to correct 2d mode

--- web/client/epics/__tests__/globeswitcher-test.js
+++ web/client/epics/__tests__/globeswitcher-test.js
@@ -9,22 +9,23 @@
 var expect = require('expect');
 
 const {toggle3d, UPDATE_LAST_2D_MAPTYPE} = require('../../actions/globeswitcher');
+const { localConfigLoaded } = require('../../actions/localConfig');
+
 const assign = require('object-assign');
 
-const {updateRouteOn3dSwitch} = require('../globeswitcher');
+
+const { updateRouteOn3dSwitch, updateLast2dMapTypeOnChangeEvents} = require('../globeswitcher');
+
 const {testEpic} = require('./epicTestUtils');
 describe('globeswitcher Epics', () => {
-    it('updates maptype', (done) => {
-        testEpic(updateRouteOn3dSwitch, 2, assign({hash: "/viewer/leaflet/2"}, toggle3d(true, "leaflet")), actions => {
-            expect(actions.length).toBe(2);
+    it('updates maptype toggling to 3D', (done) => {
+        testEpic(updateRouteOn3dSwitch, 1, assign({hash: "/viewer/leaflet/2"}, toggle3d(true, "leaflet")), actions => {
+            expect(actions.length).toBe(1);
             actions.map((action) => {
                 switch (action.type) {
                 case "@@router/CALL_HISTORY_METHOD":
                     expect(action.payload.method).toBe('push');
                     expect(action.payload.args.length).toBe(1);
-                    break;
-                case UPDATE_LAST_2D_MAPTYPE:
-                    expect(action.mapType).toBe("leaflet");
                     break;
                 default:
                     expect(true).toBe(false);
@@ -35,18 +36,14 @@ describe('globeswitcher Epics', () => {
         });
 
     });
-
-    it('updates maptype on newmap', (done) => {
-        testEpic(updateRouteOn3dSwitch, 2, assign({hash: "/viewer/leaflet/new"}, toggle3d(true, "leaflet")), actions => {
-            expect(actions.length).toBe(2);
+    it('updates maptype toggling from 3D', (done) => {
+        testEpic(updateRouteOn3dSwitch, 1, assign({ hash: "/viewer/cesium/2" }, toggle3d(true, "leaflet")), actions => {
+            expect(actions.length).toBe(1);
             actions.map((action) => {
                 switch (action.type) {
                     case "@@router/CALL_HISTORY_METHOD":
                         expect(action.payload.method).toBe('push');
                         expect(action.payload.args.length).toBe(1);
-                        break;
-                    case UPDATE_LAST_2D_MAPTYPE:
-                        expect(action.mapType).toBe("leaflet");
                         break;
                     default:
                         expect(true).toBe(false);
@@ -57,4 +54,62 @@ describe('globeswitcher Epics', () => {
         });
 
     });
+
+    it('updates maptype on newmap', (done) => {
+        testEpic(updateRouteOn3dSwitch, 1, assign({hash: "/viewer/leaflet/new"}, toggle3d(true, "leaflet")), actions => {
+            expect(actions.length).toBe(1);
+            actions.map((action) => {
+                switch (action.type) {
+                    case "@@router/CALL_HISTORY_METHOD":
+                        expect(action.payload.method).toBe('push');
+                        expect(action.payload.args.length).toBe(1);
+                        break;
+                    default:
+                        expect(true).toBe(false);
+
+                }
+            });
+            done();
+        });
+
+    });
+    it('updateLast2dMapTypeOnChangeEvents', (done) => {
+        testEpic(updateLast2dMapTypeOnChangeEvents, 1, [localConfigLoaded(), assign({ hash: "/viewer/leaflet/new" }, toggle3d(true))], actions => {
+            expect(actions.length).toBe(1);
+            actions.map((action) => {
+                switch (action.type) {
+                    case UPDATE_LAST_2D_MAPTYPE:
+                        expect(action.mapType).toBe('leaflet');
+                        break;
+                    default:
+                        expect(true).toBe(false);
+
+                }
+            });
+            done();
+        });
+
+    });
+    it('updateLast2dMapTypeOnChangeEvents with custom mapType', (done) => {
+        testEpic(updateLast2dMapTypeOnChangeEvents, 1, [localConfigLoaded(), assign({ hash: "/viewer/leaflet/new" }, toggle3d(true))], actions => {
+            expect(actions.length).toBe(1);
+            actions.map((action) => {
+                switch (action.type) {
+                    case UPDATE_LAST_2D_MAPTYPE:
+                        expect(action.mapType).toBe('openlayers');
+                        break;
+                    default:
+                        expect(true).toBe(false);
+
+                }
+            });
+            done();
+        }, {
+            maptype: {
+                mapType: 'openlayers'
+            }
+        });
+
+    });
+
 });
