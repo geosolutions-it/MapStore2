dcc6a941488777c2bf924019846cc76c1304f5ef
Lorenzo Natali <lorenzo.natali@geo-solutions.it>
2018-05-15 10:18:51 +0200

    Add unit tests

--- web/client/components/widgets/widget/__tests__/LegendWidget-test.jsx
+++ web/client/components/widgets/widget/__tests__/LegendWidget-test.jsx
@@ -26,6 +26,14 @@ describe('LegendWidget component', () => {
         const container = document.getElementById('container');
         const el = container.querySelector('.mapstore-widget-card');
         expect(el).toExist();
+        expect(container.querySelector('.glyphicon-pencil')).toExist();
+        expect(container.querySelector('.glyphicon-trash')).toExist();
+    });
+    it('view only mode', () => {
+        ReactDOM.render(<LegendWidget canEdit={false} />, document.getElementById("container"));
+        const container = document.getElementById('container');
+        expect(container.querySelector('.glyphicon-pencil')).toNotExist();
+        expect(container.querySelector('.glyphicon-trash')).toNotExist();
     });
     it('Test LegendWidget onEdit callback', () => {
         const actions = {
