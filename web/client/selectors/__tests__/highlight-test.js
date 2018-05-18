cd36fb317ed75a605d7d2dfc0ca1287eed2e1526
allyoucanmap <bovio.stefano@gmail.com>
2018-05-18 16:51:22 +0200

    Improved highlight selector

--- web/client/selectors/__tests__/highlight-test.js
+++ web/client/selectors/__tests__/highlight-test.js
@@ -94,6 +94,8 @@ describe('Test highlight selectors', () => {
         const features = filteredspatialObject(initialState);
         expect(features).toExist();
         expect(features).toBe(spatialObject);
+
+        expect(filteredspatialObject({})).toBe('emptyObject');
     });
     it('test filteredGeometry', () => {
         const geometry = initialState.query.filterObj.spatialField.geometry;
