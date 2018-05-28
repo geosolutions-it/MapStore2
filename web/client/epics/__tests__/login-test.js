9cf7c67ea198d730b04d387262ed4140ed2b9d16
Edgar Vitus Mlowe <mloweedgar0@gmail.com>
2018-05-28 13:33:39 +0300

    Fix #2306: preserving changes on map after login (#2938)

--- web/client/epics/__tests__/login-test.js
+++ web/client/epics/__tests__/login-test.js
@@ -8,8 +8,8 @@
 
 const expect = require('expect');
 const {INIT_CATALOG} = require('../../actions/catalog');
-const {loginSuccess, logout} = require('../../actions/security');
-const {initCatalogOnLoginOutEpic} = require('../login');
+const {loginSuccess, logout, logoutWithReload} = require('../../actions/security');
+const {initCatalogOnLoginOutEpic, reloadMapConfig} = require('../login');
 
 const {testEpic} = require('./epicTestUtils');
 
@@ -22,6 +22,20 @@ describe('login Epics', () => {
             done();
         };
         testEpic(initCatalogOnLoginOutEpic, 1, loginSuccess(), epicResult, {});
+    });
+    it('keeps map changes on login', (done) => {
+        const epicResult = actions => {
+            expect(actions.length).toBe(0);
+            done();
+        };
+        testEpic(reloadMapConfig, 0, loginSuccess(), epicResult, {});
+    });
+    it('removes unsaved map changes on logout', (done) => {
+        const epicResult = actions => {
+            expect(actions.length).toBe(0);
+            done();
+        };
+        testEpic(reloadMapConfig, 0, logoutWithReload(), epicResult, {});
     });
 
     it('init catalog on logout', (done) => {
