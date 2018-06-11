57b191efdf075df0d86def6c1e7e4ffdceeeb259
mloweedgar <mloweedgar0@gmail.com>
2018-06-11 04:33:20 +0300

    Connected to #2806: Remove un used variable

--- web/client/components/data/query/__tests__/NumberField-test.jsx
+++ web/client/components/data/query/__tests__/NumberField-test.jsx
@@ -8,7 +8,6 @@
 const expect = require('expect');
 const React = require('react');
 const ReactDOM = require('react-dom');
-const TestUtils = require('react-dom/test-utils');
 
 const NumberField = require('../NumberField');
 
