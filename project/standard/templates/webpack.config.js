e861d69adb53a4b28f4d79f59dfdc3baf348ca82
stefano bovio <stefano.bovio@geo-solutions.it>
2018-06-21 16:58:12 +0200

    Fix #2948 Update pom.xml, config and template for project (#2999)

--- project/standard/templates/webpack.config.js
+++ project/standard/templates/webpack.config.js
@@ -5,7 +5,9 @@ const extractThemesPlugin = require('./MapStore2/themes.js').extractThemesPlugin
 
 module.exports = require('./MapStore2/buildConfig')(
     {
-        '__PROJECTNAME__': path.join(__dirname, "js", "app")
+        '__PROJECTNAME__': path.join(__dirname, "js", "app"),
+        '__PROJECTNAME__-embedded': path.join(__dirname, "MapStore2", "web", "client", "product", "embedded"),
+        '__PROJECTNAME__-api': path.join(__dirname, "MapStore2", "web", "client", "product", "api")
     },
     themeEntries,
     {
