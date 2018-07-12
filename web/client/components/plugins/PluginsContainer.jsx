022b8a9aa0a6946b98a893992971f75bf76ec2f4
mbarto <maurobartolomeoli@gmail.com>
2018-07-12 13:00:50 +0200

    Fixes #3068: correct monitoring of plugins state to limit re-renderings (#3069)

--- web/client/components/plugins/PluginsContainer.jsx
+++ web/client/components/plugins/PluginsContainer.jsx
@@ -12,7 +12,7 @@ const PluginsUtils = require('../../utils/PluginsUtils');
 
 const assign = require('object-assign');
 
-const {get} = require('lodash');
+const {get, isEqual} = require('lodash');
 const {componentFromProp} = require('recompose');
 const Component = componentFromProp('component');
 
@@ -89,6 +89,19 @@ class PluginsContainer extends React.Component {
 
     componentWillReceiveProps(newProps) {
         this.loadPlugins(newProps.pluginsState, newProps);
+    }
+
+    shouldComponentUpdate(nextProps, nextState) {
+        return nextProps.pluginsConfig !== this.props.pluginsConfig
+            || nextProps.plugins !== this.props.plugins
+            || nextProps.params !== this.props.params
+            || nextProps.mode !== this.props.mode
+            || nextProps.monitoredState !== this.props.monitoredState
+            || nextProps.className !== this.props.className
+            || nextProps.style !== this.props.style
+            || nextProps.defaultMode !== this.props.defaultMode
+            || !isEqual(nextProps.pluginsState, this.props.pluginsState)
+            || !isEqual(nextState.loadedPlugins, this.state.loadedPlugins);
     }
 
     getState = (path, newProps) => {
