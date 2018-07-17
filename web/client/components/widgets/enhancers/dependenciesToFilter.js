dda1fcbcb5546d050b5cf627908aaf311d5a5dd2
Lorenzo Natali <lorenzo.natali@geo-solutions.it>
2018-07-17 15:59:58 +0200

    Support for CQL Filter parsing

--- web/client/components/widgets/enhancers/dependenciesToFilter.js
+++ web/client/components/widgets/enhancers/dependenciesToFilter.js
@@ -13,31 +13,31 @@ const filterBuilder = require('../../../utils/ogc/Filter/FilterBuilder');
  * Merges filter object and dependencies map into an ogc filter
  */
 module.exports = compose(
-   withPropsOnChange(
-       ({mapSync, geomProp, dependencies = {}} = {}, nextProps = {}, filter) =>
+    withPropsOnChange(
+        ({mapSync, geomProp, dependencies = {}} = {}, nextProps = {}, filter) =>
             mapSync !== nextProps.mapSync
             || dependencies.viewport !== (nextProps.dependencies && nextProps.dependencies.viewport)
             || geomProp !== nextProps.geomProp
             || filter !== nextProps.filter,
-       ({mapSync, geomProp = "the_geom", dependencies = {}, filter: filterObj} = {}) => {
-           const viewport = dependencies.viewport;
-           const {filter, property, and} = filterBuilder({gmlVersion: "3.1.1"});
-           if (!mapSync || !dependencies.viewport) {
-               return {
-                   filter: filterObj ? filter(and((FilterUtils.toOGCFilterParts(filterObj, "1.1.0", "ogc") || []))) : undefined
-               };
-           }
+        ({mapSync, geomProp = "the_geom", dependencies = {}, filter: filterObj} = {}) => {
+            const viewport = dependencies.viewport;
+            const {filter, property, and} = filterBuilder({gmlVersion: "3.1.1"});
+            if (!mapSync || !dependencies.viewport) {
+                return {
+                    filter: filterObj ? filter(and((FilterUtils.toOGCFilterParts(filterObj, "1.1.0", "ogc") || []))) : undefined
+                };
+            }
 
-           const bounds = Object.keys(viewport.bounds).reduce((p, c) => {
-               return {...p, [c]: parseFloat(viewport.bounds[c])};
-           }, {});
-           const geom = CoordinatesUtils.getViewportGeometry(bounds, viewport.crs);
-           return {
-               filter: filter(and(
-                   ...(filterObj ? FilterUtils.toOGCFilterParts(filterObj, "1.1.0", "ogc") : []),
+            const bounds = Object.keys(viewport.bounds).reduce((p, c) => {
+                return {...p, [c]: parseFloat(viewport.bounds[c])};
+            }, {});
+            const geom = CoordinatesUtils.getViewportGeometry(bounds, viewport.crs);
+            return {
+                filter: filter(and(
+                    ...(filterObj ? FilterUtils.toOGCFilterParts(filterObj, "1.1.0", "ogc") : []),
                     property(geomProp).intersects(geom)))
-           };
-       }
-   )
+            };
+        }
+    )
 
 );
