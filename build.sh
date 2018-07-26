013d559165a9d2d1e0b799b79189d72299f48864
Alessandro Parma <randomorder@users.noreply.github.com>
2018-07-26 17:02:49 +0200

    Update build.sh
    More verbose build script

--- build.sh
+++ build.sh
@@ -1,12 +1,32 @@
 #!/bin/bash
 set -e
 
+echo "Running NPM install to update dependencies"
+echo `date`
 npm install
+
+echo "Building MS2 bundles"
+echo `date`
 npm run compile
+
+echo "Cleanup Documentation"
+echo `date`
 npm run cleandoc
+
+echo "Checking syntax"
+echo `date`
 npm run lint
+
+echo "Run MapStore2 tests"
+echo `date`
 npm test
+
+echo "Creating Documentation"
+echo `date`
 npm run doc
+
+echo "Building final WAR package"
+echo `date`
 if [ $# -eq 0 ]
   then
     mvn clean install
@@ -14,4 +34,6 @@ if [ $# -eq 0 ]
     mvn clean install -Dmapstore2.version=$1
 fi
 
+echo "Final Cleanup"
+echo `date`
 npm run cleandoc
