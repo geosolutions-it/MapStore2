bb2e67864430ba18080457698ec74772564d64e7
allyoucanmap <bovio.stefano@gmail.com>
2018-08-02 12:06:06 +0200

    Updated map layout epic to listen to TOGGLE_MAPINFO_STATE

--- web/client/epics/maplayout.js
+++ web/client/epics/maplayout.js
@@ -10,7 +10,7 @@ const {updateMapLayout} = require('../actions/maplayout');
 const {TOGGLE_CONTROL, SET_CONTROL_PROPERTY} = require('../actions/controls');
 const {MAP_CONFIG_LOADED} = require('../actions/config');
 const {SIZE_CHANGE, CLOSE_FEATURE_GRID, OPEN_FEATURE_GRID} = require('../actions/featuregrid');
-const {CLOSE_IDENTIFY, ERROR_FEATURE_INFO} = require('../actions/mapInfo');
+const {CLOSE_IDENTIFY, ERROR_FEATURE_INFO, TOGGLE_MAPINFO_STATE} = require('../actions/mapInfo');
 const {SHOW_SETTINGS, HIDE_SETTINGS} = require('../actions/layers');
 const {mapInfoRequestsSelector} = require('../selectors/mapinfo');
 
@@ -32,11 +32,13 @@ const {isFeatureGridOpen, getDockSize} = require('../selectors/featuregrid');
  */
 
 const updateMapLayoutEpic = (action$, store) =>
-    action$.ofType(MAP_CONFIG_LOADED, SIZE_CHANGE, CLOSE_FEATURE_GRID, OPEN_FEATURE_GRID, CLOSE_IDENTIFY, TOGGLE_CONTROL, SET_CONTROL_PROPERTY, SHOW_SETTINGS, HIDE_SETTINGS, ERROR_FEATURE_INFO)
+    action$.ofType(MAP_CONFIG_LOADED, SIZE_CHANGE, CLOSE_FEATURE_GRID, OPEN_FEATURE_GRID, CLOSE_IDENTIFY, TOGGLE_MAPINFO_STATE, TOGGLE_CONTROL, SET_CONTROL_PROPERTY, SHOW_SETTINGS, HIDE_SETTINGS, ERROR_FEATURE_INFO)
         .switchMap(() => {
 
-            if (get(store.getState(), "browser.mobile")) {
-                const bottom = mapInfoRequestsSelector(store.getState()).length > 0 ? {bottom: '50%'} : {bottom: undefined};
+            const state = store.getState();
+
+            if (get(state, "browser.mobile")) {
+                const bottom = mapInfoRequestsSelector(state).length > 0 ? {bottom: '50%'} : {bottom: undefined};
                 const boundingMapRect = {
                     ...bottom
                 };
@@ -47,9 +49,9 @@ const updateMapLayoutEpic = (action$, store) =>
 
             const mapLayout = {left: {sm: 300, md: 500, lg: 600}, right: {md: 658}, bottom: {sm: 30}};
 
-            if (get(store.getState(), "mode") === 'embedded') {
+            if (get(state, "mode") === 'embedded') {
                 const height = {height: 'calc(100% - ' + mapLayout.bottom.sm + 'px)'};
-                const bottom = mapInfoRequestsSelector(store.getState()).length > 0 ? {bottom: '50%'} : {bottom: undefined};
+                const bottom = mapInfoRequestsSelector(state).length > 0 ? {bottom: '50%'} : {bottom: undefined};
                 const boundingMapRect = {
                     ...bottom
                 };
@@ -60,23 +62,23 @@ const updateMapLayoutEpic = (action$, store) =>
             }
 
             const leftPanels = head([
-                get(store.getState(), "controls.queryPanel.enabled") && {left: mapLayout.left.lg} || null,
-                get(store.getState(), "controls.widgetBuilder.enabled") && {left: mapLayout.left.md} || null,
-                get(store.getState(), "layers.settings.expanded") && {left: mapLayout.left.md} || null,
-                get(store.getState(), "controls.drawer.enabled") && {left: mapLayout.left.sm} || null
+                get(state, "controls.queryPanel.enabled") && {left: mapLayout.left.lg} || null,
+                get(state, "controls.widgetBuilder.enabled") && {left: mapLayout.left.md} || null,
+                get(state, "layers.settings.expanded") && {left: mapLayout.left.md} || null,
+                get(state, "controls.drawer.enabled") && {left: mapLayout.left.sm} || null
             ].filter(panel => panel)) || {left: 0};
 
             const rightPanels = head([
-                get(store.getState(), "controls.details.enabled") && {right: mapLayout.right.md} || null,
-                get(store.getState(), "controls.annotations.enabled") && {right: mapLayout.right.md} || null,
-                get(store.getState(), "controls.metadataexplorer.enabled") && {right: mapLayout.right.md} || null,
-                mapInfoRequestsSelector(store.getState()).length > 0 && {right: mapLayout.right.md} || null
+                get(state, "controls.details.enabled") && {right: mapLayout.right.md} || null,
+                get(state, "controls.annotations.enabled") && {right: mapLayout.right.md} || null,
+                get(state, "controls.metadataexplorer.enabled") && {right: mapLayout.right.md} || null,
+                get(state, "mapInfo.enabled") && mapInfoRequestsSelector(state).length > 0 && {right: mapLayout.right.md} || null
             ].filter(panel => panel)) || {right: 0};
 
-            const dockSize = getDockSize(store.getState()) * 100;
-            const bottom = isFeatureGridOpen(store.getState()) && {bottom: dockSize + '%', dockSize} || {bottom: mapLayout.bottom.sm};
+            const dockSize = getDockSize(state) * 100;
+            const bottom = isFeatureGridOpen(state) && {bottom: dockSize + '%', dockSize} || {bottom: mapLayout.bottom.sm};
 
-            const transform = isFeatureGridOpen(store.getState()) && {transform: 'translate(0, -' + mapLayout.bottom.sm + 'px)'} || {transform: 'none'};
+            const transform = isFeatureGridOpen(state) && {transform: 'translate(0, -' + mapLayout.bottom.sm + 'px)'} || {transform: 'none'};
             const height = {height: 'calc(100% - ' + mapLayout.bottom.sm + 'px)'};
 
             const boundingMapRect = {
