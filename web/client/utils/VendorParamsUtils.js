6e41e426fcbf9a71632630d7849fe19d8e9492c6
Lorenzo Natali <lorenzo.natali@geo-solutions.it>
2018-08-03 13:18:40 +0200

    Updated documentation

--- web/client/utils/VendorParamsUtils.js
+++ web/client/utils/VendorParamsUtils.js
@@ -10,8 +10,12 @@ const FilterUtils = require('./FilterUtils');
 
 module.exports = {
     /**
+     * Check layer options to manipulate and manage vendor params in case of GeoServer usage.
      * If you have a filterObj in options, this should be converted into a CQL_FILTER and joined with the existing cql filter, if any.
-     * (filterObj is the temp filter applied to the layer)
+     * (filterObj is the temp filter applied to the layer when you apply, for instance, sync with feature grid)
+     * @memberof Utils.VendorParamsUtils
+     * @param {object} options layer options
+     * @returns params for the layer's request, properly manipulated
      */
     optionsToVendorParams: (options = {}) => {
         const cqlFilterFromObject = FilterUtils.isFilterValid(options.filterObj) && FilterUtils.toCQLFilter(options.filterObj);
