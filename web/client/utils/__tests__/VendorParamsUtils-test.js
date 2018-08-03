6c3ed01c90a50dcbc18064e7a9bc2dd8aa27c97b
Lorenzo Natali <offtherailz@gmail.com>
2018-08-03 18:33:03 +0200

    Fix #3142. Don't add CQL_FILTER when not present (#3143)

---
+++ web/client/utils/__tests__/VendorParamsUtils-test.js
@@ -0,0 +1,26 @@
+/*
+ * Copyright 2018, GeoSolutions Sas.
+ * All rights reserved.
+ *
+ * This source code is licensed under the BSD-style license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+const expect = require('expect');
+const { optionsToVendorParams} = require('../VendorParamsUtils');
+describe('VendorParamUtils ', () => {
+
+    it('optionsToVendorParams', () => {
+        expect(optionsToVendorParams()).toNotExist();
+        expect(optionsToVendorParams({
+            params: {
+                CQL_FILTER: "INCLUDE"
+            }
+        }).CQL_FILTER).toBe("INCLUDE");
+    });
+    it('optionsToVendorParams do not add other params to the request (#3142)', () => {
+        const params = optionsToVendorParams({ params: {viewParams: "a:1"}});
+        expect(Object.keys(params).length).toBe(1);
+    });
+
+});
