6db6fcd0163418c4d3246633b18511dff0c36693
baloola <baloola-mu@hotmail.com>
2018-08-07 17:12:19 +0200

    adding unit test & editing the if statment

--- web/client/components/data/query/CrossLayerFilter.jsx
+++ web/client/components/data/query/CrossLayerFilter.jsx
@@ -52,7 +52,7 @@ module.exports = ({
 
     return (<SwitchPanel
         loading={loadingCapabilities}
-        expanded={crossLayerExpanded && !loadingCapabilities && !errorObj || (operation && queryCollection.typeName) }
+        expanded={operation && queryCollection.typeName ? true : crossLayerExpanded && !loadingCapabilities && !errorObj }
         error={errorObj}
         errorMsgId={"queryPanel"}
         buttons={[
