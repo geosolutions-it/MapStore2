cd22184dc4ce81d6da7c3057c5eea23cdeb24d47
Lorenzo Natali <lorenzo.natali@geo-solutions.it>
2018-08-10 12:59:55 +0200

    Fixed various issues about the status

--- web/client/components/misc/__tests__/NumberPicker-test.jsx
+++ web/client/components/misc/__tests__/NumberPicker-test.jsx
@@ -27,11 +27,11 @@ describe("This test for NumberPicker component", () => {
         const spyonChange = expect.spyOn(actions, 'onChange');
         ReactDOM.render(<NumberPicker onChange={actions.onChange} />, document.getElementById("container"));
         expect(document.querySelector('.rw-numberpicker')).toExist();
-        ReactTestUtils.Simulate.mouseDown(document.querySelector('.rw-btn')); // <-- trigger event callback
+        ReactTestUtils.Simulate.mouseDown(document.querySelector('.increment')); // <-- trigger event callback
         expect(spyonChange).toHaveBeenCalled();
         expect(spyonChange.calls[0].arguments[0]).toBe(1);
-        ReactTestUtils.Simulate.mouseDown(document.querySelectorAll('.rw-btn')[1]);
-        expect(spyonChange.calls[1].arguments[0]).toBe(-1);
+        ReactTestUtils.Simulate.mouseDown(document.querySelector('.decrement'));
+        expect(spyonChange.calls[1].arguments[0]).toBe(0);
     });
     it('Test NumberPicker onChange with value', () => {
         const actions = {
@@ -41,11 +41,12 @@ describe("This test for NumberPicker component", () => {
         ReactDOM.render(<NumberPicker value={2} onChange={actions.onChange} />, document.getElementById("container"));
         expect(document.querySelector('.rw-numberpicker')).toExist();
         expect(document.querySelector('input').value).toBe('2');
-        ReactTestUtils.Simulate.mouseDown(document.querySelector('.rw-btn')); // <-- trigger event callback
+        ReactTestUtils.Simulate.mouseDown(document.querySelector('.increment')); // <-- trigger event callback
         expect(spyonChange).toHaveBeenCalled();
         expect(spyonChange.calls[0].arguments[0]).toBe(3);
-        ReactTestUtils.Simulate.mouseDown(document.querySelectorAll('.rw-btn')[1]);
-        expect(spyonChange.calls[1].arguments[0]).toBe(1);
+        ReactDOM.render(<NumberPicker value={3} onChange={actions.onChange} />, document.getElementById("container"));
+        ReactTestUtils.Simulate.mouseDown(document.querySelector('.decrement'));
+        expect(spyonChange.calls[1].arguments[0]).toBe(2);
     });
     it('Test NumberPicker onChange', () => {
         const actions = {
@@ -58,4 +59,72 @@ describe("This test for NumberPicker component", () => {
         expect(spyonChange).toHaveBeenCalled();
         expect(spyonChange.calls[0].arguments[0]).toBe(7);
     });
+    it('Test NumberPicker onChange invalid input', () => {
+        const actions = {
+            onChange: () => { }
+        };
+        const spyonChange = expect.spyOn(actions, 'onChange');
+        ReactDOM.render(<NumberPicker onChange={actions.onChange} />, document.getElementById("container"));
+        expect(document.querySelector('.rw-numberpicker')).toExist();
+        ReactTestUtils.Simulate.change(document.querySelector('input'), { target: { value: 'a' } }); // <-- trigger event callback
+        expect(spyonChange).toNotHaveBeenCalled();
+        expect(document.querySelector('input').value).toBe('');
+    });
+    it('Test NumberPicker onChange reset onBlur ', () => {
+        const actions = {
+            onChange: () => { }
+        };
+        const spyonChange = expect.spyOn(actions, 'onChange');
+        ReactDOM.render(<NumberPicker value={1} onChange={actions.onChange} />, document.getElementById("container"));
+        expect(document.querySelector('.rw-numberpicker')).toExist();
+        ReactTestUtils.Simulate.change(document.querySelector('input'), { target: { value: '.' } }); // <-- trigger event callback
+        expect(spyonChange).toNotHaveBeenCalled();
+        ReactTestUtils.Simulate.blur(document.querySelector('input')); // <-- trigger event callback
+        expect(spyonChange).toHaveBeenCalled();
+        expect(spyonChange.calls[0].arguments[0]).toBe(null);
+    });
+    it('Test NumberPicker onChange onBlur do not trigger twice', () => {
+        const actions = {
+            onChange: () => { }
+        };
+        const spyonChange = expect.spyOn(actions, 'onChange');
+        ReactDOM.render(<NumberPicker value={1} onChange={actions.onChange} />, document.getElementById("container"));
+        expect(document.querySelector('.rw-numberpicker')).toExist();
+        ReactTestUtils.Simulate.change(document.querySelector('input'), { target: { value: '.' } }); // <-- trigger event callback
+        expect(spyonChange).toNotHaveBeenCalled();
+        ReactTestUtils.Simulate.change(document.querySelector('input'), { target: { value: '.7' } }); // <-- trigger event callback
+        ReactDOM.render(<NumberPicker value={0.7} onChange={actions.onChange} />, document.getElementById("container"));
+        ReactTestUtils.Simulate.blur(document.querySelector('input')); // <-- trigger event callback
+        expect(spyonChange).toHaveBeenCalled();
+        expect(spyonChange.calls[0].arguments[0]).toBe(0.7);
+        // do not trigger twice on blur if value is updated
+        expect(spyonChange.calls.length).toBe(1);
+    });
+    it('Test NumberPicker onChange with increment decrement correct precision', () => {
+        const actions = {
+            onChange: () => { }
+        };
+        const spyonChange = expect.spyOn(actions, 'onChange');
+        ReactDOM.render(<NumberPicker value={7.12} onChange={actions.onChange} />, document.getElementById("container"));
+        expect(document.querySelector('.rw-numberpicker')).toExist();
+        expect(document.querySelector('input').value).toBe('7.12');
+        ReactTestUtils.Simulate.mouseDown(document.querySelector('.increment')); // <-- trigger event callback
+        expect(spyonChange).toHaveBeenCalled();
+        expect(spyonChange.calls[0].arguments[0]).toBe(8.12); // without round it will be 8.1200000000001
+        expect(document.querySelector('input').value).toBe('8.12');
+    });
+    it('Test NumberPicker onChange increment decrement with value', () => {
+        const actions = {
+            onChange: () => { }
+        };
+        const spyonChange = expect.spyOn(actions, 'onChange');
+        ReactDOM.render(<NumberPicker value={8.2} onChange={actions.onChange} />, document.getElementById("container"));
+        expect(document.querySelector('.rw-numberpicker')).toExist();
+        expect(document.querySelector('input').value).toBe('8.2');
+        ReactTestUtils.Simulate.mouseDown(document.querySelector('.decrement')); // <-- trigger event callback
+        expect(spyonChange).toHaveBeenCalled();
+        expect(spyonChange.calls[0].arguments[0]).toBe(7.2); // without round it will be 2.1999999999999
+        expect(document.querySelector('input').value).toBe('7.2');
+    });
+
 });
