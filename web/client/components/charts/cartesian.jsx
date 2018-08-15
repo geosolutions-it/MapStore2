fb76623e1d5e62d665258916236a12d415df15d8
baloola <baloola-mu@hotmail.com>
2018-08-15 20:05:07 +0200

    oblique label with shortend strings

--- web/client/components/charts/cartesian.jsx
+++ web/client/components/charts/cartesian.jsx
@@ -8,10 +8,25 @@
 
 
 const React = require('react');
-const { XAxis, YAxis, CartesianGrid} = require('recharts');
+const{isString, isNumber, truncate,inRange}=require('lodash')
+const { XAxis, YAxis, CartesianGrid,Text} = require('recharts');
+
+const customize = (label)=>(
+    isString(label)?
+        truncate(label,{'length': 5})
+        :isNumber(label)?
+            label
+    :null
+)
+
+const CustomizedTick = ({x, y,height, payload}) => (
+    <Text style={{fill:'#666'}} x={x} y={y+height/2} angle ={-45} textAnchor="middle" >
+    {customize(payload.value)}
+    </Text>
+);
 
 const renderCartesianTools = ({xAxis, yAxis, cartesian}) => ([
-    xAxis && xAxis.show !== false ? <XAxis key="xaxis" {...xAxis}/> : null,
+    xAxis && xAxis.show !== false ? <XAxis key="xaxis" tick={<CustomizedTick/>} {...xAxis}/> : null,
     yAxis ? <YAxis key="yaxis" {...yAxis}/> : null,
     cartesian !== false ? <CartesianGrid key="cartesiangrid" {...cartesian}/> : null]);
 module.exports = {renderCartesianTools};
