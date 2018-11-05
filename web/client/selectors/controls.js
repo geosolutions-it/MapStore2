2e2f6e036a458579383ce35a8ba0c6d2b0b41dee
Matteo V <matteo.velludini.geosolutions@gmail.com>
2018-11-05 10:06:31 +0100

    Fix #3304 update default backgrounds (#3305)
    * Fix #3304 update backgrounds
    
    * added test and a comment on thums for deleted backgrounds

--- web/client/selectors/controls.js
+++ web/client/selectors/controls.js
@@ -5,5 +5,6 @@ module.exports = {
     wfsDownloadAvailable: state => !!get(state, "controls.wfsdownload.available"),
     wfsDownloadSelector: state => !!get(state, "controls.wfsdownload.enabled"),
     widgetBuilderAvailable: state => get(state, "controls.widgetBuilder.available", false),
-    widgetBuilderSelector: (state) => get(state, "controls.widgetBuilder.enabled")
+    widgetBuilderSelector: (state) => get(state, "controls.widgetBuilder.enabled"),
+    drawerEnabledControlSelector: (state) => get(state, "controls.drawer.enabled", false)
 };
