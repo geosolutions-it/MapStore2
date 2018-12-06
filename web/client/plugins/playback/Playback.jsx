e91309fecf39e33a86f3ec33df9c9232b63e3596
Lorenzo Natali <lorenzo.natali@geo-solutions.it>
2018-12-06 17:36:36 +0100

    Fix #3396. Disable settings when animation is running

--- web/client/plugins/playback/Playback.jsx
+++ web/client/plugins/playback/Playback.jsx
@@ -116,6 +116,7 @@ module.exports = playbackEnhancer(({
                     glyph: "wrench",
                     bsStyle: (status !== statusMap.PLAY && status !== statusMap.PAUSE) && showSettings ? 'success' : 'primary',
                     active: (status !== statusMap.PLAY || status !== statusMap.PAUSE) && !!showSettings,
+                    disabled: (status === statusMap.PLAY || status === statusMap.PAUSE),
                     onClick: () => status !== statusMap.PLAY && onShowSettings(!showSettings),
                     tooltip: <Message msgId={"playback.settings.title"} />
                 }
