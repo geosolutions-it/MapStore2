d09e85f7227709787bfad24d751a06dd7ee0d74d
stefano bovio <stefano.bovio@geo-solutions.it>
2019-02-18 10:48:28 +0100

    Fix #3537 Zoom to layer does not work (#3538)
    * Removed map reducers from map plugin
    
    * Updated test of Map plugin
    
    * Removed map initial config from reducers in test

--- web/client/plugins/Map.jsx
+++ web/client/plugins/Map.jsx
@@ -400,8 +400,6 @@ module.exports = {
         onFontError: errorLoadingFont
     })(MapPlugin),
     reducers: {
-        map: require('../reducers/map'),
-        layers: require('../reducers/layers'),
         draw: require('../reducers/draw'),
         highlight: require('../reducers/highlight'),
         maptype: require('../reducers/maptype'),
