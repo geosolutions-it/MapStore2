1df8823f13385a40f609ed82ee84afb143cc6ddc
Lorenzo Natali <lorenzo.natali@geo-solutions.it>
2019-03-29 18:02:21 +0100

    documented and clean up zoom feature

--- web/client/epics/map.js
+++ web/client/epics/map.js
@@ -29,60 +29,7 @@ const {warning} = require('../actions/notifications');
 const {resetControls} = require('../actions/controls');
 const {clearLayers} = require('../actions/layers');
 const {removeAllAdditionalLayers} = require('../actions/additionallayers');
-const { head, isArray } = require('lodash');
-
-
-const legacyZoomToExtent = (action, state) => {
-    let zoom = 0;
-    let extent = [];
-    // TODO: move out of here
-    if (isArray(action.extent)) {
-        extent = action.extent.map((val) => {
-            // MapUtils.getCenterForExtent returns an array of strings sometimes (catalog)
-            if (typeof val === 'string' || val instanceof String) {
-                return Number(val);
-            }
-            return val;
-        });
-    } else {
-        extent = Object.keys(action.extent).map(v => {
-            if (typeof action.extent[v] === 'string' || action.extent[v] instanceof String) {
-                return Number(action.extent[v]);
-            }
-            return action.extent[v];
-        });
-    }
-    let bounds = CoordinatesUtils.reprojectBbox(extent, action.crs, state.bbox && state.bbox.crs || "EPSG:4326");
-    if (bounds) {
-        // center by the max. extent defined in the map's config
-        let center = CoordinatesUtils.reproject(MapUtils.getCenterForExtent(extent, action.crs), action.crs, 'EPSG:4326');
-        // workaround to get zoom 0 for -180 -90... - TODO do it better
-        let full = action.crs === "EPSG:4326" && extent && extent[0] <= -180 && extent[1] <= -90 && extent[2] >= 180 && extent[3] >= 90;
-        if (full) {
-            zoom = 1;
-        } else {
-            let mapBBounds = CoordinatesUtils.reprojectBbox(extent, action.crs, state.projection || "EPSG:4326");
-            // NOTE: STATE should contain size !!!
-            zoom = MapUtils.getZoomForExtent(mapBBounds, state.size, 0, 21, null);
-        }
-        if (action.maxZoom && zoom > action.maxZoom) {
-            zoom = action.maxZoom;
-        }
-        let newBounds = { minx: bounds[0], miny: bounds[1], maxx: bounds[2], maxy: bounds[3] };
-        let newBbox = { ...state.bbox, bounds: newBounds };
-        return Rx.Observable.of(changeMapView(
-            center,
-            zoom,
-            newBbox,
-            state.size,
-            action.mapStateSource,
-            state.projection,
-            state.viewerOptions
-        ));
-    }
-    Rx.Observable.empty();
-
-};
+const { head, isArray, isObject, mapValues } = require('lodash');
 
 const handleCreationBackgroundError = (action$, store) =>
     action$.ofType(CREATION_ERROR_LAYER)
@@ -136,14 +83,93 @@ const handleCreationLayerError = (action$, store) =>
 const resetMapOnInit = action$ =>
     action$.ofType(INIT_MAP).switchMap(() => Rx.Observable.of(removeAllAdditionalLayers(), resetControls(), clearLayers()));
 
+/**
+ * Convert and normalize the extent into an array `minx,miny,maxx, maxy`
+ * @param {object|array} extent extent object to normalize
+ */
+const toBoundsArray = extent => {
+    // clean up extent
+    if (isArray(extent)) {
+        return extent.map((val) => {
+            // MapUtils.getCenterForExtent returns an array of strings sometimes (catalog)
+            if (typeof val === 'string' || val instanceof String) {
+                return Number(val);
+            }
+            return val;
+        });
+    }
+    if (isObject(extent)) {
+        const numericExtent = mapValues(extent, v => {
+            if (typeof v === 'string' || v instanceof String) {
+                return Number(v);
+            }
+            return v;
+        });
+        return [
+            numericExtent.minx,
+            numericExtent.miny,
+            numericExtent.maxx,
+            numericExtent.maxy
+        ];
+    }
+};
+/**
+ * Base implementation of zoom To Extent that becomes a changeMapView operation.
+ * It doesn't take into account padding or any other operation
+ * @param {object} action
+ * @param {object} mapState the map object in state
+ */
+const legacyZoomToExtent = (action, mapState) => {
+    let zoom = 0;
+    let extent = [];
+    let bounds = CoordinatesUtils.reprojectBbox(extent, action.crs, mapState.bbox && mapState.bbox.crs || "EPSG:4326");
+    if (bounds) {
+        // center by the max. extent defined in the map's config
+        let center = CoordinatesUtils.reproject(MapUtils.getCenterForExtent(extent, action.crs), action.crs, 'EPSG:4326');
+        // workaround to get zoom 0 for -180 -90... - TODO do it better
+        let full = action.crs === "EPSG:4326" && extent && extent[0] <= -180 && extent[1] <= -90 && extent[2] >= 180 && extent[3] >= 90;
+        if (full) {
+            zoom = 1;
+        } else {
+            let mapBBounds = CoordinatesUtils.reprojectBbox(extent, action.crs, mapState.projection || "EPSG:4326");
+            // NOTE: mapState should contain size !!!
+            zoom = MapUtils.getZoomForExtent(mapBBounds, mapState.size, 0, 21, null);
+        }
+        if (action.maxZoom && zoom > action.maxZoom) {
+            zoom = action.maxZoom;
+        }
+        let newBounds = { minx: bounds[0], miny: bounds[1], maxx: bounds[2], maxy: bounds[3] };
+        let newBbox = { ...mapState.bbox, bounds: newBounds };
+        return Rx.Observable.of(changeMapView(
+            center,
+            zoom,
+            newBbox,
+            mapState.size,
+            action.mapStateSource,
+            mapState.projection,
+            mapState.viewerOptions
+        ));
+    }
+    Rx.Observable.empty();
+};
 
+
+/**
+ * Implement ZOOM_TO_EXTENT action. If mapHooks are present, it uses ZOOM_TO_EXTENT_HOOK to make the map zoom on it's own
+ * (mapping libraries have maxZoom and padding support). Otherwise, triggers a changeMapView to emulate the same operation.
+ * @memberof epics.map
+ */
 const zoomToExtentEpic = (action$, {getState = () => {} }) =>
-    action$.ofType(ZOOM_TO_EXTENT).switchMap((action ) => {
+    action$.ofType(ZOOM_TO_EXTENT).switchMap(( action ) => {
+        const extent = toBoundsArray(action.extent);
+        if (!extent) {
+            return Rx.Observable.empty();
+        }
         const hook = MapUtils.getHook(MapUtils.ZOOM_TO_EXTENT_HOOK);
         const padding = mapPaddingSelector(getState());
         if (hook) {
             // TODO: manage crs
-            const { extent, crs, maxZoom } = action;
+            const { crs, maxZoom } = action;
             hook(extent, {
                 crs,
                 padding,
@@ -151,7 +177,7 @@ const zoomToExtentEpic = (action$, {getState = () => {} }) =>
             });
             return Rx.Observable.empty();
         }
-        return legacyZoomToExtent(action, mapSelector(getState()) );
+        return legacyZoomToExtent({...action, extent}, mapSelector(getState()) );
     });
 module.exports = {
     handleCreationLayerError,
