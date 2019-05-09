8c6c9a95b8814f4972c6ff24d03ce1734fa19276
Lorenzo Natali <offtherailz@gmail.com>
2019-05-09 10:20:17 +0200

    Fix indentation

--- web/client/epics/contenttabs.js
+++ web/client/epics/contenttabs.js
@@ -1,10 +1,10 @@
 /*
- * Copyright 2018, GeoSolutions Sas.
+ * Copyright 2019, GeoSolutions Sas.
  * All rights reserved.
  *
  * This source code is licensed under the BSD-style license found in the
  * LICENSE file in the root directory of this source tree.
-*/
+ */
 
 const Rx = require('rxjs');
 const {findKey} = require('lodash');
