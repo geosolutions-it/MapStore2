2d13165757cdb78376ee8038dc33885c470a3d82
allyoucanmap <bovio.stefano@gmail.com>
2019-07-02 16:33:21 +0200

    add wms and wmts services on catalog epics of TOC

--- web/client/plugins/TOC.jsx
+++ web/client/plugins/TOC.jsx
@@ -592,7 +592,9 @@ const TOCPlugin = connect(tocSelector, {
 })(LayerTree);
 
 const API = {
-    csw: require('../api/CSW')
+    csw: require('../api/CSW'),
+    wms: require('../api/WMS'),
+    wmts: require('../api/WMTS')
 };
 
 module.exports = {
