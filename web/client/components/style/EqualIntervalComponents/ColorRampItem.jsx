59f6b691260c3cd89360ca9c269075750b8f7c9a
Isaac Kaso <kasongoyo@gmail.com>
2019-07-11 10:58:28 +0300

    add copyright signature to the files

--- web/client/components/style/EqualIntervalComponents/ColorRampItem.jsx
+++ web/client/components/style/EqualIntervalComponents/ColorRampItem.jsx
@@ -1,8 +1,20 @@
+/**
+ * Copyright 2019, GeoSolutions Sas.
+ * All rights reserved.
+ *
+ * This source code is licensed under the BSD-style license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
 const React = require('react');
 const PropTypes = require('prop-types');
 const colors = require('./ExtendColorBrewer');
 const Message = require('../../I18N/Message');
 
+/**
+ * @name ColorRampItem
+ * @description Simple component to display color label
+ */
 const ColorRampItem = ({ item }) => {
     let ramp = item && (item.ramp || colors[item.name] && colors[item.name][5]) || [];
     return (
