35b42a782ac9623006593286e544312cd8cf73dc
mbarto <maurobartolomeoli@gmail.com>
2019-07-16 18:28:46 +0200

    #3863: improved Print plugin documentation with info on printing usin… (#3975)
    * #3863: improved Print plugin documentation with info on printing using custom scales
    
    * #3863: fixed typo

--- web/client/plugins/Print.jsx
+++ web/client/plugins/Print.jsx
@@ -45,16 +45,36 @@ require('./print/print.css');
  * @memberof plugins
  * @static
  *
+ * @prop {boolean} cfg.useFixedScales if true the printing scale is constrained to the nearest scale of the ones configured
+ * in the config.yml file, if false the current scale is used
  * @prop {object} cfg.overrideOptions overrides print options, this will override options created from current state of map
- * @prop {bool} cfg.overrideOptions.gedetic print in geodetic mode
+ * @prop {boolean} cfg.overrideOptions.geodetic prints in geodetic mode: in geodetic mode scale calculation is more precise on
+ * printed maps, but the preview is not accurate
  * @prop {string} cfg.overrideOptions.outputFilename name of output file
+ * @prop {object} cfg.mapPreviewOptions options for the map preview tool
+ * @prop {boolean} cfg.mapPreviewOptions.enableScalebox if true a combobox to select the printing scale is shown over the preview
+ * this is particularly useful if useFixedScales is also true, to show the real printing scales
+ * @prop {boolean} cfg.mapPreviewOptions.enableRefresh true by default, if false the preview is not updated if the user pans or zooms the main map
  *
  * @example
+ * // printing in geodetic mode
  * {
  *   "name": "Print",
  *   "cfg": {
  *       "overrideOptions": {
- *          "gedetic": false
+ *          "geodetic": true
+ *       }
+ *    }
+ * }
+ *
+ * @example
+ * // Using a list of fixed scales with scale selector
+ * {
+ *   "name": "Print",
+ *   "cfg": {
+ *       "useFixedScales": true,
+ *       "mapPreviewOptions": {
+ *          "enableScalebox": true
  *       }
  *    }
  * }
