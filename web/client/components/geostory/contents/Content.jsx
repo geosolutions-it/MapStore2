c973361bdb7a45ce2e03f4e2bbe1d3d9bf74603a
Lorenzo Natali <lorenzo.natali@geo-solutions.it>
2019-08-02 17:57:37 +0200

    WIP scroll immersive

--- web/client/components/geostory/contents/Content.jsx
+++ web/client/components/geostory/contents/Content.jsx
@@ -10,7 +10,7 @@ import TextB from './Text';
 import enhanceContent from './enhancers/enhanceContent';
 
 
-const Text = enhanceContent({visibilityEnhancerOptions: { threshold: [0.75] }})(TextB);
+const Text = enhanceContent({visibilityEnhancerOptions: { threshold: [0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1] }})(TextB);
 const DummyComponent = ({ type, inViewRef }) => <div ref={inViewRef} className="ms-content ms-content-unknown">{`warning: unknown content type "${type}"`}</div>;
 
 
--- web/client/components/geostory/layouts/sections/enhancers/immersiveBackgroundManager.jsx
+++ web/client/components/geostory/layouts/sections/enhancers/immersiveBackgroundManager.jsx
@@ -5,27 +5,66 @@
  * This source code is licensed under the BSD-style license found in the
  * LICENSE file in the root directory of this source tree.
  */
-import { compose, withState, withProps, withHandlers } from 'recompose';
-import { findIndex, get } from "lodash";
+import { compose, createEventHandler, withPropsOnChange, withHandlers, mapPropsStream } from 'recompose';
+import { findIndex, get, maxBy } from "lodash";
+import { Observable } from "rxjs";
 
 const getContentIndex = (contents, id) => {
     const index = findIndex(contents, { id });
     return contents[index === -1 || !index ? 0 : index];
 };
 
+const createBackgroundIdStream = (intersection$) =>
+    intersection$
+        // create a map with the latest states of each intersection event
+        .scan((visibleItems = {}, { id, visible, entry }) => ({
+            ...visibleItems,
+            [id]: {
+                visible,
+                entry
+            }
+        }), {})
+        .do(r => console.log(r))
+        // select the current background id
+        .map((visibleItems = {}) =>
+            // get the one with max intersectionRatio that should be the selected background ID
+            maxBy(
+                Object.keys(visibleItems)
+                    .filter(k => get(visibleItems[k], 'visible')),  // exclude items not visible anymore
+                (k) => get(visibleItems[k], 'event.intersectionRatio')
+            )
+        )
+        .do(r => console.log(`backgroundId: ${r}`))
+        // optimization
+        .distinctUntilChanged()
+        // create the prop
+        .map(backgroundId => ({
+            backgroundId
+        }));
 
 /**
  * Holds the current background as background property
  * by intercepting onVisibilityChange from components.
  */
 export default compose(
-    withState('backgroundId', "setBackgroundId", undefined),
-    withHandlers({
-        onVisibilityChange: ({ setBackgroundId = () => { } }) => ({ visible, id }) => visible && setBackgroundId(id)
+    mapPropsStream(props$ => {
+        const { handler: onVisibilityChange, stream: intersection$ } = createEventHandler();
+        return Observable.combineLatest(
+            props$,
+            createBackgroundIdStream(intersection$, props$)
+                .startWith({}), // emit first event from all the streams to allow first rendering
+            // HERE WE CAN PLACE EVERY OTHER INTERSECTION EVENT STREAM HANDLER
+            (...propsParts) => ({
+                ...(propsParts.reduce((props = {}, part) => ({ ...props, ...part }), {})),
+                onVisibilityChange
+            })
+        );
     }),
-    withProps(({ backgroundId, contents = [] }) => ({
-        background: get(getContentIndex(contents, backgroundId) || 0, 'background') || {
-            type: 'none'
-        }
-    }))
+    withPropsOnChange(
+        ['backgroundId, contents'],
+        ({ backgroundId, contents = [] }) => ({
+            background: get(getContentIndex(contents, backgroundId) || 0, 'background') || {
+                type: 'none'
+            }
+        }))
 );
