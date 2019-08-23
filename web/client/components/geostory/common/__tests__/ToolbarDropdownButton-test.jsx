0200951c286aed4835173f45929572db086595fd
Matteo Velludini <matteo.velludini@geo-solutions.it>
2019-08-23 12:09:09 +0200

    fix test

--- web/client/components/geostory/common/__tests__/ToolbarDropdownButton-test.jsx
+++ web/client/components/geostory/common/__tests__/ToolbarDropdownButton-test.jsx
@@ -7,7 +7,6 @@
  */
 import React from 'react';
 import ReactDOM from 'react-dom';
-import ReactTestUtils from 'react-dom/test-utils';
 
 import expect from 'expect';
 import ToolbarDropdownButton from '../ToolbarDropdownButton';
