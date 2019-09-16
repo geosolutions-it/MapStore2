66316227659ccb42660f7e274482335d51b76532
vlt1 <54526151+vlt1@users.noreply.github.com>
2019-09-16 16:23:56 +0300

    #3923 Measurement errors when switching CRS (#4172) (#4198)
    (cherry picked from commit 3fae9010a2b0eef96d2598ce9678f731f6ae8982)

---
+++ web/client/plugins/__tests__/CRSSelector-test.jsx
@@ -0,0 +1,71 @@
+import expect from 'expect';
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import CRSSelectorPlugin from '../CRSSelector';
+import { getPluginForTest } from './pluginsTestUtils';
+
+describe('CRSSelector Plugin', () => {
+    beforeEach((done) => {
+        document.body.innerHTML = '<div id="container"></div>';
+        setTimeout(done);
+    });
+
+    afterEach((done) => {
+        ReactDOM.unmountComponentAtNode(document.getElementById("container"));
+        document.body.innerHTML = '';
+        setTimeout(done);
+    });
+
+    it('Shows CRSSelector Plugin', () => {
+        const { Plugin } = getPluginForTest(CRSSelectorPlugin, {
+            map: {
+                projection: "EPSG:900913"
+            },
+            localConfig: {
+                projectionDefs: []
+            }
+        });
+
+        ReactDOM.render(<Plugin filterAllowedCRS={["EPSG:4326", "EPSG:3857"]} additionalCRS={{}}/>, document.getElementById("container"));
+        expect(document.getElementsByClassName('ms-prj-selector').length).toBe(1);
+    });
+
+    it('CRSSelector is not rendered when Measure Panel is enabled', () => {
+        const { Plugin } = getPluginForTest(CRSSelectorPlugin, {
+            controls: {
+                measure: {
+                    enabled: true
+                }
+            },
+            map: {
+                projection: "EPSG:900913"
+            },
+            localConfig: {
+                projectionDefs: []
+            }
+        });
+
+        ReactDOM.render(<Plugin filterAllowedCRS={["EPSG:4326", "EPSG:3857"]} additionalCRS={{}}/>, document.getElementById("container"));
+        expect(document.getElementsByClassName('ms-prj-selector').length).toBe(0);
+    });
+
+    it('CRSSelector is not rendered when Query Panel is enabled', () => {
+        const { Plugin } = getPluginForTest(CRSSelectorPlugin, {
+            controls: {
+                queryPanel: {
+                    enabled: true
+                }
+            },
+            map: {
+                projection: "EPSG:900913"
+            },
+            localConfig: {
+                projectionDefs: []
+            }
+        });
+
+        ReactDOM.render(<Plugin filterAllowedCRS={["EPSG:4326", "EPSG:3857"]} additionalCRS={{}}/>, document.getElementById("container"));
+        expect(document.getElementsByClassName('ms-prj-selector').length).toBe(0);
+    });
+});
