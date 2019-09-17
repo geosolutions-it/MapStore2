252c6062b73af8a5635bb305ca16878107f1a9eb
Lorenzo Natali <lorenzo.natali@geo-solutions.it>
2019-09-17 11:24:39 +0200

    #4212 Fix Uglify build failure issue (#4213)

--- build/buildConfig.js
+++ build/buildConfig.js
@@ -154,7 +154,15 @@ module.exports = (bundles, themeEntries, paths, extractThemesPlugin, prod, publi
                 use: [{
                     loader: "babel-loader"
                 }],
-                include: paths.code
+                include: [
+                    paths.code,
+                    // the followings are here to skip issues caused by uglifyPlugin (see #4212)
+                    // TODO: a solution to this issue that uses only libs
+                    // like https://www.npmjs.com/package/uglify-js-es6#harmony when stable, terser, babel-minify ...
+                    path.join(__dirname, "..", "node_modules", "query-string"),
+                    path.join(__dirname, "..", "node_modules", "strict-uri-encode"),
+                    path.join(__dirname, "..", "node_modules", "split-on-first")
+                ]
             }
         ].concat(prod ? [{
                 test: /\.html$/,
