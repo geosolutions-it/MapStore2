43a979967a95895801d8f6cd9d30393532c082a8
kaso <kasongoyo@gmail.com>
2019-10-16 17:31:30 +0300

    Fix 4304 Settings modal window text and dropdown problem (#4361)

--- web/client/components/misc/__tests__/FeatureInfoFormatSelector-test.jsx
+++ web/client/components/misc/__tests__/FeatureInfoFormatSelector-test.jsx
@@ -17,7 +17,9 @@ describe('FeatureInfoFormatSelector', () => {
     const data = {
         k0: "v0",
         k1: "v1",
-        k2: "v2"
+        k2: "v2",
+        //  duplicated key value needed to test that featureInfo with same formats are selectable.
+        k3: "v2"
     };
     const defaultVal = data.k1;
 
