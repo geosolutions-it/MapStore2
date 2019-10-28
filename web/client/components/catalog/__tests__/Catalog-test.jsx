8fbc11beb879d2ba65946fc4f4f1a5e5816f20e3
Vladislav Shatilenya <vladislav.shatilenya@yotec.net>
2019-10-28 18:28:51 +0300

    tests added

--- web/client/components/catalog/__tests__/Catalog-test.jsx
+++ web/client/components/catalog/__tests__/Catalog-test.jsx
@@ -9,6 +9,8 @@ const React = require('react');
 const ReactDOM = require('react-dom');
 const expect = require('expect');
 const TestUtils = require('react-dom/test-utils');
+const {FormGroup} = require('react-bootstrap');
+const Select = require('react-select');
 
 const Catalog = require('../Catalog.jsx');
 
@@ -47,5 +49,45 @@ describe('Test Catalog panel', () => {
         expect(buttons.length).toBe(1);
         const searchButton = buttons[0];
         TestUtils.Simulate.click(searchButton);
+    });
+    it('test service format', () => {
+        const formatOptions = [{
+            label: "image/png8",
+            value: "image/png8"
+        }, {
+            label: "image/jpeg",
+            value: "image/jpeg"
+        }];
+        const item = ReactDOM.render(<Catalog
+            services={{"csw": {
+                type: "csw",
+                url: "url",
+                title: "csw",
+                format: "image/png8"
+            }}}
+            selectedService="csw"
+            formatOptions={formatOptions}
+            mode="edit"
+            newService={{
+                url: "url",
+                type: "csw",
+                title: "csw",
+                oldService: "csw",
+                showAdvancedSettings: true,
+                format: "image/png8"
+            }}
+        />, document.getElementById("container"));
+
+        expect(item).toExist();
+
+        const formatFormGroups = TestUtils.scryRenderedComponentsWithType(item, FormGroup).filter(fg => {
+            const labels = TestUtils.scryRenderedDOMComponentsWithTag(fg, 'label');
+            return labels.length === 1 && labels[0].textContent === 'Format';
+        });
+        expect(formatFormGroups.length).toBe(1);
+        const formatSelect = TestUtils.findRenderedComponentWithType(formatFormGroups[0], Select);
+        expect(formatSelect).toExist();
+        expect(formatSelect.props.value).toBe('image/png8');
+        expect(formatSelect.props.options).toEqual(formatOptions);
     });
 });
