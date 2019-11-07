4201c6e0ea75543d05df9b8a2432adc47a0488bc
allyoucanmap <bovio.stefano@gmail.com>
2019-11-07 16:36:45 +0100

    add support for custom node in toc plugin

--- web/client/plugins/TOC.jsx
+++ web/client/plugins/TOC.jsx
@@ -213,7 +213,9 @@ class LayerTree extends React.Component {
         activateLayerFilterTool: PropTypes.bool,
         catalogActive: PropTypes.bool,
         refreshLayerVersion: PropTypes.func,
-        hideOpacityTooltip: PropTypes.bool
+        hideOpacityTooltip: PropTypes.bool,
+        layerNodeComponent: PropTypes.func,
+        groupNodeComponent: PropTypes.func
     };
 
     static contextTypes = {
@@ -298,8 +300,9 @@ class LayerTree extends React.Component {
     };
 
     getDefaultGroup = () => {
+        const GroupNode = this.props.groupNodeComponent || DefaultGroup;
         return (
-            <DefaultGroup
+            <GroupNode
                 onSort={!this.props.filterText && this.props.activateSortLayer ? this.props.onSort : null}
                 {...this.props.groupOptions}
                 titleTooltip={this.props.activateTitleTooltip}
@@ -314,8 +317,9 @@ class LayerTree extends React.Component {
     }
 
     getDefaultLayer = () => {
+        const LayerNode = this.props.layerNodeComponent || DefaultLayer;
         return (
-            <DefaultLayer
+            <LayerNode
                 {...this.props.layerOptions}
                 titleTooltip={this.props.activateTitleTooltip}
                 showFullTitleOnExpand={this.props.showFullTitleOnExpand}
@@ -492,6 +496,8 @@ class LayerTree extends React.Component {
  * @prop {boolean} cfg.showFullTitleOnExpand shows full length title in the legend. default `false`.
  * @prop {boolean} cfg.hideOpacityTooltip hide toolip on opacity sliders
  * @prop {string[]|string|object|function} cfg.metadataTemplate custom template for displaying metadata
+ * @prop {element} cfg.groupNodeComponent render a custom component for group node
+ * @prop {element} cfg.layerNodeComponent render a custom component for layer node
  * example :
  * ```
  * {
