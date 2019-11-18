817ac3a1887d9a62b58ca993000e4803677e6a5d
Krzysztof Boczkowski <krzysztof.boczkowski@clurgo.com>
2019-11-18 09:07:09 +0100

    fix: added missing copyrights

--- web/client/components/geostory/contents/__tests__/WebPage-test.jsx
+++ web/client/components/geostory/contents/__tests__/WebPage-test.jsx
@@ -1,8 +1,14 @@
+/*
+ * Copyright 2019, GeoSolutions Sas.
+ * All rights reserved.
+ *
+ * This source code is licensed under the BSD-style license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
 import React from 'react';
 import ReactDOM from 'react-dom';
 import expect from 'expect';
 import WebPage from '../WebPage';
-// import ReactTestUtils from 'react-dom/test-utils';
 
 describe('WebPage component', () => {
     beforeEach((done) => {
