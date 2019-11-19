f31487d626b33f4012009fcf1685e661a7775511
Lorenzo Natali <offtherailz@gmail.com>
2019-11-19 14:08:45 +0100

    Apply suggestions from code review
    Co-Authored-By: Matteo V. <matteo.velludini.geosolutions@gmail.com>

--- web/client/plugins/__tests__/ContextCreator-test.jsx
+++ web/client/plugins/__tests__/ContextCreator-test.jsx
@@ -1,3 +1,12 @@
+/**
+ * Copyright 2019, GeoSolutions Sas.
+ * All rights reserved.
+ *
+ * This source code is licensed under the BSD-style license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+
 import React from 'react';
 import ReactDOM from 'react-dom';
 import ReactTestUtils from 'react-dom/test-utils';
