78401035cf915a8e86008f23969d883c6220ce3a
Matteo V <matteo.velludini@geo-solutions.it>
2020-01-23 12:58:30 +0100

    Fix 4781 create new map button is now configurable (#4782)

--- web/client/plugins/dashboard/EmptyDashboardsView.jsx
+++ web/client/plugins/dashboard/EmptyDashboardsView.jsx
@@ -17,14 +17,18 @@ const Message = require('../../components/I18N/Message');
 
 class EmptyDashboards extends React.Component {
     static propTypes = {
-        loggedIn: PropTypes.bool
+        loggedIn: PropTypes.bool,
+        showCreateButton: PropTypes.bool
     }
     static contextTypes = {
         router: PropTypes.object
     };
+    static defaultProps = {
+        showCreateButton: true
+    }
 
     render() {
-        return (<div style={{width: "100%", textAlign: "center"}}>{this.props.loggedIn
+        return (<div style={{width: "100%", textAlign: "center"}}>{this.props.loggedIn && this.props.showCreateButton
             ? (<Button bsStyle="primary" onClick={() => { this.context.router.history.push("/dashboard"); }}>
                 <Message msgId="resources.dashboards.createANewOne" />
             </Button>)
