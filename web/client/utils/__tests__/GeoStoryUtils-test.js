70002be5d375d4951ed21a90ebc75f1fcfb41d10
Andrea <kappu72@gmail.com>
2020-02-05 12:17:47 +0100

    Fixed current story map search (#4836)
    * Fixed current story map search
    
    * Added search to description property

--- web/client/utils/__tests__/GeoStoryUtils-test.js
+++ web/client/utils/__tests__/GeoStoryUtils-test.js
@@ -316,6 +316,16 @@ describe("GeoStory Utils", () => {
         expect(filterResources(resources, "re").length).toBe(2);
         expect(filterResources(resources, "e").length).toBe(3);
     });
+    it('test filterResources without title but name', () => {
+        const resources = [{data: {name: "res1"}}, {data: {name: "res2"}}, {data: {title: "not matching title"}}];
+        expect(filterResources(resources, "re").length).toBe(2);
+        expect(filterResources(resources, "e").length).toBe(3);
+    });
+    it('test filterResources with title and description', () => {
+        const resources = [{data: {title: "res1"}}, {data: {description: "res2"}}, {data: {title: "not matching title"}}];
+        expect(filterResources(resources, "re").length).toBe(2);
+        expect(filterResources(resources, "e").length).toBe(3);
+    });
     it('test isWebPageSection', () => {
         const element = {
             type: SectionTypes.PARAGRAPH,
