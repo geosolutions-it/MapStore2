0a7797e205d6a567f2eaa92640c93bb7d04e4e27
Lorenzo Natali <lorenzo.natali@geo-solutions.it>
2020-02-12 14:39:20 +0100

    #4876. Introduced noRoot option to skip root rendering playback (#4890)

--- web/client/components/plugins/__tests__/PluginsContainer-test.jsx
+++ web/client/components/plugins/__tests__/PluginsContainer-test.jsx
@@ -6,6 +6,8 @@
  * LICENSE file in the root directory of this source tree.
  */
 const expect = require('expect');
+const assign = require('object-assign');
+const PropTypes = require('prop-types');
 
 const React = require('react');
 const ReactDOM = require('react-dom');
@@ -17,9 +19,41 @@ class My extends React.Component {
     }
     myFunc() {}
 }
+class Container extends React.Component {
+    static propTypes = {
+        items: PropTypes.any
+    }
+    render() {
+        const {items} = this.props;
+        const renderCmp = (item = {}) => {
+            const ItemCmp = item.plugin;
+            if (ItemCmp) {
+                return <ItemCmp />;
+            }
+            return <div id={`no-impl-item-${item.name}`} />;
+
+        };
+        return (<React.Fragment>
+            {items.map(renderCmp)}
+        </React.Fragment>);
+    }
+
+}
+
+class NoRootPlugin extends React.Component {
+    render() {
+        return <div id="no-root"/>;
+    }
+}
 const plugins = {
     MyPlugin: My,
-    OtherPlugin: My
+    OtherPlugin: My,
+    ContainerPlugin: Container,
+    NoRootPlugin: assign(NoRootPlugin, { noRoot: true, Container: {
+        name: 'no-root-plugin',
+        position: 1,
+        priority: 1
+    }})
 };
 
 const pluginsCfg = {
@@ -33,6 +67,13 @@ const pluginsCfg = {
 
 const pluginsCfg2 = {
     desktop: ["My", "Other"]
+};
+
+const pluginsCfg3 = {
+    desktop: ["My", "NoRoot"]
+};
+const pluginsCfg4 = {
+    desktop: ["Container", "NoRoot"]
 };
 
 describe('PluginsContainer', () => {
@@ -69,5 +110,29 @@ describe('PluginsContainer', () => {
 
         const rendered = cmpDom.getElementsByTagName("div");
         expect(rendered.length).toBe(2);
+    });
+    it('test noRoot option disable root rendering of plugins', () => {
+        // Not rendered without container
+        let cmp = ReactDOM.render(<PluginsContainer mode="desktop" defaultMode="desktop" params={{}}
+            plugins={plugins} pluginsConfig={pluginsCfg3} />, document.getElementById("container"));
+        expect(cmp).toExist();
+
+        let cmpDom = ReactDOM.findDOMNode(cmp);
+        expect(cmpDom).toExist();
+
+        let rendered = cmpDom.getElementsByTagName("div");
+
+        // rendered in container
+        expect(rendered.length).toBe(1);
+        cmp = ReactDOM.render(<PluginsContainer mode="desktop" defaultMode="desktop" params={{}}
+            plugins={plugins} pluginsConfig={pluginsCfg4} />, document.getElementById("container"));
+        expect(cmp).toExist();
+
+        cmpDom = ReactDOM.findDOMNode(cmp);
+        expect(cmpDom).toExist();
+
+        rendered = cmpDom.getElementsByTagName("div");
+        expect(document.getElementById('no-impl-item-no-root-plugin')).toNotExist();
+        expect(document.getElementById('no-root')).toExist();
     });
 });
