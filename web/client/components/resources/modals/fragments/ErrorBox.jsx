2c1a28e3d0458445349ab3713fcece0c258942e5
mv88 <matteo.velludini.geosolutions@gmail.com>
2020-02-18 11:23:27 +0100

    Add tests, and the statusText forbidden in locales

--- web/client/components/resources/modals/fragments/ErrorBox.jsx
+++ web/client/components/resources/modals/fragments/ErrorBox.jsx
@@ -10,7 +10,9 @@ const React = require('react');
 const DEFAULT_MESSAGES = {
     "FORMAT": "map.errorFormat",
     "SIZE": "map.errorSize",
-    409: "dashboard.errors.resourceAlreadyExists"
+    409: "dashboard.errors.resourceAlreadyExists",
+    403: "dashboard.errors.forbidden",
+    405: "dashboard.errors.forbidden405"
 };
 
 const Message = require('../../../I18N/Message');
