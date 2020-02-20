70161a1429d7611ab4fe9be0c97fca79d8b49a4f
allyoucanmap <bovio.stefano@gmail.com>
2020-02-20 14:57:31 +0100

    removed double copyright

--- web/client/hooks/__tests__/usePlugins-test.js
+++ web/client/hooks/__tests__/usePlugins-test.js
@@ -5,13 +5,6 @@
  * This source code is licensed under the BSD-style license found in the
  * LICENSE file in the root directory of this source tree.
  */
-/*
- * Copyright 2020, GeoSolutions Sas.
- * All rights reserved.
- *
- * This source code is licensed under the BSD-style license found in the
- * LICENSE file in the root directory of this source tree.
- */
 
 import expect from 'expect';
 import React from 'react';
