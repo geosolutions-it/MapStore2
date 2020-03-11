3c69b894fa4e5d90be78ae8656f9f3b162826d99
Lorenzo Natali <offtherailz@gmail.com>
2020-03-11 12:03:35 +0100

    Update web/client/reducers/usersession.js

--- web/client/reducers/usersession.js
+++ web/client/reducers/usersession.js
@@ -1,3 +1,10 @@
+/*
+ * Copyright 2020, GeoSolutions Sas.
+ * All rights reserved.
+ *
+ * This source code is licensed under the BSD-style license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
 import { USER_SESSION_SAVED, USER_SESSION_LOADING } from "../actions/usersession";
 
 export default (state = {}, action) => {
