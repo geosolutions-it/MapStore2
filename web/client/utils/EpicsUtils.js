f45c72315a14ff9e774f619f573e18541e2d3a85
Lorenzo Natali <lorenzo.natali@geo-solutions.it>
2020-03-19 12:31:30 +0100

    fixed tests and signature

--- web/client/utils/EpicsUtils.js
+++ web/client/utils/EpicsUtils.js
@@ -3,13 +3,14 @@
  * It allows also to identify the error showing in console the name of the epic that triggered the exception and the error.
  * At the end, it throws the exception again so it can be automatically intercepted in dev tools.
  * @memberof utils.EpicsUtils
- * @param {string} epicName the name of the epic
- * @returns {function} function that wraps the epic adding error handling functionality
+ * @param {epic} the epic to wrap
+ * @param {string} [key] the name of the epic
+ * @returns {epic} the epic with error handling functionalities
  */
-const defaultEpicWrapper = k => epic => (...args) =>
+const defaultEpicWrapper = (epic, k = "--unknown--") => (...args) =>
     epic(...args).catch((error, source) => {
         // eslint-disable-next-line
-        console.error(`Error in epic ${k}. Source`, error);
+        console.error(`Error in epic "${k}". Source`, error);
         setTimeout(() => {
             // throw anyway error
             throw error;
@@ -25,4 +26,4 @@ const defaultEpicWrapper = k => epic => (...args) =>
  * @return {array} the wrapped epics list as an array (usable as an input to redux-observable combineEpics function).
  */
 export const wrapEpics = (epics, wrapper = defaultEpicWrapper) =>
-    Object.keys(epics).map(k => wrapper(k)(epics[k]) );
+    Object.keys(epics).map(k => wrapper(epics[k], k));
