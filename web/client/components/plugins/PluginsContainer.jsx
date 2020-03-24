aa6ec45bf3e7ee71467da0f70970e76b34a50a78
Lorenzo Natali <lorenzo.natali@geo-solutions.it>
2020-03-24 14:26:44 +0100

    Update PluginsContainer.jsx

--- web/client/components/plugins/PluginsContainer.jsx
+++ web/client/components/plugins/PluginsContainer.jsx
@@ -89,21 +89,20 @@ class PluginsContainer extends React.Component {
         this.loadPlugins(this.props.pluginsState, this.props);
     }
 
-    UNSAFE_componentWillReceiveProps(newProps) {
-        this.loadPlugins(newProps.pluginsState, newProps);
-    }
-
     shouldComponentUpdate(nextProps, nextState) {
-        return nextProps.pluginsConfig !== this.props.pluginsConfig
-            || nextProps.plugins !== this.props.plugins
+        return !isEqual(nextProps.pluginsConfig, this.props.pluginsConfig)
+            || !isEqual(nextProps.plugins, this.props.plugins)
             || nextProps.params !== this.props.params
             || nextProps.mode !== this.props.mode
-            || nextProps.monitoredState !== this.props.monitoredState
+            || !isEqual(nextProps.monitoredState, this.props.monitoredState)
             || nextProps.className !== this.props.className
             || nextProps.style !== this.props.style
             || nextProps.defaultMode !== this.props.defaultMode
             || !isEqual(nextProps.pluginsState, this.props.pluginsState)
             || !isEqual(nextState.loadedPlugins, this.state.loadedPlugins);
+    }
+    UNSAFE_componentWillUpdate(newProps) {
+        this.loadPlugins(newProps.pluginsState, newProps);
     }
 
     getState = (path, newProps) => {
