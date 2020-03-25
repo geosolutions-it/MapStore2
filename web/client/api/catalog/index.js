77aedd8124d71f97c458fe01099591b14d8b2f24
Lorenzo Natali <lorenzo.natali@geo-solutions.it>
2020-03-25 14:26:26 +0100

    improved catalog API

--- web/client/api/catalog/index.js
+++ web/client/api/catalog/index.js
@@ -16,9 +16,24 @@ import { validate, testService } from './common';
 
 
 /**
- * API for catalog
- * Must implement:
- *
+ * APIs collection for catalog.
+ * Each entry must implement:
+ * - `textSearch` (url, startPosition, maxRecords, text) => function that returns a promise. The promise emits an object with this shape:
+ * ```javascript
+ * {
+ *      numberOfRecordsMatched: 20
+ *      numberOfRecordsReturned: 5
+ *      records: [{}] // records
+ *      service: {
+ *          // some information about the service
+ *      }
+ * }
+ * ```
+ * Optionally implements validation functions:
+ * - `validate`: function that gets the service object and returns an Observable. The stream emit an exception if the service validation fails. Otherwise it emits the service object.
+ * - `testService` function that gets the service object and returns an Observable. The stream emit an exception if the service do not respond. Otherwise it emits the service object.
+ * @memberof api
+ * @name catalog
  */
 export default {
     csw: {
