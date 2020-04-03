51ee1458d48fb2241bb71bc54cd1c297aa0fe83f
Mauro Bartolomeoli <maurobartolomeoli@gmail.com>
2020-04-03 16:46:14 +0200

    #5121: preventive client-side validation of uploaded extensions

---
+++ web/client/utils/__tests__/ExtensionsUtils-test.js
@@ -0,0 +1,62 @@
+/*
+ * Copyright 2020, GeoSolutions Sas.
+ * All rights reserved.
+ *
+ * This source code is licensed under the BSD-style license found in the
+ * LICENSE file in the root directory of this source tree.
+*/
+import expect from "expect";
+import {checkZipBundle, ERROR} from "../ExtensionsUtils";
+import axios from '../../libs/ajax';
+
+describe('checkZipBundle', () => {
+    it('throws an error if uploaded zip does not contain index.json', (done) => {
+        axios.get("base/web/client/test-resources/extensions/myplugin_no_index.zip", { responseType: "blob" }).then(({data}) => {
+            checkZipBundle(data).catch(e => {
+                expect(e).toBe(ERROR.MISSING_INDEX);
+                done();
+            });
+        });
+    });
+    it('throws an error if uploaded zip contains a malformed index.json', (done) => {
+        axios.get("base/web/client/test-resources/extensions/myplugin_wrong_json.zip", { responseType: "blob" }).then(({data}) => {
+            checkZipBundle(data).catch(e => {
+                expect(e).toBe(ERROR.MALFORMED_INDEX);
+                done();
+            });
+        });
+    });
+    it('throws an error if uploaded zip does not contain a js bundle', (done) => {
+        axios.get("base/web/client/test-resources/extensions/myplugin_no_js.zip", { responseType: "blob" }).then(({data}) => {
+            checkZipBundle(data).catch(e => {
+                expect(e).toBe(ERROR.MISSING_BUNDLE);
+                done();
+            });
+        });
+    });
+    it('throws an error if uploaded zip index.json does not contain a plugin', (done) => {
+        axios.get("base/web/client/test-resources/extensions/myplugin_no_plugin.zip", { responseType: "blob" }).then(({data}) => {
+            checkZipBundle(data).catch(e => {
+                expect(e).toBe(ERROR.MISSING_PLUGIN);
+                done();
+            });
+        });
+    });
+    it('throws an error if uploaded zip contains more than one js file', (done) => {
+        axios.get("base/web/client/test-resources/extensions/myplugin_too_many_bundles.zip", { responseType: "blob" }).then(({data}) => {
+            checkZipBundle(data).catch(e => {
+                expect(e).toBe(ERROR.TOO_MANY_BUNDLES);
+                done();
+            });
+        });
+    });
+    it('return the file to upload if everything is ok', (done) => {
+        axios.get("base/web/client/test-resources/extensions/myplugin.zip", { responseType: "blob" }).then(({data}) => {
+            checkZipBundle(data).then(extension => {
+                expect(extension).toExist();
+                expect(extension.name).toBe("My");
+                done();
+            });
+        });
+    });
+});
