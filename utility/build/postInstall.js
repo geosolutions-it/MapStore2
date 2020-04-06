cc949a8afd7fed597b70af5e1d8cebb967a468a8
Andrea Cappugi <kappu72@gmail.com>
2020-04-06 14:00:57 +0200

    fixed mocha path

--- utility/build/postInstall.js
+++ utility/build/postInstall.js
@@ -4,4 +4,4 @@ const dirPrefix = isInProject ? '..' : '.'
 fs.removeSync(`${dirPrefix}/node_modules/leaflet-simple-graticule/node_modules`);
 fs.removeSync(`${dirPrefix}/node_modules/react-sortable-items/node_modules/react-dom`);
 fs.emptyDirSync(`${dirPrefix}/node_modules/mocha`);
-fs.copySync(`${dirPrefix}/node_modules/mocha`, `${dirPrefix}/node_modules/@geosolutions/mocha node_modules/mocha`);
+fs.copySync(`${dirPrefix}/node_modules/mocha`, `${dirPrefix}/node_modules/@geosolutions/mocha ${dirPrefix}/node_modules/mocha`);
