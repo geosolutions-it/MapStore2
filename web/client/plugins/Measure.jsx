9388521e64afaaabee107f2128efb1d9aa4747ec
Surendhar Dharmalingam <dsuren1@gmail.colm>
2020-04-24 12:06:39 +0530

    C125-145: JSDoc for measure true bearing configuration options

--- web/client/plugins/Measure.jsx
+++ web/client/plugins/Measure.jsx
@@ -56,14 +56,24 @@ const toggleMeasureTool = toggleControl.bind(null, 'measure', null);
  * @class
  * @name Measure
  * @memberof plugins
- * @prop {boolean} showResults shows the measure in the panel itself.
- * @prop {object} defaultOptions these are the options used to initialize the state of the Measure plugin, defaulti is {}
- * @prop {boolean} defaultOptions.showCoordinateEditor if true, tells the component to render the CoordinateEditor in a side panel otherwise it will render a modal without it, default is false
- * @prop {boolean} defaultOptions.showAddAsAnnotation if true, shows the button addAsAnnotation in the toolbar
- * @prop {boolean} defaultOptions.geomType geomType for the measure tool, can be "LineString" or "Bearing" or "Polygon", default is "LineString"
- * @prop {boolean} defaultOptions.format "decimal" of "aeronautical" format used for coordinate editor, default is "decimal"
- * @prop {boolean} defaultOptions.showLabel determines, whether to show measurement labels(like area for polygons)
- * @prop {boolean} defaultOptions.showSegmentLengths determines, whether to show segment labels(of line segments for LineString, for example)
+ * @prop {boolean} showResults: shows the measure in the panel itself.
+ * @prop {object} defaultOptions: these are the options used to initialize the state of the Measure plugin, defaulti is {}
+ * @prop {boolean} defaultOptions.showCoordinateEditor: if true, tells the component to render the CoordinateEditor in a side panel otherwise it will render a modal without it, default is false
+ * @prop {boolean} defaultOptions.showAddAsAnnotation: if true, shows the button addAsAnnotation in the toolbar
+ * @prop {object} defaultOptions.trueBearing: allows measurement configuration of angular distance from true north to the object. ISO Spec (https://www.sis.se/api/document/preview/905247/)
+ * @prop {boolean} defaultOptions.trueBearing.measureTrueBearing: if true, displays the measurement in true bearing (000° T).
+ * @prop {integer} defaultOptions.trueBearing.fractionDigits: Value denotes the fractional digit to used for the representation of true bearing.
+ * For example this enables measurement in true bearing with fractional digits of 2 (000.00° - 359.99° T)
+ * ```
+ * "trueBearing": {
+ *  "measureTrueBearing": true,
+ *  "fractionDigits": 2
+ *  }
+ * ```
+ * @prop {boolean} defaultOptions.geomType: geomType for the measure tool, can be "LineString" or "Bearing" or "Polygon", default is "LineString"
+ * @prop {boolean} defaultOptions.format: "decimal" of "aeronautical" format used for coordinate editor, default is "decimal"
+ * @prop {boolean} defaultOptions.showLabel: determines, whether to show measurement labels(like area for polygons)
+ * @prop {boolean} defaultOptions.showSegmentLengths: determines, whether to show segment labels(of line segments for LineString, for example)
   */
 const Measure = connect(
     createSelector([
