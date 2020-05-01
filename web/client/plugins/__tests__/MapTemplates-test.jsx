5c3d0a57bc68df87af257838c139d6e02a5458e2
Vladislav Shatilenya <vladislav.shatilenya@yotec.net>
2020-05-01 15:53:49 +0300

    Solves Map Template favourite UI issue

--- web/client/plugins/__tests__/MapTemplates-test.jsx
+++ web/client/plugins/__tests__/MapTemplates-test.jsx
@@ -51,4 +51,62 @@ describe('MapTemplates Plugins', () => {
         expect(document.getElementsByClassName('map-templates-loader')[0]).toNotExist();
         expect(document.getElementsByClassName('map-templates-panel')[0]).toExist();
     });
+    it('MapTemplatesPanel with template with format', () => {
+        const template = {
+            id: 1,
+            format: 'wmc'
+        };
+        const { Plugin } = getPluginForTest(MapTemplates, {
+            controls: {
+                mapTemplates: {
+                    enabled: true
+                }
+            },
+            maptemplates: {
+                mapTemplatesLoaded: true,
+                templates: [template]
+            }
+        });
+        ReactDOM.render(<Plugin/>, document.getElementById("container"));
+        const sideCards = document.getElementsByClassName('mapstore-side-card');
+        expect(sideCards.length).toBe(1);
+        const formatIcon = document.getElementsByClassName('glyphicon-ext-wmc')[0];
+        expect(formatIcon).toExist();
+    });
+    it('MapTemplatesPanel with favourite template with format', () => {
+        const template = {
+            id: 1,
+            format: 'wmc',
+            favourite: true,
+            name: 'Map Template',
+            description: 'This is a map template.'
+        };
+        const { Plugin } = getPluginForTest(MapTemplates, {
+            controls: {
+                mapTemplates: {
+                    enabled: true
+                }
+            },
+            maptemplates: {
+                mapTemplatesLoaded: true,
+                templates: [template]
+            }
+        });
+        ReactDOM.render(<Plugin/>, document.getElementById("container"));
+        const sideCards = document.getElementsByClassName('mapstore-side-card');
+        expect(sideCards.length).toBe(2);
+        const formatIcons = document.getElementsByClassName('glyphicon-ext-wmc');
+        expect(formatIcons.length).toBe(2);
+        const favouriteIcon = document.getElementsByClassName('glyphicon-star');
+        expect(favouriteIcon).toExist();
+        const favouriteIconEmpty = document.getElementsByClassName('glyphicon-star-empty');
+        expect(favouriteIconEmpty).toExist();
+        const favouriteCard = document.querySelector('.mapstore-side-card.ms-sm');
+        expect(favouriteCard).toExist();
+        const leftContainer = favouriteCard.getElementsByClassName('mapstore-side-card-left-container')[0];
+        expect(leftContainer).toExist();
+        const leftContainerStyle = getComputedStyle(leftContainer);
+        expect(leftContainerStyle).toExist();
+        expect(leftContainerStyle["flex-direction"]).toBe('row');
+    });
 });
