22cac7b1f921b0f6376370c4854fe4188d8ec5f4
stefano bovio <bovio.stefano@gmail.com>
2020-06-10 18:27:25 +0200

    hide dropdown of story buttons if map is in edit mode (#5450)

--- web/client/components/geostory/common/__tests__/ToolbarDropdownButton-test.jsx
+++ web/client/components/geostory/common/__tests__/ToolbarDropdownButton-test.jsx
@@ -10,6 +10,7 @@ import ReactDOM from 'react-dom';
 
 import expect from 'expect';
 import ToolbarDropdownButton from '../ToolbarDropdownButton';
+import { act, Simulate } from 'react-dom/test-utils';
 describe('ToolbarDropdownButton component', () => {
     beforeEach((done) => {
         document.body.innerHTML = '<div id="container"></div>';
@@ -24,6 +25,43 @@ describe('ToolbarDropdownButton component', () => {
         ReactDOM.render(<ToolbarDropdownButton />, document.getElementById("container"));
         const container = document.getElementById('container');
         const el = container.querySelector('.square-button-md.no-border');
-        expect(el).toExist();
+        expect(el).toBeTruthy();
+    });
+    it('should hide dropdown when disabled', () => {
+        ReactDOM.render(<ToolbarDropdownButton
+            glyph="small"
+            options={[{
+                value: 'value',
+                label: 'Label'
+            }]}
+        />, document.getElementById("container"));
+        const container = document.getElementById('container');
+        const buttonNode = container.querySelector('.square-button-md.no-border');
+        expect(buttonNode).toBeTruthy();
+        let dropdownMenuNode = container.querySelector('.dropdown.open');
+        expect(dropdownMenuNode).toBeFalsy();
+
+        // open dropdown
+        act(() => {
+            Simulate.click(buttonNode);
+        });
+
+        dropdownMenuNode = container.querySelector('.dropdown.open');
+        expect(dropdownMenuNode).toBeTruthy();
+
+        // disabled component
+        act(() => {
+            ReactDOM.render(<ToolbarDropdownButton
+                disabled
+                glyph="small"
+                options={[{
+                    value: 'value',
+                    label: 'Label'
+                }]}
+            />, document.getElementById("container"));
+        });
+
+        dropdownMenuNode = container.querySelector('.dropdown.open');
+        expect(dropdownMenuNode).toBeFalsy();
     });
 });
