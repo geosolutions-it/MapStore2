efef71905120f131e77fcbb4f1af9e843d499b79
mv88 <matteo.velludini.geosolutions@gmail.com>
2020-07-02 12:16:10 +0200

    #5353 improve featuregrid toolbar doc

--- web/client/components/data/featuregrid/toolbars/Toolbar.jsx
+++ web/client/components/data/featuregrid/toolbars/Toolbar.jsx
@@ -19,11 +19,43 @@ const getSaveMessageId = ({saving, saved}) => {
 /**
  * Standard Toolbar for the FeatureGrid plugin.
  *
+ * @param {bool} disableToolbar if true it disables all the buttons in the toolbar
+ * @param {bool} disableDownload if true it disables the Download button
+ * @param {bool} disableZoomAll if true it disables the ZoomAll button (defaults to false)
+ * @param {bool} displayDownload used to set visibility of download button
  * @param {bool} showAdvancedFilterButton shows / hide the advanced filter button (defaults to true)
+ * @param {bool} showChartButton shows / hide the Chart (widget) button (defaults to true)
  * @param {bool} showSyncOnMapButton shows / hide the show on map button (defaults to true)
- */
-module.exports = ({ events = {}, syncPopover = { showPopoverSync: true, dockSize: "32.2%" }, mode = "VIEW",
-    showAdvancedFilterButton = true, showSyncOnMapButton = true, showChartButton = true, selectedCount, hasChanges, hasGeometry, hasNewFeatures, isSimpleGeom, isDrawing = false, isEditingAllowed, saving = false, saved = false, isDownloadOpen, isColumnsOpen, disableToolbar, isSearchAllowed, disableDownload, displayDownload, isSyncActive = false, hasSupportedGeometry = true, disableZoomAll = false, isFilterActive = false, showTimeSyncButton = false, timeSync = false} = {}) => {
+ * @param {bool} showTimeSyncButton shows / hide the timeSync button (defaults to false)
+*/
+module.exports = ({
+    disableToolbar,
+    disableDownload,
+    disableZoomAll = false,
+    displayDownload,
+    events = {},
+    hasChanges,
+    hasGeometry,
+    hasNewFeatures,
+    hasSupportedGeometry = true,
+    isColumnsOpen,
+    isDrawing = false,
+    isEditingAllowed,
+    isFilterActive = false,
+    isDownloadOpen,
+    isSearchAllowed,
+    isSimpleGeom,
+    isSyncActive = false,
+    saved = false,
+    saving = false,
+    selectedCount,
+    showAdvancedFilterButton = true,
+    showChartButton = true,
+    showSyncOnMapButton = true,
+    showTimeSyncButton = false,
+    syncPopover = { showPopoverSync: true, dockSize: "32.2%" }, mode = "VIEW",
+    timeSync = false
+} = {}) => {
     return (<ButtonGroup id="featuregrid-toolbar" className="featuregrid-toolbar featuregrid-toolbar-margin">
         <TButton
             id="edit-mode"
