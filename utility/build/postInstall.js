b7b859f03b617ec9c9de7bf0bd2e01953b47329c
Matteo V <matteo.velludini@geo-solutions.it>
2020-08-14 17:14:24 +0200

    #5613 creating a new story now works on newly created project (#5790)

--- utility/build/postInstall.js
+++ utility/build/postInstall.js
@@ -1,6 +1,6 @@
 const fs = require('fs-extra');
 const isInProject = !fs.existsSync('./node_modules/cesium');
-const dirPrefix = isInProject ? '..' : '.'
+const dirPrefix = isInProject ? '..' : '.';
 fs.removeSync(`${dirPrefix}/node_modules/leaflet-simple-graticule/node_modules`);
 fs.removeSync(`${dirPrefix}/node_modules/react-sortable-items/node_modules/react-dom`);
 fs.emptyDirSync(`${dirPrefix}/node_modules/mocha`);
