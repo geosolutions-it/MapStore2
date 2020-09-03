3206014a7b34e4e311137a25ab060c39444ff594
Lorenzo Natali <lorenzo.natali@geo-solutions.it>
2020-09-03 12:15:29 +0200

    #5819: Fixed version of terrestris dependencies (#5820)

--- utility/build/postInstall.js
+++ utility/build/postInstall.js
@@ -3,5 +3,6 @@ const isInProject = !fs.existsSync('./node_modules/cesium');
 const dirPrefix = isInProject ? '..' : '.'
 fs.removeSync(`${dirPrefix}/node_modules/leaflet-simple-graticule/node_modules`);
 fs.removeSync(`${dirPrefix}/node_modules/react-sortable-items/node_modules/react-dom`);
+fs.removeSync(`${dirPrefix}/node_modules/geostyler-openlayers-parser/node_modules/@terrestris`); // explicit dependency in package.json
 fs.emptyDirSync(`${dirPrefix}/node_modules/mocha`);
 fs.copySync(`${dirPrefix}/node_modules/@geosolutions/mocha`, `${dirPrefix}/node_modules/mocha`);
