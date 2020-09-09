74e2d1bfcdac783c54469d6226d590557b6d3ecb
Suren <dsuren1@gmail.com>
2020-09-09 13:41:04 +0530

    #5773: Fix issues with GFI popup in edit mode (#5787)

--- web/client/reducers/__tests__/mapInfo-test.js
+++ web/client/reducers/__tests__/mapInfo-test.js
@@ -15,7 +15,7 @@ const assign = require('object-assign');
 require('babel-polyfill');
 
 describe('Test the mapInfo reducer', () => {
-    let appState = {requests: [{reqId: 10, request: "test"}, {reqId: 11, request: "test1"}]};
+    let appState = {configuration: {infoFormat: 'text/plain'}, responses: [], requests: [{reqId: 10, request: "test"}, {reqId: 11, request: "test1"}]};
 
     it('returns original state on unrecognized action', () => {
         let state = mapInfo(1, {type: 'UNKNOWN'});
@@ -33,24 +33,17 @@ describe('Test the mapInfo reducer', () => {
 
         let state = mapInfo( appState, testAction);
         expect(state.responses).toExist();
-        expect(state.responses.length).toBe(1);
-        expect(state.responses[0].response).toBe("error");
-        expect(state.responses[0].queryParams).toBe("params");
-        expect(state.responses[0].layerMetadata).toBe("meta");
+        expect(state.responses.length).toBe(0);
 
         state = mapInfo(assign({}, appState, {responses: []}), testAction);
         expect(state.responses).toExist();
-        expect(state.responses.length).toBe(1);
-        expect(state.responses[0].response).toBe("error");
-        expect(state.responses[0].queryParams).toBe("params");
-        expect(state.responses[0].layerMetadata).toBe("meta");
+        expect(state.responses.length).toBe(0);
+
         state = mapInfo(assign({}, appState, {responses: ["test"]}), {...testAction, reqId: 11});
         expect(state.responses).toExist();
-        expect(state.responses.length).toBe(2);
+        expect(state.responses.length).toBe(1);
         expect(state.responses[0]).toBe("test");
-        expect(state.responses[1].response).toBe("error");
-        expect(state.responses[1].queryParams).toBe("params");
-        expect(state.responses[1].layerMetadata).toBe("meta");
+        expect(state.responses[1]).toBeFalsy();
     });
 
     it('creates an wms feature info exception', () => {
@@ -63,31 +56,21 @@ describe('Test the mapInfo reducer', () => {
         };
 
         let state = mapInfo(appState, testAction);
-        expect(state.responses).toExist();
-        expect(state.responses.length).toBe(1);
-        expect(state.responses[0].response).toBe("exception");
-        expect(state.responses[0].queryParams).toBe("params");
-        expect(state.responses[0].layerMetadata).toBe("meta");
+        expect(state.responses).toBeTruthy();
+        expect(state.responses.length).toBe(0);
 
         state = mapInfo(assign({}, appState, {responses: []}), testAction);
         expect(state.responses).toExist();
-        expect(state.responses.length).toBe(1);
-        expect(state.responses[0].response).toBe("exception");
-        expect(state.responses[0].queryParams).toBe("params");
-        expect(state.responses[0].layerMetadata).toBe("meta");
-
+        expect(state.responses.length).toBe(0);
 
         state = mapInfo(assign({}, appState, {responses: ["test"]}), {...testAction, reqId: 11});
         expect(state.responses).toExist();
-        expect(state.responses.length).toBe(2);
+        expect(state.responses.length).toBe(1);
         expect(state.responses[0]).toBe("test");
-        expect(state.responses[1].response).toBe("exception");
-        expect(state.responses[1].queryParams).toBe("params");
-        expect(state.responses[1].layerMetadata).toBe("meta");
 
     });
 
-    it('creates a feature info data from succesfull request', () => {
+    it('creates a feature info data from successful request', () => {
         let testAction = {
             type: 'LOAD_FEATURE_INFO',
             data: "data",
@@ -102,6 +85,7 @@ describe('Test the mapInfo reducer', () => {
         expect(state.responses[0].response).toBe("data");
         expect(state.responses[0].queryParams).toBe("params");
         expect(state.responses[0].layerMetadata).toBe("meta");
+        expect(state.index).toBe(0);
 
         state = mapInfo(assign({}, appState, {responses: []}), testAction);
         expect(state.responses).toExist();
@@ -109,14 +93,16 @@ describe('Test the mapInfo reducer', () => {
         expect(state.responses[0].response).toBe("data");
         expect(state.responses[0].queryParams).toBe("params");
         expect(state.responses[0].layerMetadata).toBe("meta");
+        expect(state.index).toBe(0);
 
         state = mapInfo(assign({}, appState, {responses: ["test"]}), {...testAction, reqId: 11});
         expect(state.responses).toExist();
         expect(state.responses.length).toBe(2);
-        expect(state.responses[0]).toBe("test");
+        expect(state.responses[0]).toBeTruthy();
         expect(state.responses[1].response).toBe("data");
         expect(state.responses[1].queryParams).toBe("params");
         expect(state.responses[1].layerMetadata).toBe("meta");
+        expect(state.index).toBe(1);
     });
 
     it('creates a feature info data from vector info request', () => {
@@ -145,7 +131,16 @@ describe('Test the mapInfo reducer', () => {
             metadata: "meta"
         };
 
-        let state = mapInfo({requests: []}, testAction);
+        let state = mapInfo({requests: [{}], configuration: {}}, testAction);
+        expect(state.responses).toExist();
+        expect(state.responses.length).toBe(2);
+        expect(state.responses[1].response).toExist();
+        expect(state.responses[1].response.features.length).toBe(1);
+        expect(state.responses[1].format).toBe('JSON');
+        expect(state.responses[1].queryParams.lng).toBe(10.0);
+        expect(state.responses[1].layerMetadata).toBe("meta");
+
+        state = mapInfo({requests: [{}], configuration: {trigger: "hover"}}, testAction);
         expect(state.responses).toExist();
         expect(state.responses.length).toBe(1);
         expect(state.responses[0].response).toExist();
