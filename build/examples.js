9e56ffa34d680c876a85af8b8fb7f9730e2b5828
Lorenzo Natali <lorenzo.natali@geo-solutions.it>
2020-09-14 18:27:56 +0200

    Solved conflicts

--- build/examples.js
+++ build/examples.js
@@ -3,12 +3,6 @@ var path = require("path");
 module.exports = {
     mouseposition: path.join(__dirname, "..", "web", "client", "examples", "mouseposition", "app"),
     scalebar: path.join(__dirname, "..", "web", "client", "examples", "scalebar", "app"),
-    "3dviewer": path.join(__dirname, "..", "web", "client", "examples", "3dviewer", "app"),
     featuregrid: path.join(__dirname, "..", "web", "client", "examples", "featuregrid", "app"),
-    print: path.join(__dirname, "..", "web", "client", "examples", "print", "app"),
-    login: path.join(__dirname, "..", "web", "client", "examples", "login", "app"),
-    plugins: path.join(__dirname, "..", "web", "client", "examples", "plugins", "app"),
-    api: path.join(__dirname, "..", "web", "client", "examples", "api", "app"),
-    hooks: path.join(__dirname, "..", "web", "client", "examples", "hooks", "app"),
-    lazyplugins: path.join(__dirname, "..", "web", "client", "examples", "lazyplugins", "app")
+    api: path.join(__dirname, "..", "web", "client", "examples", "api", "app")
 };
