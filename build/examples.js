120edefaff71d6f188b77b33560f8786e55ed28e
Lorenzo Natali <lorenzo.natali@geo-solutions.it>
2020-09-14 16:17:37 +0200

    removed also lazypligins, login,  hooks

--- build/examples.js
+++ build/examples.js
@@ -4,8 +4,5 @@ module.exports = {
     mouseposition: path.join(__dirname, "..", "web", "client", "examples", "mouseposition", "app"),
     scalebar: path.join(__dirname, "..", "web", "client", "examples", "scalebar", "app"),
     featuregrid: path.join(__dirname, "..", "web", "client", "examples", "featuregrid", "app"),
-    login: path.join(__dirname, "..", "web", "client", "examples", "login", "app"),
-    api: path.join(__dirname, "..", "web", "client", "examples", "api", "app"),
-    hooks: path.join(__dirname, "..", "web", "client", "examples", "hooks", "app"),
-    lazyplugins: path.join(__dirname, "..", "web", "client", "examples", "lazyplugins", "app")
+    api: path.join(__dirname, "..", "web", "client", "examples", "api", "app")
 };
