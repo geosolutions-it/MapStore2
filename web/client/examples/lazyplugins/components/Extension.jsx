a35c5fe6f93e9986e3bbcf9221ca2aef4e3a2ac3
Lorenzo Natali <lorenzo.natali@geo-solutions.it>
2020-09-14 18:27:56 +0200

    #5298: removed not working examples. (#5870)

--- web/client/examples/lazyplugins/components/Extension.jsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import Message from "../../../components/I18N/Message";
-
-const Extension = ({ value = 0, onIncrease }) => {
-    return <div style={{ top: "600px", zIndex: 1000 }}><span><Message msgId="myplugin.message"/>{value}</span><button onClick={onIncrease}>+</button></div>;
-};
-
-export default Extension;
