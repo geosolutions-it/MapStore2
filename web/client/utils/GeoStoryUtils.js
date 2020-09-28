20ecd3c271ad0447e436580f724b33b28e0d8a31
AGMETEOR <guwats10@gmail.com>
2020-09-28 18:02:33 +0300

    Add move editor transform functions to EditorUtils

--- web/client/utils/GeoStoryUtils.js
+++ web/client/utils/GeoStoryUtils.js
@@ -22,7 +22,6 @@ import isObject from "lodash/isObject";
 import includes from "lodash/includes";
 import replace from 'lodash/replace';
 import uuid from 'uuid';
-import { Entity } from 'draft-js';
 
 export const EMPTY_CONTENT = "EMPTY_CONTENT";
 // Allowed StoryTypes
@@ -497,83 +496,6 @@ export const parseHashUrlScrollUpdate = (url, hash = '', storyId, sectionId, col
     return null;
 };
 
-// customGetEntityId is a utility function used by html-to-draftjs library in order
-// to create anhor tag chunks
-export const customGetEntityId = (node) => {
-    let entityId = undefined;
-    if (
-        node instanceof HTMLAnchorElement
-    ) {
-        const entityConfig = {};
-        if (node.dataset && node.dataset.mention !== undefined) {
-            entityConfig.url = node.href;
-            entityConfig.text = node.innerHTML;
-            entityConfig.value = node.dataset.value;
-            entityId = Entity.__create(
-                'MENTION',
-                'IMMUTABLE',
-                entityConfig,
-            );
-        } else {
-            entityConfig.url = node.getAttribute ? node.getAttribute('href') || node.href : node.href;
-            entityConfig.title = node.innerHTML;
-            entityConfig.targetOption = node.target;
-            const interactionType = node.getAttribute && node.getAttribute('data-geostory-interaction-type');
-            const interactionParams = node.getAttribute && node.getAttribute('data-geostory-interaction-params');
-            const interactionName = node.getAttribute && node.getAttribute('data-geostory-interaction-name');
-
-            let attributes = {};
-
-            if (interactionName && interactionParams && interactionType) {
-                attributes['data-geostory-interaction-type'] = interactionType;
-                attributes['data-geostory-interaction-params'] = interactionParams;
-                attributes['data-geostory-interaction-name'] = interactionName;
-
-            }
-
-            entityConfig.attributes = attributes;
-            entityId = Entity.__create(
-                'LINK',
-                'MUTABLE',
-                entityConfig,
-            );
-        }
-    }
-    return entityId;
-};
-
-// customEntityTransform is a utility function used by draftjs-to-html inorder to create html from
-// entities in draftjs
-export const customEntityTransform = (entity, text) => {
-    if (entity.type === 'MENTION') {
-        return `< href="${entity.data.url}" class="wysiwyg-mention" data-mention data-value="${entity.data.value}">${text}</a>`;
-    }
-
-    if (entity.type === 'LINK') {
-        const targetOption = entity.data.targetOption || '_self';
-        const attributes = entity.data.attributes;
-
-        if (attributes && attributes['data-geostory-interaction-type']) {
-            return `<a data-geostory-interaction-name="${attributes['data-geostory-interaction-name']}" data-geostory-interaction-type="${attributes['data-geostory-interaction-type']}" data-geostory-interaction-params="${attributes['data-geostory-interaction-params']}" onclick="__geostory_interaction(type='scrollTo', '${attributes['data-geostory-interaction-params']}')">${text}</a>`;
-        }
-        return `<a href="${entity.data.url}" target="${targetOption}">${text}</a>`;
-    }
-
-    if (entity.type === 'IMAGE') {
-        const alignment = entity.data.alignment;
-
-        if (alignment && alignment.length) {
-            return `<div style="text-align:'${alignment};'"><img src="${entity.data.src}" alt="${entity.data.alt}" style="height:'${entity.data.height}', width: '${entity.data.width}'" /></div>`;
-        }
-
-        return `<img src="${entity.data.src}" alt="${entity.data.alt}" style="height:'${entity.data.height}', width: '${entity.data.width}'" />`;
-    }
-
-    if (entity.type === 'EMBEDDED_LINK') {
-        return `<iframe width="${entity.data.width}" height="${entity.data.height}" src="${entity.data.src}" frameBorder="0"></iframe>`;
-    }
-    return text;
-};
 /**
  * Creates a configuration from localConfig object to be used by the webfontloader library
  * @param {array} fontFamilies - font families configured from localConfig
