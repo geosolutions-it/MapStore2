2c3b7d29c13f6003ba54f29470f188e4081fb1f4
Lorenzo Natali <lorenzo.natali@geo-solutions.it>
2020-11-04 16:49:22 +0100

    fixed chart options and resize

--- web/client/components/charts/PlotlyChart.jsx
+++ web/client/components/charts/PlotlyChart.jsx
@@ -1,11 +1,64 @@
 import React from 'react';
 import Plot from 'react-plotly.js';
+
+function getData({type, xDataKey, yDataKey, data}) {
+    const x = data.map(d => d[xDataKey]);
+    const y = data.map(d => d[yDataKey]);
+    switch (type) {
+    case 'pie':
+        return {
+            textposition: 'inside', // this avoids text to overflow the chart div when rendered outside
+            values: y,
+            labels: x
+        };
+
+    default:
+        return {
+            x,
+            y
+        };
+    }
+}
+function getMargins({ type, yAxis }) {
+    switch (type) {
+    case 'pie':
+        return { t: 5, b: 5, l: 2, r: 2 };
+    default:
+        return {
+            l: !yAxis ? 5 : undefined,
+            r: 5,
+            t: 20,
+            pad: 4
+        };
+    }
+}
+function getLayoutOptions({cartesian, type, yAxis, xAxisAngle} ) {
+    switch (type) {
+    case 'pie':
+        return {};
+    // line / bar
+    default :
+        return {
+            yaxis: {
+                automargin: true,
+                showticklabels: yAxis,
+                // showticklabels,showline for yAxis false
+                showgrid: cartesian
+            },
+            xaxis: {
+                visible: true,
+                automargin: true,
+                tickangle: xAxisAngle ?? 'auto'
+            }
+        };
+    }
+}
 /**
  * Adapter for the data format and options format of the MapStore charts
  * and the Library format.
  */
 export const toPlotly = ({
-    data,
+    data = [],
     xAxis,
     xAxisAngle,
     series,
@@ -18,49 +71,37 @@ export const toPlotly = ({
 }) => {
     const xDataKey = xAxis.dataKey;
     const yDataKey = series[0].dataKey;
-    const x = data.map(d => d[xDataKey]);
-    const y = data.map(d => d[yDataKey]);
+
     return {
         layout: {
             showlegend: legend,
-            legend: { "orientation": "h" },
+            // legend: { "orientation": "h" },
             // https://plotly.com/javascript/setting-graph-size/
             // automargin: true ok for big widgets.
             // small widgets should be adapted accordingly
-            yaxis: {
-                automargin: true,
-                showticklabels: yAxis,
-                // showticklabels,showline for yAxis false
-                showgrid: cartesian
-            },
-            xaxis: {
-                visible: true,
-                automargin: true,
-                tickangle: xAxisAngle ?? 'auto'
-            },
-            margin: {
-                l: !yAxis ? 5 : undefined,
-                r: 5,
-                t: 20,
-                pad: 4
-            },
+            ...getLayoutOptions({ cartesian, type, yAxis, xAxisAngle}),
+            margin: getMargins({type, yAxis}),
             autosize: false,
             automargin: false,
             height,
             width
         },
         data: [{
+            type,
             name: yAxisLabel ?? yDataKey,
-            // for pie
-            values: y,
-            labels: x,
-            // for line/bar
-            x,
-            y,
-            type
+            ...getData({ type, data, xDataKey, yDataKey})
         }],
         config: {
-            // displayModeBar
+            displayModeBar: width > 250, // minimal to display 8 tools.
+            modeBarButtonsToRemove: [
+                // to use less space, they looks not so useful
+                "lasso2d",
+                "select2d",
+                "hoverCompareCartesian",
+                "hoverClosestCartesian",
+                "hoverClosestPie"
+            ],
+            displaylogo: false
         }
     };
 };
