852e5589cd31852f0b0d33b8cf4bc30d0e299951
Vladislav Shatilenya <vladislav.shatilenya@yotec.net>
2020-11-17 22:13:53 +0300

    merge mistake fix

--- web/client/components/TOC/Toolbar.jsx
+++ web/client/components/TOC/Toolbar.jsx
@@ -295,7 +295,7 @@ class Toolbar extends React.Component {
                         </Button>
                     </OverlayTrigger>
                     : null}
-                {this.props.activateTool.activateWidgetTool && (status === 'LAYER') && this.props.selectedLayers.length === 1 && this.props.selectedLayers[0].search !== 'vector' && !this.props.settings.expanded && !this.props.layerMetadata.expanded && !this.props.wfsdownload.expanded ?
+                {this.props.activateTool.activateWidgetTool && (status === 'LAYER') && this.props.selectedLayers.length === 1 && this.props.selectedLayers[0].search !== 'vector' && !this.props.settings.expanded && !this.props.layerMetadata.expanded && !this.props.layerdownload.expanded ?
                     <OverlayTrigger
                         key="widgets"
                         placement="top"
