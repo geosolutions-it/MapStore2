e2efa91b3c619e137c600c26a388760dd3601a6b
Lorenzo Natali <lorenzo.natali@geo-solutions.it>
2020-11-24 09:55:40 +0100

    web/client/components/data/featuregrid/toolbars/TButton.jsx

--- web/client/components/data/featuregrid/toolbars/TButton.jsx
+++ web/client/components/data/featuregrid/toolbars/TButton.jsx
@@ -6,6 +6,8 @@
  * LICENSE file in the root directory of this source tree.
  */
 import React from 'react';
+import PropTypes from 'prop-types';
+
 import { Button, Glyphicon } from 'react-bootstrap';
 
 const hideStyle = {
@@ -16,6 +18,18 @@ const hideStyle = {
 const normalStyle = {};
 const getStyle = (visible) => visible ? normalStyle : hideStyle;
 export default class SimpleTButton extends React.Component {
+    propTypes = {
+        disabled: PropTypes.bool,
+        id: PropTypes.string,
+        visible: PropTypes.bool,
+        onClick: PropTypes.func,
+        glyph: PropTypes.string,
+        active: PropTypes.bool,
+        className: PropTypes.string
+    }
+    defaultProps = {
+        className: "square-button"
+    }
     render() {
         const { disabled, id, visible, onClick, glyph, active, className = "square-button", ...props } = this.props;
         return (<Button {...props} bsStyle={active ? "success" : "primary"} disabled={disabled} id={`fg-${id}`}
