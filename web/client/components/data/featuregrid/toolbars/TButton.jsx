ac91678de2c4098b24d181d1532cfb11619fea88
Lorenzo Natali <lorenzo.natali@geo-solutions.it>
2020-11-25 16:21:58 +0100

    #6235 fix map sync button popup position (#6236)

--- web/client/components/data/featuregrid/toolbars/TButton.jsx
+++ web/client/components/data/featuregrid/toolbars/TButton.jsx
@@ -5,7 +5,8 @@
  * This source code is licensed under the BSD-style license found in the
  * LICENSE file in the root directory of this source tree.
  */
-import React from 'react';
+import React, { forwardRef } from 'react';
+
 import { Button, Glyphicon } from 'react-bootstrap';
 
 const hideStyle = {
@@ -15,15 +16,14 @@ const hideStyle = {
 };
 const normalStyle = {};
 const getStyle = (visible) => visible ? normalStyle : hideStyle;
-
-export const SimpleTButton = ({disabled, id, visible, onClick, glyph, active, className = "square-button", ...props}) => {
-    return (<Button {...props} bsStyle={active ? "success" : "primary"} disabled={disabled} id={`fg-${id}`}
+export const SimpleTButton = forwardRef(({ disabled, id, visible, onClick, glyph, active, className = "square-button", ...props }, ref) => {
+    return (<Button ref={ref} {...props} bsStyle={active ? "success" : "primary"} disabled={disabled} id={`fg-${id}`}
         style={getStyle(visible)}
         className={className}
         onClick={() => !disabled && onClick()}>
-        <Glyphicon glyph={glyph}/>
+        <Glyphicon glyph={glyph} />
     </Button>);
-};
+});
 
 
 export default SimpleTButton;
