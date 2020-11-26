367549084b58b3b6fbae87809e4793a20f5de1a1
stefano bovio <stefano.bovio@geo-solutions.it>
2020-11-26 15:12:46 +0100

    #6190: check if the response is not undefined in properties viewer (#6192)

--- web/client/components/data/identify/viewers/PropertiesViewer.js
+++ web/client/components/data/identify/viewers/PropertiesViewer.js
@@ -14,7 +14,7 @@ export default ({response, layer, rowViewer}) => {
     const RowViewer = (layer && layer.rowViewer) || rowViewer || PropertiesViewer;
     return (
         <div className="mapstore-json-viewer">
-            {(response.features || []).map((feature, i) => {
+            {(response?.features || []).map((feature, i) => {
                 return <RowViewer key={i} feature={feature} title={feature.id + ''} exclude={["bbox"]} {...feature.properties}/>;
             })}
         </div>
