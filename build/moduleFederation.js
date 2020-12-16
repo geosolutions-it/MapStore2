453aca0ae66c77e7ae59fe832dab63af4302d988
Lorenzo Natali <lorenzo.natali@geo-solutions.it>
2020-12-16 10:44:38 +0100

    add all dependencies to shared

--- build/moduleFederation.js
+++ build/moduleFederation.js
@@ -10,60 +10,20 @@
 const ModuleFederationPlugin = require('webpack/lib/container/ModuleFederationPlugin');
 // TODO: a finest system should check package.json dependencies to share them all.
 // here some code that started to do this (with some fixes for issues with module resolution).
-// const dependencies = require('../package.json').dependencies;
-// const excludes = [
-//     "react-draft-wysiwyg", "html-to-draftjs", "geostyler-geocss-parser", "@geosolutions/wkt-parser", "@turf/bbox-polygon"
-// ];
-// const deps = Object.keys(dependencies)
-//     .filter(d => !excludes.includes(d))
-//     .reduce((acc, d) => ({
-//         ...acc,
-//         [d]: dependencies[d]
-//     }),
-//     {});
+const dependencies = require('../package.json').dependencies;
+const excludes = [
+    "react-draft-wysiwyg", "html-to-draftjs", "geostyler-geocss-parser", "@geosolutions/wkt-parser", "@turf/bbox-polygon"
+];
 
 // the shared libraries, to use both in all federated modules (extensions/main product)
 const shared = {
-    "url": {
-        eager: true,
-        singleton: true
-    },
-    "redux-observable": {
-        eager: true,
-        singleton: true
-    },
-    "lodash": {
-        eager: true,
-        singleton: true
-    },
-    "lodash/curry": {
-        eager: true,
-        singleton: true
-    },
-    "react-intl": {
-        eager: true,
-        singleton: true
-    },
-    "bootstrap": {
-        eager: true,
-        singleton: true
-    },
-    "rxjs": {
-        eager: true,
-        singleton: true
-    },
-    "react-redux": {
-        eager: true,
-        singleton: true
-    },
-    react: {
-        eager: true,
-        singleton: true
-    },
-    'react-dom': {
-        eager: true,
-        singleton: true
-    }
+    ...Object.keys(dependencies).filter(v => !excludes.includes(v)).reduce((acc, v) => ({
+        ...acc,
+        [v]: {
+            eager: true,
+            singleton: true
+        }
+    }), {})
 };
 
 /**
