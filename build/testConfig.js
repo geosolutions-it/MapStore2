2b47c32c6a7757748b1f02047c5f6db24d70be7a
Lorenzo Natali <lorenzo.natali@geo-solutions.it>
2020-12-16 16:13:20 +0100

    add missing plugins to travis build

--- build/testConfig.js
+++ build/testConfig.js
@@ -24,6 +24,8 @@ module.exports = ({browsers = [ 'ChromeHeadless' ], files, path, testFile, singl
         'karma-mocha',
         'karma-mocha-reporter',
         'karma-coverage',
+        'karma-coveralls',
+        'karma-junit-reporter',
         'karma-firefox-launcher'
 
     ],
