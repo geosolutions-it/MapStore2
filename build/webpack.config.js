f2f29880cc4da80d2846fef0eec8c663844300b7
allyoucanmap <bovio.stefano@gmail.com>
2021-01-21 10:40:42 +0100

    remove dashboard-embedded entry in dev webpack

--- build/webpack.config.js
+++ build/webpack.config.js
@@ -5,8 +5,7 @@ const extractThemesPlugin = require('./themes.js').extractThemesPlugin;
 const moduleFederationPlugin = require('./moduleFederation.js').plugin;
 module.exports = require('./buildConfig')(
     {
-        [process.env.bundle || "mapstore2"]: path.join(__dirname, "..", "web", "client", "product", process.env.entrypoint || process.env.bundle || "app"),
-        'dashboard-embedded': path.join(__dirname, "..", "web", "client", "product", "dashboardEmbedded")
+        [process.env.bundle || "mapstore2"]: path.join(__dirname, "..", "web", "client", "product", process.env.entrypoint || process.env.bundle || "app")
     },
     { ["themes/default"]: themeEntries["themes/" + (process.env.theme || "default")]},
     {
