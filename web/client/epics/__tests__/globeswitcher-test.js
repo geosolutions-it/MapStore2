0aa60a573e56393450d10e58bfea963159ddda8e
Matteo V <matteo.velludini@geo-solutions.it>
2021-01-25 12:19:02 +0100

    #6417 add globe switcher to context and support switching from there (#6438)

--- web/client/epics/__tests__/globeswitcher-test.js
+++ web/client/epics/__tests__/globeswitcher-test.js
@@ -11,9 +11,50 @@ import expect from 'expect';
 import { toggle3d, UPDATE_LAST_2D_MAPTYPE } from '../../actions/globeswitcher';
 import { localConfigLoaded } from '../../actions/localConfig';
 import assign from 'object-assign';
-import { updateRouteOn3dSwitch, updateLast2dMapTypeOnChangeEvents } from '../globeswitcher';
+import { replaceMapType, updateRouteOn3dSwitch, updateLast2dMapTypeOnChangeEvents } from '../globeswitcher';
 import { testEpic } from './epicTestUtils';
+import { MAP_TYPE_CHANGED } from './../../actions/maptype';
+
 describe('globeswitcher Epics', () => {
+
+    it('toggle to 3d for context maps', (done) => {
+        const NUM_ACTIONS = 1;
+        testEpic(updateRouteOn3dSwitch, NUM_ACTIONS, assign({}, toggle3d(true, "openlayers"), { hash: "/context/3dmap/123" }), actions => {
+            expect(actions.length).toBe(NUM_ACTIONS);
+            actions.map((action) => {
+                switch (action.type) {
+                case MAP_TYPE_CHANGED:
+                    expect(action.mapType).toBe("cesium");
+                    break;
+                default:
+                    expect(true).toBe(false);
+
+                }
+            });
+            done();
+        }, {
+            globeswitcher: {last2dMapType: "openlayers"}
+        });
+    });
+    it('toggle from 3d for context maps', (done) => {
+        const NUM_ACTIONS = 1;
+        testEpic(updateRouteOn3dSwitch, NUM_ACTIONS, assign({ hash: "/context/3dmap/123" }, toggle3d(false, "cesium")), actions => {
+            expect(actions.length).toBe(NUM_ACTIONS);
+            actions.map((action) => {
+                switch (action.type) {
+                case MAP_TYPE_CHANGED:
+                    expect(action.mapType).toBe("leaflet");
+                    break;
+                default:
+                    expect(true).toBe(false);
+
+                }
+            });
+            done();
+        }, {
+            globeswitcher: {last2dMapType: "leaflet"}
+        });
+    });
     it('updates maptype toggling to 3D', (done) => {
         testEpic(updateRouteOn3dSwitch, 1, assign({hash: "/viewer/leaflet/2"}, toggle3d(true, "leaflet")), actions => {
             expect(actions.length).toBe(1);
@@ -104,6 +145,44 @@ describe('globeswitcher Epics', () => {
             maptype: {
                 mapType: 'openlayers'
             }
+        });
+
+    });
+    it('testing replaceMapType with viewer regex', () => {
+        const urls = [
+            {
+                url: "/viewer/openlayers/123",
+                newMapType: "cesium",
+                expected: "/viewer/cesium/123"
+            },
+            {
+                url: "/viewer/openlayers/new/context/123",
+                newMapType: "cesium",
+                expected: "/viewer/cesium/new/context/123"
+            },
+            {
+                url: "/viewer/cesium/123",
+                newMapType: "openlayers",
+                expected: "/viewer/openlayers/123"
+            },
+            {
+                url: "/viewer/123",
+                newMapType: "cesium",
+                expected: "/viewer/cesium/123"
+            },
+            {
+                url: "/viewer/123",
+                newMapType: "openlayers",
+                expected: "/viewer/openlayers/123"
+            },
+            {
+                url: "/context/ctxName/123",
+                newMapType: "openlayers",
+                expected: "/context/ctxName/123"
+            }
+        ];
+        urls.forEach(({url, newMapType, expected}) => {
+            expect(replaceMapType(url, newMapType)).toBe(expected);
         });
 
     });
