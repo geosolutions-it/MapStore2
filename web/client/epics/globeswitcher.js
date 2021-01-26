9d9d0fcd7e7ae7f3f0fa090391c9be7eac9cbdc3
Matteo V <matteo.velludini@geo-solutions.it>
2021-01-26 18:29:23 +0100

    #6417 add globe switcher to context and support switching from there (#6438) (#6464)

--- web/client/epics/globeswitcher.js
+++ web/client/epics/globeswitcher.js
@@ -7,17 +7,28 @@
  */
 import { TOGGLE_3D, updateLast2dMapType } from '../actions/globeswitcher';
 
-import { MAP_TYPE_CHANGED } from '../actions/maptype';
+import { changeMapType, MAP_TYPE_CHANGED } from '../actions/maptype';
 import { mapTypeSelector } from '../selectors/maptype';
 import { LOCAL_CONFIG_LOADED } from '../actions/localConfig';
 import Rx from 'rxjs';
-import { get } from 'lodash';
-
-const defaultRegexes = [/\/viewer\/\w+\/(\w+)/, /\/viewer\/(\w+)/];
 import { push } from 'connected-react-router';
+import { last2dMapTypeSelector } from './../selectors/globeswitcher';
 
-const replaceMapType = (path, newMapType) => {
-    const match = defaultRegexes.reduce((previous, regex) => {
+const VIEWERS_REGEX = [
+    /\/viewer\/\w+\/(\w+)/,
+    /\/viewer\/(\w+)/
+];
+const CONTEXT_NEW_MAP_REGEX = /\/viewer\/\w+\/(\w+)\/context\/(\w+)/;
+const CONTEXT_REGEX = /\/context\/(\w+)/;
+
+export const replaceMapType = (path, newMapType) => {
+    // check context new regex  first
+    const contextMatch = path.match(CONTEXT_NEW_MAP_REGEX);
+    if (contextMatch) {
+        return `/viewer/${newMapType}/${contextMatch[1]}/context/${contextMatch[2]}`;
+    }
+    // check normal viewer regex after
+    const match = VIEWERS_REGEX.reduce((previous, regex) => {
         return previous || path.match(regex);
     }, null);
     if (match) {
@@ -34,7 +45,13 @@ const replaceMapType = (path, newMapType) => {
 export const updateRouteOn3dSwitch = (action$, store) =>
     action$.ofType(TOGGLE_3D)
         .switchMap( (action) => {
-            const newPath = replaceMapType(action.hash || location.hash, action.enable ? "cesium" : get(store.getState(), "globeswitcher.last2dMapType") || 'leaflet');
+            const last2dMapType = last2dMapTypeSelector(store.getState());
+            const hash = action.hash || location.hash;
+            if (hash.match(CONTEXT_REGEX) && !hash.match(CONTEXT_NEW_MAP_REGEX)) {
+                // hash can be "#/context/3d"
+                return Rx.Observable.of(changeMapType(action.originalMapType !== "cesium" ? "cesium" : last2dMapType ));
+            }
+            const newPath = replaceMapType(hash, action.enable ? "cesium" : last2dMapType);
             if (newPath) {
                 return Rx.Observable.from([push(newPath)]);
             }
