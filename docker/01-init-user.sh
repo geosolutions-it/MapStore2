93b4066b4ac0b464374b3e97225ec8921c510aaf
Asdrubal Gonzalez <agpenton@gmail.com>
2021-02-10 16:19:11 +0400

    Database initialization added.

--- docker/01-init-user.sh
+++ docker/01-init-user.sh
@@ -2,7 +2,19 @@
 set -e
 
 psql -v ON_ERROR_STOP=1 --username postgres --dbname postgres <<-EOSQL
-    CREATE USER geostore;
-    CREATE DATABASE geostore;
-    GRANT ALL PRIVILEGES ON DATABASE geostore TO geostore;
+    CREATE user geostore LOGIN PASSWORD 'geostore' NOSUPERUSER INHERIT NOCREATEDB NOCREATEROLE;
+    CREATE SCHEMA geostore;
+    GRANT USAGE ON SCHEMA geostore TO geostore ;
+    GRANT ALL ON SCHEMA geostore TO geostore ;
+    alter user geostore set search_path to geostore , public;
+    CREATE user geostore_test LOGIN PASSWORD 'geostore_test' NOSUPERUSER INHERIT NOCREATEDB NOCREATEROLE;
+    CREATE SCHEMA geostore_test;
+    GRANT USAGE ON SCHEMA geostore_test TO geostore_test;
+    GRANT ALL ON SCHEMA geostore_test TO geostore_test;
+    alter user geostore_test set search_path to geostore_test, public;
 EOSQL
+#psql -v ON_ERROR_STOP=1 --username postgres --dbname postgres <<-EOSQL
+#    CREATE USER geostore;
+#    CREATE DATABASE geostore;
+#    GRANT ALL PRIVILEGES ON DATABASE geostore TO geostore;
+#EOSQL
