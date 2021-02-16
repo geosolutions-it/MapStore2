babb91baafcdfb8d2c88918c1120f980fe790e38
Matteo V <matteo.velludini@geo-solutions.it>
2021-02-16 15:54:22 +0100

    #6460: fix cesium layer refresh when layer filter toggled on/off (#6491)

--- web/client/components/map/cesium/plugins/WMSLayer.js
+++ web/client/components/map/cesium/plugins/WMSLayer.js
@@ -15,7 +15,7 @@ const {
     needProxy
 } = require('../../../../utils/ProxyUtils');
 const assign = require('object-assign');
-const {isArray} = require('lodash');
+const {isArray, isEqual} = require('lodash');
 const WMSUtils = require('../../../../utils/cesium/WMSUtils');
 const {getAuthenticationParam, getURLs} = require('../../../../utils/LayersUtils');
 const { optionsToVendorParams } = require('../../../../utils/VendorParamsUtils');
@@ -160,9 +160,12 @@ const updateLayer = (layer, newOptions, oldOptions) => {
         .filter((key) => {
             const oldOption = oldOptions[key] === undefined ? oldParams && oldParams[key] : oldOptions[key];
             const newOption = newOptions[key] === undefined ? newParams && newParams[key] : newOptions[key];
-            return oldOption !== newOption;
+            return !isEqual(oldOption, newOption);
         });
-    if (newParameters.length > 0 || newOptions.securityToken !== oldOptions.securityToken || newOptions.tileSize !== oldOptions.tileSize) {
+    if (newParameters.length > 0 ||
+        newOptions.securityToken !== oldOptions.securityToken ||
+        !isEqual(newOptions.layerFilter, oldOptions.layerFilter) ||
+        newOptions.tileSize !== oldOptions.tileSize) {
         return createLayer(newOptions);
     }
     return null;
