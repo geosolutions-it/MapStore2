7e0f8c163ebd1066180d0fc17f1b254bd473dc1e
stefano bovio <stefano.bovio@geo-solutions.it>
2021-02-16 12:45:04 +0100

    #6172: add missing files to fix embedded in standard project (#6529)

--- project/standard/templates/webpack.config.js
+++ project/standard/templates/webpack.config.js
@@ -7,7 +7,7 @@ const ModuleFederationPlugin = require('./MapStore2/build/moduleFederation').plu
 module.exports = require('./MapStore2/build/buildConfig')(
     {
         '__PROJECTNAME__': path.join(__dirname, "js", "app"),
-        '__PROJECTNAME__-embedded': path.join(__dirname, "MapStore2", "web", "client", "product", "embedded"),
+        '__PROJECTNAME__-embedded': path.join(__dirname, "js", "embedded"),
         '__PROJECTNAME__-api': path.join(__dirname, "MapStore2", "web", "client", "product", "api"),
         'geostory-embedded': path.join(__dirname, "js", "geostoryEmbedded"),
         "dashboard-embedded": path.join(__dirname, "js", "dashboardEmbedded")
