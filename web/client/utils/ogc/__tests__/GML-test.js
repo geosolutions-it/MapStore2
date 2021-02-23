701c2619507ff4f661efd1dd5184de0cc8b8dbea
Luca Orlandini <luca.orlandini@gmail.com>
2021-02-23 09:18:41 +0100

    fix polygon external internal (#6554)

--- web/client/utils/ogc/__tests__/GML-test.js
+++ web/client/utils/ogc/__tests__/GML-test.js
@@ -7,8 +7,9 @@
  */
 
 const expect = require('expect');
-const {processOGCGeometry} = require('../GML');
+const {processOGCGeometry, polygonElement} = require('../GML');
 const V1_1_0 = "1.1.0";
+const srsName = "EPSG:3857";
 const point = {"type": "Point", "coordinates": [100.0, 0.0] };
 const lineString = { "type": "LineString",
     "coordinates": [ [100.0, 0.0], [101.0, 1.0] ]
@@ -17,6 +18,12 @@ const polygon = {
     "type": "Polygon",
     "coordinates": [[[102.0, 2.0], [103.0, 2.0], [103.0, 3.0], [102.0, 3.0], [102.0, 2.0]]]
 };
+
+const polygonExternalInternal = {
+    "type": "Polygon",
+    "coordinates": [[[-13837894.938769765, 6152246.765038833], [-13870573.06472578, 5167186.555807996], [-13885246.404215768, 5141882.531585468], [-13060956.58557037, 3836666.5247580605], [-10834726.501826838, 2986698.362005187], [-10494170.270500567, 3494875.037811458], [-9336249.138400448, 3489971.447318631], [-8715477.041201625, 4086300.4560303073], [-8433239.847950352, 4713623.15121573], [-7624933.488392262, 5936526.215342932], [-9247909.64555257, 5111113.43704269], [-10569782.887260191, 6277905.950416091], [-13822622.687463861, 6276333.806899306]], [[-13082405.114977926, 5738510.996205103], [-12975574.219813585, 4728558.514625181], [-11716961.038662285, 4179094.355509075], [-9922209.486299047, 4394852.33719347], [-9935385.546249239, 5499208.293757439]]]
+};
+
 const multiPolygon = {
     "type": "MultiPolygon",
     "coordinates": [
@@ -73,14 +80,27 @@ const EXPECTED_RESULTS = {
                                         + '<gml:posList>100 0 101 0 101 1 100 1 100 0</gml:posList>'
                                     + '</gml:LinearRing>'
                                 + '</gml:exterior>'
-                                + '<gml:exterior>'
+                                + '<gml:interior>'
                                     + '<gml:LinearRing>'
                                         + '<gml:posList>100.2 0.2 100.8 0.2 100.8 0.8 100.2 0.8 100.2 0.2</gml:posList>'
                                     + '</gml:LinearRing>'
-                                + '</gml:exterior>'
+                                + '</gml:interior>'
                             + '</gml:Polygon>'
                         + '</gml:polygonMember>'
-                    + '</gml:MultiPolygon>'
+                    + '</gml:MultiPolygon>',
+        exteriorInterior: '<gml:Polygon srsName="EPSG:3857">'
+                            + '<gml:exterior>'
+                            +    '<gml:LinearRing>'
+                            +        '<gml:posList>-13837894.938769765 6152246.765038833 -13870573.06472578 5167186.555807996 -13885246.404215768 5141882.531585468 -13060956.58557037 3836666.5247580605 -10834726.501826838 2986698.362005187 -10494170.270500567 3494875.037811458 -9336249.138400448 3489971.447318631 -8715477.041201625 4086300.4560303073 -8433239.847950352 4713623.15121573 -7624933.488392262 5936526.215342932 -9247909.64555257 5111113.43704269 -10569782.887260191 6277905.950416091 -13822622.687463861 6276333.806899306 -13837894.938769765 6152246.765038833</gml:posList>'
+                            +    '</gml:LinearRing>'
+                            + '</gml:exterior>'
+                            + '<gml:interior>'
+                            +    '<gml:LinearRing>'
+                            +        '<gml:posList>-13082405.114977926 5738510.996205103 -12975574.219813585 4728558.514625181 -11716961.038662285 4179094.355509075 -9922209.486299047 4394852.33719347 -9935385.546249239 5499208.293757439 -13082405.114977926 5738510.996205103</gml:posList>'
+                            +    '</gml:LinearRing>'
+                            + '</gml:interior>'
+                        + '</gml:Polygon>'
+
     }
 };
 describe('Test GeoJSON/GML geometry conversion', () => {
@@ -104,5 +124,10 @@ describe('Test GeoJSON/GML geometry conversion', () => {
     it('MultiPolygon to GML 1.1.0', () => {
         const gmlMultiPolygon = processOGCGeometry(V1_1_0, multiPolygon);
         expect(gmlMultiPolygon).toBe(EXPECTED_RESULTS.V1_1_0.multiPolygon);
+    });
+
+    it('Polygon with holes to GML external internal', () => {
+        const gmlMultiPolygon = polygonElement(polygonExternalInternal.coordinates, srsName, V1_1_0);
+        expect(gmlMultiPolygon).toBe(EXPECTED_RESULTS.V1_1_0.exteriorInterior);
     });
 });
