1b711608fdd3c2369abda02b3fc72b0590113d65
Muhwezi Aggrey <aggrey256@gmail.com>
2021-03-01 16:04:32 +0300

    show messages in widget other than hover

--- web/client/components/widgets/enhancers/emptyChartState.js
+++ web/client/components/widgets/enhancers/emptyChartState.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import Message from '../../I18N/Message';
 import emptyState from '../../misc/enhancers/emptyState';
+import EmptyView from '../../misc/EmptyView';
 
 export default emptyState(
     ({data = []}) => !data || data.length === 0,
     ({mapSync, iconFit} = {}) => ({
         iconFit,
         tooltip: mapSync ? <Message msgId="widgets.errors.nodatainviewport" /> : <Message msgId="widgets.errors.nodata" />
-    })
+    }),
+    () => <EmptyView content={<Message msgId="widgets.errors.nodatainviewport" />}/>
 );
