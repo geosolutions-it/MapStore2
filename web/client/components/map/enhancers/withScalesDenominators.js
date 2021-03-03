f990ca9c7f43e0f27997a1bd49d6cf217c85ffc3
Luca Orlandini <luca.orlandini@gmail.com>
2021-03-03 14:54:03 +0100

    rename file HOC

--- web/client/components/map/enhancers/withScalesDenominators.js
+++ web/client/components/map/enhancers/withScalesDenominators.js
@@ -11,11 +11,12 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { getResolutionsForScales, DEFAULT_SCREEN_DPI} from '../../../utils/MapUtils';
 
-const overrideMapProps = (Component) => {
+const withScalesDenominators = (Component) => {
 
     return (props) => {
-        const projection = "EPSG:3857";
-        const dpi = DEFAULT_SCREEN_DPI;
+        const projection = props?.map?.projection || 'EPSG:3857';
+        // if null the getResolutionsForScales will use DEFAULT_SCREEN_DPI automatically
+        const dpi = props?.map?.mapOptions?.view?.DPI || null;
         const scales = props?.map?.mapOptions?.view?.scales;
         const resolutions = (scales) ? (getResolutionsForScales(scales, projection, dpi)) : null;
         const mapProps = (resolutions) ? {
@@ -36,7 +37,7 @@ const overrideMapProps = (Component) => {
     };
 };
 
-overrideMapProps.propTypes = {
+withScalesDenominators.propTypes = {
     Component: PropTypes.element
 };
-export default overrideMapProps;
+export default withScalesDenominators;
