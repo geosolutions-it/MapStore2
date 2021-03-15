c9f6473c6e75a446462efd7365e0968b72851227
Muhwezi Aggrey <aggrey256@gmail.com>
2021-03-15 12:26:51 +0300

    add filter cross layer options

--- web/client/epics/autocomplete.js
+++ web/client/epics/autocomplete.js
@@ -17,7 +17,8 @@ import {
     loadingFilterFieldOptions,
     setAutocompleteMode,
     toggleMenu,
-    UPDATE_CROSS_LAYER_FILTER_FIELD
+    UPDATE_CROSS_LAYER_FILTER_FIELD,
+    updateCrossLayerFilterFieldOptions
 } from '../actions/queryform';
 import { FEATURE_TYPE_SELECTED } from '../actions/wfsquery';
 import { error } from '../actions/notifications';
@@ -69,18 +70,18 @@ export const fetchAutocompleteOptionsEpic = (action$, store) =>
             const maxFeaturesWPS = maxFeaturesWPSSelector(state);
             let filterField = {};
             if (action.type === UPDATE_CROSS_LAYER_FILTER_FIELD) {
-                filterField = state.queryform.crossLayerFilter;
+                filterField = state.queryform.crossLayerFilter?.collectGeometries?.queryCollection?.filterFields?.filter((f) => f.rowId === action.rowId)[0] || {};
             } else {
                 filterField = state.queryform && state.queryform.filterFields && state.queryform.filterFields.filter((f) => f.rowId === action.rowId)[0];
             }
             if (action.fieldOptions.selected === "selected") {
                 return Rx.Observable.from([
-                    updateFilterFieldOptions(filterField, [], 0)
+                    action.type === UPDATE_CROSS_LAYER_FILTER_FIELD ? updateCrossLayerFilterFieldOptions(filterField, [], 0) : updateFilterFieldOptions(filterField, [], 0)
                 ]);
             }
             const data = getWpsPayload({
                 attribute: filterField.attribute,
-                layerName: typeNameSelector(state),
+                layerName: action.type === UPDATE_CROSS_LAYER_FILTER_FIELD ? state.queryform.crossLayerFilter?.collectGeometries?.queryCollection.typeName : typeNameSelector(state),
                 layerFilter: appliedFilterSelector(state) || storedFilterSelector(state),
                 maxFeatures: maxFeaturesWPS,
                 startIndex: action.fieldOptions.currentPage ? (action.fieldOptions.currentPage - 1) : 1 * maxFeaturesWPS,
@@ -98,13 +99,15 @@ export const fetchAutocompleteOptionsEpic = (action$, store) =>
             ).switchMap((res) => {
                 let newOptions = isArray(res.values) ? res.values : [res.values];
                 let valuesCount = res.size;
-                return Rx.Observable.from([updateFilterFieldOptions(filterField, newOptions, valuesCount), toggleMenu(action.rowId, true)] );
+                return Rx.Observable.from(action.type === UPDATE_CROSS_LAYER_FILTER_FIELD ? [updateCrossLayerFilterFieldOptions(filterField, newOptions, valuesCount), toggleMenu(action.rowId, true)] :
+                    [updateFilterFieldOptions(filterField, newOptions, valuesCount), toggleMenu(action.rowId, true)] );
             })
                 .startWith(loadingFilterFieldOptions(true, filterField))
                 .catch( () => {
                 // console.log("error: " + e + " data:" + e.data);
                     return Rx.Observable.from([
-                        updateFilterFieldOptions(filterField, [], 0),
+                        action.type === UPDATE_CROSS_LAYER_FILTER_FIELD ? updateCrossLayerFilterFieldOptions(filterField, [], 0) :
+                            updateFilterFieldOptions(filterField, [], 0),
                         error({
                             title: "warning",
                             message: "warning", // TODO add tranlations
