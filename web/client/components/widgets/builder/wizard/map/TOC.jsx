5b36b82ff5a68b435d6f4b6bd50318caad0a333b
Muhwezi Deo <39124174+MuhweziDeo@users.noreply.github.com>
2021-03-23 16:10:40 +0300

    #5111: Tooltips on map widget TOC (#6672)

--- web/client/components/widgets/builder/wizard/map/TOC.jsx
+++ web/client/components/widgets/builder/wizard/map/TOC.jsx
@@ -47,6 +47,7 @@ export default enhanceTOC(({
         layerElement={<DefaultLayer
             selectedNodes={selectedNodes}
             onSelect={onSelect}
+            titleTooltip
             propertiesChangeHandler={(layer, changes) => Object.keys(changes).map(k => changeLayerProperty(layer, k, changes[k]))}
             onUpdateNode={(layer, _, changes) => Object.keys(changes).map(k => changeLayerProperty(layer, k, changes[k]))}
             onToggle={(id, expanded) => changeLayerProperty(id, "expanded", !expanded)} />} />
