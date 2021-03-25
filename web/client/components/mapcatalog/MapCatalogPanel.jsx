94847809dbfd2c9daca5d2581076051837ce7bed
Muhwezi Aggrey <aggrey256@gmail.com>
2021-03-25 10:46:41 +0300

    add ms2-border-layout-content class to virtual scroll

--- web/client/components/mapcatalog/MapCatalogPanel.jsx
+++ web/client/components/mapcatalog/MapCatalogPanel.jsx
@@ -172,7 +172,7 @@ export default compose(
             initialStreamDebounce: 300
         },
         scrollSpyOptions: {
-            querySelector: '.map-catalog-panel > .map-catalog > .ms2-border-layout-body > .ms2-border-layout-content',
+            querySelector: '.map-catalog-panel > .map-catalog > .ms2-border-layout-body',
             pageSize: 12
         },
         hasMore: ({items = [], total = 0}) => items.length < total
