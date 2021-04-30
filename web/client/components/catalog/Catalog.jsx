d8403f4d9a0e19850dde56d63e6da5cf06d1853c
mv88 <matteo.velludini.geosolutions@gmail.com>
2021-04-30 11:35:31 +0200

    fix lint

--- web/client/components/catalog/Catalog.jsx
+++ web/client/components/catalog/Catalog.jsx
@@ -5,7 +5,7 @@
  * This source code is licensed under the BSD-style license found in the
  * LICENSE file in the root directory of this source tree.
 */
-import { isNil, has, omit, isObject } from 'lodash';
+import { isNil } from 'lodash';
 
 import assign from 'object-assign';
 import PropTypes from 'prop-types';
