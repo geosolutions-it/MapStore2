d980af9960879bb6d91ad4dd7ba6f3a381f50f35
Lorenzo Natali <lorenzo.natali@geo-solutions.it>
2021-05-07 14:35:59 +0200

    improved comparison robustness using lodash isEqual

--- web/client/plugins/FeatureEditor.jsx
+++ web/client/plugins/FeatureEditor.jsx
@@ -9,7 +9,7 @@ import React from 'react';
 import {connect} from 'react-redux';
 import {createSelector, createStructuredSelector} from 'reselect';
 import {bindActionCreators} from 'redux';
-import { get, pick } from 'lodash';
+import { get, pick, isEqual } from 'lodash';
 import {compose, lifecycle} from 'recompose';
 import ReactDock from 'react-dock';
 
@@ -276,7 +276,7 @@ const EditorPlugin = compose(
         componentDidUpdate(oldProps) {
             const newOptions = pick(this.props, ['showFilteredObject', 'showTimeSync', 'timeSync', 'customEditorsOptions']);
             const oldOptions = pick(oldProps, ['showFilteredObject', 'showTimeSync', 'timeSync', 'customEditorsOptions']);
-            if (JSON.stringify(newOptions) !== JSON.stringify(oldOptions) ) {
+            if (!isEqual(newOptions, oldOptions) ) {
                 this.props.onMount(newOptions);
             }
         }
