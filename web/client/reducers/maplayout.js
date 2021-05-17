02017fe2593d49b61b7295fb63bca8a837644f0e
Suren <dsuren1@gmail.com>
2021-05-17 20:27:17 +0530

    #6732: Map decentering on scale zoom fix (#6872)

--- web/client/reducers/maplayout.js
+++ web/client/reducers/maplayout.js
@@ -10,6 +10,13 @@ import { UPDATE_MAP_LAYOUT } from '../actions/maplayout';
 
 import assign from 'object-assign';
 
+/**
+ * Manages the map layout state. Determines and manages the placement of the components on the screen
+ * @prop {object} [layout] property helps in organizing the components on the screen (ex. Leaves space for the footer, move the navBar or the background selector when the Catalog or the TOC panel is open, respectively)
+ * @prop {object} [boundingMapRect] property treated as "padding" of the map that allows to zoom taking into account the panels that overlaps (ex. Catalog, Feature Grid)
+ *
+ * @memberof reducers
+ */
 function mapLayout(state = { layout: {}, boundingMapRect: {} }, action) {
     switch (action.type) {
     case UPDATE_MAP_LAYOUT: {
