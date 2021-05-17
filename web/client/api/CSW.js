e3c2f992f8c3778dc491996e19dbe1da1bfa23b3
Muhwezi Deo <39124174+MuhweziDeo@users.noreply.github.com>
2021-05-17 11:48:40 +0300

    #6813 filter request layers only (#6818)

--- web/client/api/CSW.js
+++ web/client/api/CSW.js
@@ -25,6 +25,71 @@ const parseUrl = (url) => {
     }));
 };
 
+export const constructXMLBody = (startPosition, maxRecords, searchText) => {
+    if (!searchText) {
+        return `<csw:GetRecords xmlns:csw="http://www.opengis.net/cat/csw/2.0.2"
+        xmlns:ogc="http://www.opengis.net/ogc"
+        xmlns:gml="http://www.opengis.net/gml"
+        xmlns:dc="http://purl.org/dc/elements/1.1/"
+        xmlns:dct="http://purl.org/dc/terms/"
+        xmlns:gmd="http://www.isotc211.org/2005/gmd"
+        xmlns:gco="http://www.isotc211.org/2005/gco"
+        xmlns:gmi="http://www.isotc211.org/2005/gmi"
+        xmlns:ows="http://www.opengis.net/ows" service="CSW" version="2.0.2" resultType="results" startPosition="${startPosition}" maxRecords="${maxRecords}">
+        <csw:Query typeNames="csw:Record">
+            <csw:ElementSetName>full</csw:ElementSetName>
+            <csw:Constraint version="1.1.0">
+        <ogc:Filter>
+            <ogc:Or>
+            <ogc:PropertyIsEqualTo>
+                <ogc:PropertyName>dc:type</ogc:PropertyName>
+                <ogc:Literal>dataset</ogc:Literal>
+            </ogc:PropertyIsEqualTo>
+            <ogc:PropertyIsEqualTo>
+                <ogc:PropertyName>dc:type</ogc:PropertyName>
+                <ogc:Literal>http://purl.org/dc/dcmitype/Dataset</ogc:Literal>
+            </ogc:PropertyIsEqualTo>
+           </ogc:Or>
+        </ogc:Filter>
+            </csw:Constraint>
+        </csw:Query>
+    </csw:GetRecords>`;
+    }
+    return `<csw:GetRecords xmlns:csw="http://www.opengis.net/cat/csw/2.0.2"
+    xmlns:ogc="http://www.opengis.net/ogc"
+    xmlns:gml="http://www.opengis.net/gml"
+    xmlns:dc="http://purl.org/dc/elements/1.1/"
+    xmlns:dct="http://purl.org/dc/terms/"
+    xmlns:gmd="http://www.isotc211.org/2005/gmd"
+    xmlns:gco="http://www.isotc211.org/2005/gco"
+    xmlns:gmi="http://www.isotc211.org/2005/gmi"
+    xmlns:ows="http://www.opengis.net/ows" service="CSW" version="2.0.2" resultType="results" startPosition="${startPosition}" maxRecords="${maxRecords}">
+    <csw:Query typeNames="csw:Record">
+        <csw:ElementSetName>full</csw:ElementSetName>
+        <csw:Constraint version="1.1.0">
+            <ogc:Filter>
+            <ogc:And>
+                <ogc:PropertyIsLike wildCard="%" singleChar="_" escapeChar="\\">
+                    <ogc:PropertyName>csw:AnyText</ogc:PropertyName>
+                    <ogc:Literal>%${searchText}%</ogc:Literal>
+                </ogc:PropertyIsLike>
+                <ogc:Or>
+                <ogc:PropertyIsEqualTo>
+                    <ogc:PropertyName>dc:type</ogc:PropertyName>
+                    <ogc:Literal>dataset</ogc:Literal>
+                </ogc:PropertyIsEqualTo>
+                <ogc:PropertyIsEqualTo>
+                    <ogc:PropertyName>dc:type</ogc:PropertyName>
+                    <ogc:Literal>http://purl.org/dc/dcmitype/Dataset</ogc:Literal>
+                </ogc:PropertyIsEqualTo>
+               </ogc:Or>
+            </ogc:And>
+            </ogc:Filter>
+        </csw:Constraint>
+    </csw:Query>
+</csw:GetRecords>`;
+};
+
 /**
  * API for local config
  */
@@ -86,11 +151,14 @@ var Api = {
     getRecords: function(url, startPosition, maxRecords, filter) {
         return new Promise((resolve) => {
             require.ensure(['../utils/ogc/CSW', '../utils/ogc/Filter'], () => {
-                const {CSW, marshaller, unmarshaller} = require('../utils/ogc/CSW');
+                const {CSW, marshaller, unmarshaller } = require('../utils/ogc/CSW');
                 let body = marshaller.marshalString({
                     name: "csw:GetRecords",
-                    value: CSW.getRecords(startPosition, maxRecords, filter)
+                    value: CSW.getRecords(startPosition, maxRecords, typeof filter !== "string" && filter)
                 });
+                if (!filter || typeof filter === "string") {
+                    body = constructXMLBody(startPosition, maxRecords, filter);
+                }
                 resolve(axios.post(parseUrl(url), body, { headers: {
                     'Content-Type': 'application/xml'
                 }}).then(
@@ -215,17 +283,7 @@ var Api = {
     },
     textSearch: function(url, startPosition, maxRecords, text) {
         return new Promise((resolve) => {
-            require.ensure(['../utils/ogc/CSW', '../utils/ogc/Filter'], () => {
-                const {Filter} = require('../utils/ogc/Filter');
-                // creates a request like this:
-                // <ogc:PropertyName>apiso:AnyText</ogc:PropertyName><ogc:Literal>%a%</ogc:Literal></ogc:PropertyIsLike>
-                let filter = null;
-                if (text) {
-                    let ops = Filter.propertyIsLike("csw:AnyText", "%" + text + "%");
-                    filter = Filter.filter(ops);
-                }
-                resolve(Api.getRecords(url, startPosition, maxRecords, filter));
-            });
+            resolve(Api.getRecords(url, startPosition, maxRecords, text));
         });
     },
     workspaceSearch: function(url, startPosition, maxRecords, text, workspace) {
