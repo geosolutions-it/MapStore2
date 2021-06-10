4e80fa5119f7d183d3595869541d57a7fee267d7
stefano bovio <stefano.bovio@geo-solutions.it>
2021-06-10 14:38:02 +0200

    #6960: Map error when default_map_backgrounds is missing. (#6968)

--- web/client/selectors/catalog.js
+++ web/client/selectors/catalog.js
@@ -15,7 +15,7 @@ import { DEFAULT_FORMAT_WMS, getUniqueInfoFormats } from "../utils/CatalogUtils"
 export const staticServicesSelector = (state) => get(state, "catalog.default.staticServices");
 export const servicesSelector = (state) => get(state, "catalog.services");
 export const servicesSelectorWithBackgrounds = createSelector(staticServicesSelector, servicesSelector, (staticServices, services) => {
-    const backgroundService = staticServices.default_map_backgrounds;
+    const backgroundService = staticServices?.default_map_backgrounds;
     if (backgroundService) {
         // static services are readOnly by default
         backgroundService.readOnly = true;
