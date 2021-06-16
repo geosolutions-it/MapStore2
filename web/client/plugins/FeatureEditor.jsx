74f846402cf23234f6b3f68d7e7ecef2bf001f61
mv88 <matteo.velludini.geosolutions@gmail.com>
2021-06-16 16:48:40 +0200

    #6894 Externalize Sync map tool for feature Grid

--- web/client/plugins/FeatureEditor.jsx
+++ web/client/plugins/FeatureEditor.jsx
@@ -12,24 +12,25 @@ import {bindActionCreators} from 'redux';
 import { get, pick, isEqual } from 'lodash';
 import {compose, lifecycle} from 'recompose';
 import ReactDock from 'react-dock';
-
-import { createPlugin } from '../utils/PluginsUtils';
-
-import * as epics from '../epics/featuregrid';
-import featuregrid from '../reducers/featuregrid';
+import ContainerDimensions from 'react-container-dimensions';
 
 import Grid from '../components/data/featuregrid/FeatureGrid';
+import BorderLayout from '../components/layout/BorderLayout';
+
+import { createPlugin } from '../utils/PluginsUtils';
+import { toChangesMap} from '../utils/FeatureGridUtils';
+import { initPlugin, sizeChange, setUp, setSyncTool} from '../actions/featuregrid';
+import * as epics from '../epics/featuregrid';
+import featuregrid from '../reducers/featuregrid';
+import {mapLayoutValuesSelector} from '../selectors/maplayout';
 import {paginationInfo, describeSelector, wfsURLSelector, typeNameSelector} from '../selectors/query';
 import {modeSelector, changesSelector, newFeaturesSelector, hasChangesSelector, selectedFeaturesSelector, getDockSize} from '../selectors/featuregrid';
-import { toChangesMap} from '../utils/FeatureGridUtils';
+
 import {getPanels, getHeader, getFooter, getDialogs, getEmptyRowsView, getFilterRenderers} from './featuregrid/panels/index';
-import BorderLayout from '../components/layout/BorderLayout';
+import {gridTools, gridEvents, pageEvents, toolbarEvents} from './featuregrid/index';
+
 const EMPTY_ARR = [];
 const EMPTY_OBJ = {};
-import {gridTools, gridEvents, pageEvents, toolbarEvents} from './featuregrid/index';
-import { initPlugin, sizeChange, setUp} from '../actions/featuregrid';
-import ContainerDimensions from 'react-container-dimensions';
-import {mapLayoutValuesSelector} from '../selectors/maplayout';
 
 
 const Dock = connect(createSelector(
@@ -263,12 +264,16 @@ const selector = createSelector(
 const EditorPlugin = compose(
     connect(() => ({}),
         (dispatch) => ({
-            onMount: bindActionCreators(setUp, dispatch)
+            onMount: bindActionCreators(setUp, dispatch),
+            setSyncTool: bindActionCreators(setSyncTool, dispatch)
         })),
     lifecycle({
         componentDidMount() {
             // only the passed properties will be picked
             this.props.onMount(pick(this.props, ['showFilteredObject', 'showTimeSync', 'timeSync', 'customEditorsOptions']));
+            if (this.props.isSyncActive) {
+                this.props.setSyncTool(true);
+            }
         },
         // TODO: fix this in contexts
         // due to multiple renders of plugins in contexts (one with default props, then with context props)
