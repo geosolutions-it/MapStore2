dab9a36463967a85bfd2c830036878ead6b200e9
Muhwezi Deo <39124174+MuhweziDeo@users.noreply.github.com>
2021-06-24 16:20:48 +0300

    #6882: Fix Time filter panel for many dates is not completely visible  (#7022)

--- web/client/components/data/query/DateField.jsx
+++ web/client/components/data/query/DateField.jsx
@@ -71,7 +71,7 @@ class DateField extends React.Component {
 
         let dateRow = this.props.operator === "><" ?
             (<div className="query-field">
-                <div className="query-field-value">
+                <div className="query-field-value date-filter-left">
                     {this.props.showLabels && <Message msgId="queryform.from"/>}
                     <UTCDateTimePicker
                         type={this.props.attType}
@@ -82,7 +82,7 @@ class DateField extends React.Component {
                         format={getDateTimeFormat(this.context.locale, this.props.attType)}
                         onChange={(date) => this.updateValueState({startDate: date, endDate: enddate})}/>
                 </div>
-                <div className="query-field-value">
+                <div className="query-field-value date-filter-right">
                     {this.props.showLabels && <Message msgId="queryform.to"/>}
                     <UTCDateTimePicker
                         type={this.props.attType}
