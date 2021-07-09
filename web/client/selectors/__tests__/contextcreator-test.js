56d8880afb879f18e82d314fa4e17cc3820662e2
mv88 <matteo.velludini.geosolutions@gmail.com>
2021-07-09 19:24:05 +0200

    update test

--- web/client/selectors/__tests__/contextcreator-test.js
+++ web/client/selectors/__tests__/contextcreator-test.js
@@ -45,6 +45,6 @@ describe('contextcreator selectors', () => {
                 ...testState.contextcreator,
                 selectedTheme: themeDark
             }
-        })).toEqual(themeDark);
+        })).toEqual(themeDark.id);
     });
 });
