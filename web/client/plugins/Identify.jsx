c5d91aa2ca3f56bae63bfe265fe70605f205eaf1
Lorenzo Natali <lorenzo.natali@geo-solutions.it>
2021-07-15 18:03:53 +0200

    Update Identify.jsx

--- web/client/plugins/Identify.jsx
+++ web/client/plugins/Identify.jsx
@@ -199,6 +199,8 @@ const identifyDefaultProps = defaultProps({
  * @prop cfg.zIndex {number} component z index order
  * @prop cfg.showInMapPopup {boolean} if true show the identify as popup
  * @prop cfg.showMoreInfo {boolean} if true shows the more info icon which allow user to show/hide Geocode viewer as popup (true by default)
+ * @prop cfg.showEdit {boolean} if true, and when the FeatureEditor plugin is present, shows and edit button to edit the current feature(s) clicked in the grid.
+ 
  *
  * @example
  * {
