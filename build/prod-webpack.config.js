5bdfe734b0b206dde4088d4b20afd6ef39554ba3
Lorenzo Natali <offtherailz@gmail.com>
2021-07-21 18:40:26 +0200

    Update prod-webpack.config.js

--- build/prod-webpack.config.js
+++ build/prod-webpack.config.js
@@ -15,7 +15,7 @@ module.exports = require('./buildConfig')(
     {
         "mapstore2": path.join(paths.code, "product", "app"),
         "embedded": path.join(paths.code, "product", "embedded"),
-        "api": path.join(paths.code, "product", "api"),
+        "ms2-api": path.join(paths.code, "product", "api"),
         "dashboard-embedded": path.join(paths.code, "product", "dashboardEmbedded"),
         "geostory-embedded": path.join(paths.code, "product", "geostoryEmbedded")
     },
