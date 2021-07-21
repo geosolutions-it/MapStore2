29c5b0736bb5654f263a590304a9fd19b614a675
ashraf-alsamman <ashraf.alsamman@gmail.com>
2021-07-21 16:23:25 +0200

    Update web/client/components/TOC/fragments/settings/Display.jsx
    Co-authored-by: Lorenzo Natali <lorenzo.natali@geo-solutions.it>

--- web/client/components/TOC/fragments/settings/Display.jsx
+++ web/client/components/TOC/fragments/settings/Display.jsx
@@ -28,7 +28,6 @@ export default class extends React.Component {
         formats: PropTypes.array,
         settings: PropTypes.object,
         onChange: PropTypes.func,
-        getUniqueInfoFormats: PropTypes.func,
         containerWidth: PropTypes.number,
         currentLocaleLanguage: PropTypes.string,
         isLocalizedLayerStylesEnabled: PropTypes.bool,
