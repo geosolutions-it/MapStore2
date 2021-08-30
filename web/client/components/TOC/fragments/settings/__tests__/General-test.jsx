18b2e9462d5f9b6ca96183a1917830399d14e0a8
Matteo V <matteo.velludini@geo-solutions.it>
2021-08-30 12:51:09 +0200

    #7240 add support for Swedish language (#7239) (#7248)
    #7240 add support for Swedish language (#7239)
    
    Co-authored-by: Mats Elfström <mats.elfstrom@giskraft.se>

--- web/client/components/TOC/fragments/settings/__tests__/General-test.jsx
+++ web/client/components/TOC/fragments/settings/__tests__/General-test.jsx
@@ -44,7 +44,7 @@ describe('test  Layer Properties General module component', () => {
         expect(comp).toExist();
         const inputs = ReactTestUtils.scryRenderedDOMComponentsWithTag( comp, "input" );
         expect(inputs).toExist();
-        expect(inputs.length).toBe(16);
+        expect(inputs.length).toBe(17);
 
     });
     it('tests Layer Properties Display component events', () => {
@@ -69,7 +69,7 @@ describe('test  Layer Properties General module component', () => {
         expect(comp).toExist();
         const inputs = ReactTestUtils.scryRenderedDOMComponentsWithTag( comp, "input" );
         expect(inputs).toExist();
-        expect(inputs.length).toBe(16);
+        expect(inputs.length).toBe(17);
         ReactTestUtils.Simulate.change(inputs[0]);
         ReactTestUtils.Simulate.blur(inputs[1]);
         expect(spy.calls.length).toBe(1);
