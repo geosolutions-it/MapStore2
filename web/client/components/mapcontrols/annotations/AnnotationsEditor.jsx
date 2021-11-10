3d0b2214fce8fdd17db3354f8c9432de56863f08
Alexander <mineralex1991@gmail.com>
2021-11-10 20:24:00 +0300

    #7508 - Fix for app crash in annotations due to invalid geometries
    - Moved code that checks validness of all edited geometries into separate method.
    - Each geometry in the list will have info whether there invalid geometry exists. If so, all geometries except selected will be styled as inactive and user will see popover with details why it happens. Inactive geometries will skip selection event. Invalid geometry will skip deselection event.
    - Removed validity indicator from Geometries (Features) list because it's redundant. Only one geometry that is currently selected can be invalid.
    - Refactoring to improve code readability.
    - Fixed typos.
    
    - Fixed conditions that led to creation of new "Marker" (Point) and "Text" geometry in case if currently selected Marker or Text is invalid.

--- web/client/components/mapcontrols/annotations/AnnotationsEditor.jsx
+++ web/client/components/mapcontrols/annotations/AnnotationsEditor.jsx
@@ -243,7 +243,8 @@ class AnnotationsEditor extends React.Component {
         onToggleShowAgain: PropTypes.func,
         onInitPlugin: PropTypes.func,
         onGeometryHighlight: PropTypes.func,
-        onUnSelectFeature: PropTypes.func
+        onUnSelectFeature: PropTypes.func,
+        setIsValidFeature: PropTypes.func
     };
 
     static defaultProps = {
@@ -350,6 +351,8 @@ class AnnotationsEditor extends React.Component {
     };
 
     renderEditingCoordButtons = () => {
+
+        const allGeometryIsValid = this.allGeometryIsValid();
         return (<Grid className="mapstore-annotations-info-viewer-buttons" fluid>
             <Row className="text-center noTopMargin">
                 <Col xs={12}>
@@ -391,8 +394,8 @@ class AnnotationsEditor extends React.Component {
                                 }
                             }, {
                                 glyph: 'floppy-disk',
-                                tooltipId: !isEmpty(this.props.selected) ? "annotations.saveGeometry" : "annotations.save",
-                                disabled: this.props.selected && this.props.selected.properties && !this.props.selected.properties.isValidFeature,
+                                tooltipId: !allGeometryIsValid ? "annotations.geometryError" : !isEmpty(this.props.selected) ? "annotations.saveGeometry" : "annotations.save",
+                                disabled: (this.props.selected && this.props.selected.properties && !this.props.selected.properties.isValidFeature) || !allGeometryIsValid,
                                 onClick: () => this.save()
                             },
                             {
@@ -498,6 +501,8 @@ class AnnotationsEditor extends React.Component {
                     geodesic={this.props.geodesic}
                     defaultPointType={this.getConfig().defaultPointType}
                     defaultStyles={this.props.defaultStyles}
+                    setIsValidFeature={this.props.setIsValidFeature}
+                    allGeometryIsValid={this.allGeometryIsValid}
                 />
                 }
             </div>
@@ -730,6 +735,7 @@ class AnnotationsEditor extends React.Component {
                                 onSetInvalidSelected={this.props.onSetInvalidSelected}
                                 onChangeText={this.props.onChangeText}
                                 renderer={"annotations"}
+                                setIsValidFeature={this.props.setIsValidFeature}
                             />
                             }
                             {this.state.tabValue === 'style' &&
@@ -763,6 +769,16 @@ class AnnotationsEditor extends React.Component {
             </div>
         );
     }
+
+    allGeometryIsValid = () => {
+        let allGeometryIsValid = true;
+        if (this.props?.editing?.features && this.props.editing?.features?.length > 0) {
+            this.props.editing.features.map((p) => {
+                if (!p?.properties?.isValidFeature) { allGeometryIsValid = false; }
+            });
+        }
+        return allGeometryIsValid;
+    };
 
     change = (field, value) => {
         this.props.onChangeProperties(field, value);
