90a18875541d2707f2e9b2498ea5244744587618
Matteo V <matteo.velludini@geo-solutions.it>
2021-11-11 14:38:29 +0100

    #7534 changes to the buildConfig calls (#7535)
    * #7534 changes to the buildConfig calls
    
    * Update build/prod-webpack.config.js

--- build/webpack.config.js
+++ build/webpack.config.js
@@ -5,21 +5,22 @@ const extractThemesPlugin = require('./themes.js').extractThemesPlugin;
 const moduleFederationPlugin = require('./moduleFederation.js').plugin;
 const config = require('./buildConfig')(
     {
-        [process.env.bundle || "mapstore2"]: path.join(__dirname, "..", "web", "client", "product", process.env.entrypoint || process.env.bundle || "app"),
-        "embedded": path.join(__dirname, "..", "web", "client", "product", "embedded"),
-        "ms2-api": path.join(__dirname, "..", "web", "client", "product", "api"),
-        "dashboard-embedded": path.join(__dirname, "..", "web", "client", "product", "dashboardEmbedded"),
-        "geostory-embedded": path.join(__dirname, "..", "web", "client", "product", "geostoryEmbedded")
-    },
-    themeEntries,
-    {
-        base: path.join(__dirname, ".."),
-        dist: path.join(__dirname, "..", "web", "client", "dist"),
-        framework: path.join(__dirname, "..", "web", "client"),
-        code: path.join(__dirname, "..", "web", "client")
-    },
-    [extractThemesPlugin, moduleFederationPlugin],
-    false,
-    undefined
+        bundles: {
+            [process.env.bundle || "mapstore2"]: path.join(__dirname, "..", "web", "client", "product", process.env.entrypoint || process.env.bundle || "app"),
+            "embedded": path.join(__dirname, "..", "web", "client", "product", "embedded"),
+            "ms2-api": path.join(__dirname, "..", "web", "client", "product", "api"),
+            "dashboard-embedded": path.join(__dirname, "..", "web", "client", "product", "dashboardEmbedded"),
+            "geostory-embedded": path.join(__dirname, "..", "web", "client", "product", "geostoryEmbedded")
+        },
+        themeEntries,
+        paths: {
+            base: path.join(__dirname, ".."),
+            dist: path.join(__dirname, "..", "web", "client", "dist"),
+            framework: path.join(__dirname, "..", "web", "client"),
+            code: path.join(__dirname, "..", "web", "client")
+        },
+        plugins: [extractThemesPlugin, moduleFederationPlugin],
+        prod: false
+    }
 );
 module.exports = config;
