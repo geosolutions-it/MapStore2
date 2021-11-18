37a89082c1d3c65d5e8f1e396056c04afd47f137
Matteo V <matteo.velludini@geosolutionsgroup.com>
2021-11-18 18:00:33 +0100

    #7534 changes to the buildConfig calls (#7535) (#7537)

--- project/standard/templates/webpack.config.js
+++ project/standard/templates/webpack.config.js
@@ -4,8 +4,8 @@ const themeEntries = require('./MapStore2/build/themes.js').themeEntries;
 const extractThemesPlugin = require('./MapStore2/build/themes.js').extractThemesPlugin;
 const ModuleFederationPlugin = require('./MapStore2/build/moduleFederation').plugin;
 
-module.exports = require('./MapStore2/build/buildConfig')(
-    {
+module.exports = require('./MapStore2/build/buildConfig')({
+    bundles: {
         '__PROJECTNAME__': path.join(__dirname, "js", "app"),
         '__PROJECTNAME__-embedded': path.join(__dirname, "js", "embedded"),
         '__PROJECTNAME__-api': path.join(__dirname, "MapStore2", "web", "client", "product", "api"),
@@ -13,20 +13,20 @@ module.exports = require('./MapStore2/build/buildConfig')(
         "dashboard-embedded": path.join(__dirname, "js", "dashboardEmbedded")
     },
     themeEntries,
-    {
+    paths: {
         base: __dirname,
         dist: path.join(__dirname, "dist"),
         framework: path.join(__dirname, "MapStore2", "web", "client"),
         code: [path.join(__dirname, "js"), path.join(__dirname, "MapStore2", "web", "client")]
     },
-    [extractThemesPlugin, ModuleFederationPlugin],
-    false,
-    undefined,
-    '.__PROJECTNAME__',
-    [],
-    {
+    plugins: [extractThemesPlugin, ModuleFederationPlugin],
+    prod: false,
+    publicPath: undefined,
+    cssPrefix: '.__PROJECTNAME__',
+    prodPlugins: [],
+    alias: {
         "@mapstore/patcher": path.resolve(__dirname, "node_modules", "@mapstore", "patcher"),
         "@mapstore": path.resolve(__dirname, "MapStore2", "web", "client"),
         "@js": path.resolve(__dirname, "js")
     }
-);
+});
