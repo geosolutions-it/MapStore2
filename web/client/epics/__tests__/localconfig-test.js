b6f791992e20e747acf666431d8c30c6d55c809c
Matteo V <matteo.velludini@geosolutionsgroup.com>
2022-01-10 12:23:56 +0100

    #6695 Sk translation (#7698)
    add support for sk locale

--- web/client/epics/__tests__/localconfig-test.js
+++ web/client/epics/__tests__/localconfig-test.js
@@ -25,7 +25,7 @@ describe('localconfig Epics', () => {
                 const suppLocales = getSupportedLocales();
                 switch (action.type) {
                 case SUPPORTED_LOCALES_REGISTERED:
-                    expect(Object.keys(suppLocales).length).toBe(12);
+                    expect(Object.keys(suppLocales).length).toBe(13);
                     break;
                 default:
                     expect(true).toBe(false);
