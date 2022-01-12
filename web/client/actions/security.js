444fd5c8234f4b30ba83bbd2eb0fb05a73e6f6f4
Lorenzo Natali <lorenzonatali@geosolutionsgroup.com>
2022-01-12 12:30:34 +0100

    restored previous changes

--- web/client/actions/security.js
+++ web/client/actions/security.js
@@ -35,6 +35,7 @@ export function loginSuccess(userDetails, username, password, authProvider) {
         userDetails: userDetails,
         // set here for compatibility reasons
         // TODO: verify if the compatibility reasons still hold and remove otherwise
+        authHeader: 'Basic ' + btoa(username + ':' + password),
         username: username,
         password: password,
         authProvider: authProvider
@@ -98,10 +99,11 @@ export function login(username, password) {
     };
 }
 
-export function changePasswordSuccess(user) {
+export function changePasswordSuccess(user, newPassword) {
     return {
         type: CHANGE_PASSWORD_SUCCESS,
-        user: user
+        user: user,
+        authHeader: 'Basic ' + btoa(user.name + ':' + newPassword)
     };
 }
 
