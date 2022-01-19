3a46cdefdae62bf7cf601109437a7546734952e7
Lorenzo Natali <lorenzonatali@geosolutionsgroup.com>
2022-01-19 17:40:31 +0100

    Street view doc enhancement

--- web/client/plugins/StreetView/StreetView.jsx
+++ web/client/plugins/StreetView/StreetView.jsx
@@ -40,9 +40,11 @@ const StreetViewPluginContainer = connect(() => ({}), {
  * @memberof plugins
  * @property {string} cfg.apiKey The API key to use.
  * Mandatory in production. In order to allow fine billing strategies (with different API keys), the API key can be defined and customized here in this configuration option or in `localConfig.json` at, in order of priority one of:
- * - `apiKeys.googleStreetViewAPIKey`,
- * - `apiKeys.googleAPIKey`,
- * - `googleAPIKey`.
+ * - `apiKeys.googleStreetViewAPIKey` - Use this if you have only one API key for enable for street view JS API for the whole application,
+ * - `apiKeys.googleAPIKey` - Use this if you have a general API key enabled for all Google APIs in MapStore.
+ * - `googleAPIKey` (for retro-compatibility only)
+ *
+ * Generally speaking, you should prefer general settings in `localConfig.json` over the plugin configuration, in order to reuse the same configuration for default viewer and all the contexts, automatically. This way you will not need to configure the `apiKey` in every context.
  * @property {boolean} [cfg.useDataLayer=true] If true, adds to the map a layer for street view data availability when the plugin is turned on.
  * @property {object} [cfg.dataLayerConfig] configuration for the data layer. By default `{provider: 'custom', type: "tileprovider", url: "https://mts1.googleapis.com/vt?hl=en-US&lyrs=svv|cb_client:apiv3&style=40,18&x={x}&y={y}&z={z}"}`
  * @class
