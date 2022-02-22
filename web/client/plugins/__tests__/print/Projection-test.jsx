ad170b809fff9df7de6b8c6e46d53f102a176d33
mbarto <maurobartolomeoli@gmail.com>
2022-02-22 16:18:17 +0100

    #7773: ol Graticule layer support for different projections + print support (#7841)

--- web/client/plugins/__tests__/print/Projection-test.jsx
+++ web/client/plugins/__tests__/print/Projection-test.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import expect from "expect";
 import { getPluginForTest, getByXPath } from '../pluginsTestUtils';
-import { getSpecTransformerChain, getMapTransformerChain, getValidatorsChain, resetDefaultPrintingService } from "../../../utils/PrintUtils";
+import { getMapTransformerChain, getValidatorsChain, resetDefaultPrintingService } from "../../../utils/PrintUtils";
 
 import PrintProjection from "../../print/Projection";
 import last from "lodash/last";
@@ -41,37 +41,10 @@ const initialState = {
     }
 };
 
-const baseSpec = {
-    layers: [{
-        type: "Vector",
-        geoJson: {
-            type: "FeatureCollection",
-            features: [{
-                type: "Point",
-                coordinates: [100000, 200000]
-            }]
-        }
-    }, {
-        type: "unknown"
-    }],
-    projection: "EPSG:3857",
-    srs: "EPSG:3857",
-    pages: [{
-        center: [100000, 200000],
-        scale: 10000
-    }]
-};
-
 function getPrintProjectionPlugin() {
     return Promise.resolve(getPluginForTest(PrintProjection, {
         ...initialState
     }));
-}
-
-function callTransformer(state, callback) {
-    setTimeout(() => {
-        last(getSpecTransformerChain()).transformer(state, baseSpec).then(callback);
-    }, 0);
 }
 
 function callMapTransformer(state, callback) {
@@ -125,51 +98,6 @@ describe('PrintProjection Plugin', () => {
                 expect(getByXPath("//option[@value='EPSG:3857']")).toExist();
                 expect(getByXPath("//option[@value='EPSG:900913']")).toNotExist();
                 done();
-            } catch (ex) {
-                done(ex);
-            }
-        });
-    });
-
-    it('transformer with default crs', (done) => {
-        getPrintProjectionPlugin().then(({ Plugin, store }) => {
-            try {
-                ReactDOM.render(<Plugin projections={[{"name": "Mercator", "value": "EPSG:3857"}, {"name": "WGS84", "value": "EPSG:4326"}]}/>, document.getElementById("container"));
-                callTransformer(store.getState(), (spec) => {
-                    expect(spec.pages[0].center[0].toFixed(2)).toEqual(100000);
-                    expect(spec.pages[0].center[1].toFixed(2)).toEqual(200000);
-                    expect(spec.pages[0].scale.toFixed(0)).toEqual(10000);
-                    expect(spec.srs).toBe('EPSG:3857');
-                    done();
-                });
-            } catch (ex) {
-                done(ex);
-            }
-        });
-    });
-
-    it('transformer with user chosen crs', (done) => {
-        getPrintProjectionPlugin().then(({ Plugin, store }) => {
-            try {
-                ReactDOM.render(<Plugin projections={[{"name": "Mercator", "value": "EPSG:3857"}, {"name": "WGS84", "value": "EPSG:4326"}]}/>, document.getElementById("container"));
-                const combo = getByXPath("//select");
-                ReactTestUtils.Simulate.change(combo, {
-                    target: {
-                        value: "EPSG:4326"
-                    }
-                });
-                callTransformer(store.getState(), (spec) => {
-                    expect(spec.pages[0].center[0].toFixed(2)).toEqual(0.9);
-                    expect(spec.pages[0].center[1].toFixed(2)).toEqual(1.80);
-                    expect(spec.pages[0].scale.toFixed(0)).toEqual(125483353);
-                    expect(spec.srs).toBe('EPSG:4326');
-                    expect(spec.layers.length).toBe(2);
-                    expect(spec.layers[0].type).toBe("Vector");
-                    expect(spec.layers[0].geoJson.features.length).toBe(1);
-                    expect(spec.layers[0].geoJson.features[0].coordinates[0].toFixed(2)).toEqual(0.90);
-                    expect(spec.layers[0].geoJson.features[0].coordinates[1].toFixed(2)).toEqual(1.80);
-                    done();
-                });
             } catch (ex) {
                 done(ex);
             }
