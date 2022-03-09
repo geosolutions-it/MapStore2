37a1468c87e929489b4dbdd9616b703c015e26f6
Anthony Agyekum Mensah <tony.m3nsa@gmail.com>
2022-03-09 16:24:51 +0000

    Issue #7868; removed redundant test fix

--- web/client/epics/__tests__/measurement-test.jsx
+++ web/client/epics/__tests__/measurement-test.jsx
@@ -267,13 +267,6 @@ describe('measurement epics', () => {
                     enabled: true
                 }
             },
-            localConfig: {
-                plugins: {
-                    desktop: [{
-                        name: 'Measure'
-                    }]
-                }
-            },
             measurement: {
                 currentFeature: 0,
                 features: [{
