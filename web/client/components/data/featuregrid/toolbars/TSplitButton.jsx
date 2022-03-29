1258be71fd041145adabc881d12956b37d8c0da9
Alexander <mineralex1991@gmail.com>
2022-03-29 15:46:49 +0300

    - Toggle of snapping when edit mode is disabled; - Re-applied styles of labels inside the dropdown with custom elements - Proper handling of number/text input changes, dropdown won't be closed unexpectedly. - Snapping layer stroke opacity will be 0.

--- web/client/components/data/featuregrid/toolbars/TSplitButton.jsx
+++ web/client/components/data/featuregrid/toolbars/TSplitButton.jsx
@@ -6,7 +6,7 @@
  * LICENSE file in the root directory of this source tree.
  */
 
-import React, {useEffect} from 'react';
+import React, {forwardRef, useEffect, useState} from 'react';
 import {Button, Dropdown} from 'react-bootstrap';
 import ContainerDimensions from 'react-container-dimensions';
 
@@ -14,10 +14,25 @@ import ContainerDimensions from 'react-container-dimensions';
 import './TSplitButton.less';
 import classnames from "classnames";
 
-export const SimpleTButton = ({ disabled, id, visible, onClick, active, buttonClassName = "square-button-md", menuStyle = {}, className, children, onMount, ...props }) => {
+export const SimpleTButton = forwardRef(({ disabled, id, visible, onClick, active, buttonClassName = "square-button-md",
+    menuStyle = {}, className, children, onMount = () => {}, ...props }, ref) => {
+    const [isShown, setIsShown] = useState(false);
     useEffect(() => {
         typeof onMount === 'function' && onMount();
     }, []);
+
+    const onSelectHandler = (e) => {
+        if  (["number", "text"].includes(e.target.type)) {
+            setIsShown(true);
+        } else {
+            setIsShown(!isShown);
+        }
+    };
+    const onToggleHandler = (isOpen, e) => {
+        if (e) {
+            setIsShown(isOpen);
+        }
+    };
 
     if (!visible) return false;
     return (
@@ -25,17 +40,20 @@ export const SimpleTButton = ({ disabled, id, visible, onClick, active, buttonCl
             <Dropdown className={classnames({
                 "split-button": true,
                 ...(className ? {[className]: true} : {})
-            })}>
-                <Button id={id} onClick={() => !disabled && onClick()} className={buttonClassName} bsStyle={active ? "success" : "primary"}>{props.title}</Button>
+            })}
+            open={isShown}
+            onToggle={onToggleHandler}
+            >
+                <Button ref={ref} id={id} onClick={() => !disabled && onClick()} className={buttonClassName} bsStyle={active ? "success" : "primary"} {...props}>{props.title}</Button>
                 <Dropdown.Toggle bsStyle={active ? "success" : "primary"} />
-                <Dropdown.Menu style={menuStyle}>
+                <Dropdown.Menu style={menuStyle} onSelect={onSelectHandler}>
                     {children}
                 </Dropdown.Menu>
             </Dropdown>
         </ContainerDimensions>
 
     );
-};
+});
 
 
 export default SimpleTButton;
