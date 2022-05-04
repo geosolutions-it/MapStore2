cd0ca65147c5b1db8a5b8817f25eba96338b2884
Alexander <mineralex1991@gmail.com>
2022-05-04 15:36:19 +0300

    DockContainer and ResponsivePanel documentation

--- web/client/components/misc/panels/DockContainer.jsx
+++ web/client/components/misc/panels/DockContainer.jsx
@@ -1,6 +1,19 @@
 import React from 'react';
 import classnames from "classnames";
 
+/**
+ * Wrapper for DockablePanel with main intension to support applying of custom styling to make dock panels have proper
+ * offset depending on the sidebars presence on the page
+ * @memberof components.misc.panels
+ * @name DockContainer
+ * @param id {string} - id applied to the container
+ * @param children {JSX.Element}
+ * @param dockStyle {object} - style object obtained from mapLayoutValuesSelector and used to calculate offsets
+ * @param className {string} - class name
+ * @param style - style object to apply to the container. Can be used to overwrite styles applied by dockStyle calculations
+ * @returns {JSX.Element}
+ * @constructor
+ */
 const DockContainer = ({ id, children, dockStyle, className, style = {}}) => {
     const persistentStyle = {
         width: `calc(100% - ${(dockStyle?.right ?? 0) + (dockStyle?.left ?? 0)}px)`,
--- web/client/components/misc/panels/ResponsivePanel.jsx
+++ web/client/components/misc/panels/ResponsivePanel.jsx
@@ -12,21 +12,30 @@ import DockablePanel from "./DockablePanel";
 import React from "react";
 
 /**
+ * Component for rendering DockPanel that supposed to:
+ * - Get dynamic width if panel cannot fit to the screen width. It will be automatically resized to the window width.
+ * - Get proper offsets based on the current map layout: with or without sidebar
+ * @memberof components.misc.panels
  * @name ResponsivePanel
- * @param props
+ * @param {boolean} dock - rendered as a dock if true, otherwise rendered as a modal window
+ * @param {object} containerStyle - style object to be applied to the DockContainer.
+ * @param {string} containerClassName - class name to be applied to the DockContainer.
+ * @param {string} containerId - id to be applied to the DockContainer.
+ * @param {number} size - maximum width of the dock panel.
+ * @param {JSX.Element} children - components to be rendered inside the dock panel.
+ * @param {JSX.Element} siblings - components to be rendered inside container after dock panel.
+ * @param {object} panelProps - props that will be applied to the DockablePanel component.
  * @returns {JSX.Element}
  */
-export default (props) => {
-    const {
-        dock = true,
-        containerStyle,
-        containerClassName,
-        containerId,
-        size,
-        children,
-        siblings,
-        ...panelProps
-    } = props;
+export default ({
+    children,
+    containerClassName,
+    containerId,
+    containerStyle,
+    dock = true,
+    siblings,
+    size,
+    ...panelProps}) => {
     return (
         <DockContainer
             dockStyle={containerStyle}
