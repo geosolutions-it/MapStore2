4207ae389e9bcaf5ed7cfaec7c40eca5cb21077f
sam rubarema <samrubarema6@gmail.com>
2022-05-17 16:05:29 +0300

    #8151 fixes measure tool window that goes below layertree pane

--- build/webpack.config.js
+++ build/webpack.config.js
@@ -1,73 +1,26 @@
 const path = require("path");
 
-const themeEntries = require("./themes.js").themeEntries;
-const extractThemesPlugin = require("./themes.js").extractThemesPlugin;
-const moduleFederationPlugin = require("./moduleFederation.js").plugin;
-const config = require("./buildConfig")({
-    bundles: {
-        [process.env.bundle || "mapstore2"]: path.join(
-            __dirname,
-            "..",
-            "web",
-            "client",
-            "product",
-            process.env.entrypoint || process.env.bundle || "app"
-        ),
-        embedded: path.join(
-            __dirname,
-            "..",
-            "web",
-            "client",
-            "product",
-            "embedded"
-        ),
-        "ms2-api": path.join(
-            __dirname,
-            "..",
-            "web",
-            "client",
-            "product",
-            "api"
-        ),
-        "dashboard-embedded": path.join(
-            __dirname,
-            "..",
-            "web",
-            "client",
-            "product",
-            "dashboardEmbedded"
-        ),
-        "geostory-embedded": path.join(
-            __dirname,
-            "..",
-            "web",
-            "client",
-            "product",
-            "geostoryEmbedded"
-        ),
-    },
-    themeEntries,
-    paths: {
-        base: path.join(__dirname, ".."),
-        dist: path.join(__dirname, "..", "web", "client", "dist"),
-        framework: path.join(__dirname, "..", "web", "client"),
-        code: path.join(__dirname, "..", "web", "client"),
-    },
-    plugins: [extractThemesPlugin, moduleFederationPlugin],
-    prod: false,
-    proxy: {
-        "/rest": {
-            target: "http://localhost/mapstore",
+const themeEntries = require('./themes.js').themeEntries;
+const extractThemesPlugin = require('./themes.js').extractThemesPlugin;
+const moduleFederationPlugin = require('./moduleFederation.js').plugin;
+const config = require('./buildConfig')(
+    {
+        bundles: {
+            [process.env.bundle || "mapstore2"]: path.join(__dirname, "..", "web", "client", "product", process.env.entrypoint || process.env.bundle || "app"),
+            "embedded": path.join(__dirname, "..", "web", "client", "product", "embedded"),
+            "ms2-api": path.join(__dirname, "..", "web", "client", "product", "api"),
+            "dashboard-embedded": path.join(__dirname, "..", "web", "client", "product", "dashboardEmbedded"),
+            "geostory-embedded": path.join(__dirname, "..", "web", "client", "product", "geostoryEmbedded")
         },
-        "/pdf": {
-            target: "http://localhost/mapstore",
+        themeEntries,
+        paths: {
+            base: path.join(__dirname, ".."),
+            dist: path.join(__dirname, "..", "web", "client", "dist"),
+            framework: path.join(__dirname, "..", "web", "client"),
+            code: path.join(__dirname, "..", "web", "client")
         },
-        "/mapstore/pdf": {
-            target: "http://localhost",
-        },
-        "/proxy": {
-            target: "http://localhost/mapstore",
-        },
-    },
-});
+        plugins: [extractThemesPlugin, moduleFederationPlugin],
+        prod: false
+    }
+);
 module.exports = config;
