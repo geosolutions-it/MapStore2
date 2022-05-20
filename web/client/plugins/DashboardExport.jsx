420c71f20b5205b174def827aed40d89d68a4814
weberjavi <weberjavi@gmail.com>
2022-05-20 12:48:17 +0200

    Refactor dashboardExport component

--- web/client/plugins/DashboardExport.jsx
+++ web/client/plugins/DashboardExport.jsx
@@ -14,47 +14,49 @@ import { createSelector } from 'reselect';
 import ExportPanel from '../components/export/ExportPanel';
 import {createPlugin} from "../utils/PluginsUtils";
 import { createControlEnabledSelector } from '../selectors/controls';
-import { originalDataSelector, dashboardResource } from '../selectors/dashboard';
+import { originalDataSelector, dashboardResource, dashboardServicesSelector } from '../selectors/dashboard';
 import { dashboardExport } from '../actions/dashboard';
 
 import { toggleControl } from '../actions/controls';
 import Message from '../components/I18N/Message';
 const isEnabled = createControlEnabledSelector('export');
 
+const mapStateToProps = createSelector(
+    isEnabled,
+    dashboardResource,
+    originalDataSelector,
+    dashboardServicesSelector,
+    (show, resource, originalData, services) => ({
+        show,
+        resource,
+        originalData,
+        services
+    }));
+
+const actions = {
+    onClose: () => toggleControl('export'),
+    onExport: dashboardExport
+};
+
 const Component = ({
     show,
     onClose,
     onExport,
     originalData,
-    resource
-}) => (
-    <ExportPanel
-        show={show}
-        // formats={pick(formats, ...enabledFormats)}
-        // selectedFormat={format}
-        // onSelect={setFormat}
-        onExport={() => {
-            onExport(originalData, resource);
-            onClose();
-        }}
-        onClose={onClose}
-    />
-);
+    resource,
+    services
+}) => {
+    const handleExport = () => onExport(originalData, resource, services);
+    return (
+        <ExportPanel
+            show={show}
+            onExport={handleExport}
+            onClose={onClose}
+        />
+    );
+};
 
-const ConnectedComponent = connect(
-    createSelector(
-        isEnabled,
-        dashboardResource,
-        originalDataSelector,
-        (show, resource, originalData) => ({
-            show,
-            resource,
-            originalData
-        })), {
-        onClose: () => toggleControl('export'),
-        onExport: (originalData, resource) => dashboardExport(originalData, resource)
-    }
-)(Component);
+const ConnectedComponent = connect(mapStateToProps, actions)(Component);
 
 const DashboardExportPlugin = createPlugin('DashboardExport', {
     component: ConnectedComponent,
