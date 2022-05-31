7e179d07d36f721d66c5a65c445b60a3e5e135b2
stefano bovio <stefano.bovio@geosolutionsgroup.com>
2022-05-31 16:31:20 +0200

    #8245: Map with mapbox or mapboxstyle background layers break the app when saved (#8246)
    * include access token to mapbox background layers
    
    * add default mapbox access token variable

--- web/client/utils/ConfigUtils.js
+++ web/client/utils/ConfigUtils.js
@@ -40,6 +40,7 @@ let defaultConfig = {
     themePrefix: "ms2",
     bingApiKey: null,
     mapquestApiKey: null,
+    mapboxAccessToken: '',
     defaultSourceType: "gxp_wmssource",
     backgroundGroup: "background",
     userSessions: {
@@ -168,6 +169,10 @@ export const setApiKeys = function(layer) {
     }
     if (layer.type === 'mapquest') {
         layer.apiKey = defaultConfig.mapquestApiKey;
+    }
+    if (layer.type === 'tileprovider' && ['MapBoxStyle', 'MapBox'].includes(layer.provider)) {
+        // include an empty string if missing to avoid errors in the layer url template
+        layer.accessToken = defaultConfig.mapboxAccessToken;
     }
     return layer;
 };
