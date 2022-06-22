82493dfa2743f60ae593c98c7f43b1cb9896f571
weberjavi <weberjavi@gmail.com>
2022-06-22 10:56:45 +0200

    Allow import/export on cesium viewer

--- web/client/plugins/MapImport.jsx
+++ web/client/plugins/MapImport.jsx
@@ -83,7 +83,6 @@ export default {
             resolve(ImportPlugin);
         });
     }, enabler: (state) => state.mapimport && state.mapimport.enabled || state.toolbar && state.toolbar.active === 'import'}, {
-        disablePluginIf: "{state('mapType') === 'cesium'}",
         BurgerMenu: {
             name: 'import',
             position: 4,
