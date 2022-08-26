aa21a1f1a53262855b619d885c9caa8a19ca7267
MV88 <matteo.velludini@gmail.com>
2022-08-26 12:50:45 +0200

    #7934 refactor to version plugin, moving it to About plugin (#8504)
    * #7934 refactor to version plugin, moving it to About plugin
    * update migration guideline
    
    # Conflicts:
    #	web/client/hooks/useModulePlugins.js
    #	web/client/plugins/Version.jsx
    #	web/client/product/plugins.js

--- web/client/product/components/viewer/about/About.jsx
+++ web/client/product/components/viewer/about/About.jsx
@@ -1,5 +1,3 @@
-import PropTypes from 'prop-types';
-
 /**
  * Copyright 2015, GeoSolutions Sas.
  * All rights reserved.
@@ -7,15 +5,18 @@ import PropTypes from 'prop-types';
  * This source code is licensed under the BSD-style license found in the
  * LICENSE file in the root directory of this source tree.
  */
+
 import React from 'react';
+import PropTypes from 'prop-types';
+import { Glyphicon } from 'react-bootstrap';
 
 import InfoButton from '../../../../components/buttons/InfoButton';
 import Dialog from '../../../../components/misc/Dialog';
-import AboutContent from './AboutContent';
 import { Message } from '../../../../components/I18N/I18N';
+import AboutContent from './AboutContent';
 import aboutImg from '../../../assets/img/Blank.gif';
-import assign from 'object-assign';
-import { Glyphicon } from 'react-bootstrap';
+import VersionInfo from './VersionInfo';
+
 
 class About extends React.Component {
     static propTypes = {
@@ -23,6 +24,11 @@ class About extends React.Component {
         modalConfig: PropTypes.object,
         withButton: PropTypes.bool,
         enabled: PropTypes.bool,
+        version: PropTypes.string,
+        githubUrl: PropTypes.string,
+        commit: PropTypes.string,
+        message: PropTypes.string,
+        date: PropTypes.string,
         onClose: PropTypes.func
     };
 
@@ -43,32 +49,59 @@ class About extends React.Component {
     };
 
     render() {
-        return this.props.withButton ? (
-            <InfoButton
-                {...this.props.modalConfig}
-                image={aboutImg}
-                title={<Message msgId="about_title"/>}
-                btnType="image"
-                className="map-logo"
-                body={
-                    <AboutContent/>
-                }/>) : (
-            <Dialog
-                id="mapstore-about"
-                style={assign({}, {zIndex: 1992, display: this.props.enabled ? "block" : "none"})}
-                modal
-                draggable
-            >
-                <span role="header">
-                    <span className="about-panel-title">
-                        <Message msgId="about_title"/>
+        if (this.props.enabled) {
+
+            if (this.props.withButton) {
+                return (
+                    <InfoButton
+                        {...this.props.modalConfig}
+                        image={aboutImg}
+                        title={<Message msgId="about_title"/>}
+                        btnType="image"
+                        className="map-logo"
+                        body={<>
+                            <VersionInfo
+                                version={this.props.version}
+                                message={this.props.message}
+                                commit={this.props.commit}
+                                date={this.props.date}
+                                githubUrl={this.props.githubUrl}
+                            />
+                            <AboutContent/>
+                        </>
+                        }
+                    />
+                );
+            }
+            return (
+                <Dialog
+                    id="mapstore-about"
+                    style={{zIndex: 1992}}
+                    modal
+                    draggable
+                >
+                    <span role="header">
+                        <span className="about-panel-title">
+                            <Message msgId="about_title"/>
+                        </span>
+                        <button onClick={this.props.onClose} className="about-panel-close close">
+                            {this.props.modalConfig.closeGlyph ? <Glyphicon glyph={this.props.modalConfig.closeGlyph}/> : <span>×</span>}
+                        </button>
                     </span>
-                    <button onClick={this.props.onClose} className="about-panel-close close">
-                        {this.props.modalConfig.closeGlyph ? <Glyphicon glyph={this.props.modalConfig.closeGlyph}/> : <span>×</span>}
-                    </button>
-                </span>
-                <div role="body"><AboutContent/></div>
-            </Dialog>);
+                    <div role="body">
+                        <VersionInfo
+                            version={this.props.version}
+                            message={this.props.message}
+                            commit={this.props.commit}
+                            date={this.props.date}
+                            githubUrl={this.props.githubUrl}
+                        />
+                        <AboutContent/>
+                    </div>
+                </Dialog>
+            );
+        }
+        return null;
     }
 }
 
---
+++ web/client/product/components/viewer/about/__tests__/VersionInfo-test.js
@@ -0,0 +1,70 @@
+/**
+ * Copyright 2022, GeoSolutions Sas.
+ * All rights reserved.
+ *
+ * This source code is licensed under the BSD-style license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+import expect from 'expect';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import VersionInfo from '../VersionInfo';
+
+describe("The VersionInfo component", () => {
+    beforeEach((done) => {
+        document.body.innerHTML = '<div id="container"></div>';
+        setTimeout(done);
+    });
+
+    afterEach((done) => {
+        ReactDOM.unmountComponentAtNode(document.getElementById("container"));
+        document.body.innerHTML = '';
+        setTimeout(done);
+    });
+
+    it('is created with defaults', () => {
+        const cmp = ReactDOM.render(<VersionInfo/>, document.getElementById("container"));
+        expect(cmp).toBeTruthy();
+    });
+
+    it('test with version info', () => {
+        const version = '$MapStore2';
+        const message = "#7934 refactor code and resolve eslint error";
+        const commit = "01046133761de880aebce08a7bf11dd858117837";
+        const date = "Thu, 23 Jun 2022 20:00:02 +0300";
+        const githubUrl = "https://github.com/geosolutions-it/MapStore/tree/sha_commit";
+
+        const vd = ReactDOM.render(
+            <VersionInfo
+                version={version}
+                githubUrl={githubUrl}
+                commit={commit}
+                message={message}
+                date={date}
+            />, document.getElementById("container"));
+        expect(vd).toBeTruthy();
+
+        const versionDialogDOM = ReactDOM.findDOMNode(vd);
+        expect(versionDialogDOM).toBeTruthy();
+        const innerModalDom = versionDialogDOM.children[0];
+        expect(innerModalDom).toBeTruthy();
+        const innerModalDomBody = innerModalDom.children[0];
+        expect(innerModalDomBody).toBeTruthy();
+        const infoclass = versionDialogDOM.querySelectorAll('.version-info .info-label');
+        expect(infoclass).toBeTruthy();
+
+        const versionValue = document.querySelector('.v_version');
+        const messageValue = document.querySelector('.v_message');
+        const commitValue = document.querySelector('.v_commit');
+        const dateValue = document.querySelector('.v_date');
+        const githubUrlValue = document.querySelector('.v_githubUrl');
+
+        expect(versionValue.textContent.trim()).toBe(version);
+        expect(messageValue.textContent.trim()).toBe(message);
+        expect(commitValue.textContent.trim()).toBe(commit);
+        expect(commitValue.innerHTML.trim()).toBe('<a href="https://github.com/geosolutions-it/MapStore/tree/sha_commit" target="_blank" class="v_githubUrl">01046133761de880aebce08a7bf11dd858117837</a>');
+        expect(dateValue.textContent.trim()).toBe(date);
+        expect(githubUrlValue.textContent.trim()).toBe(commit);
+    });
+});
