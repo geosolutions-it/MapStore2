3bc41b9e5ea898c429ea0aadac4e4098ebfad3d5
Victor Belomestnov <belom88@yandex.ru>
2022-09-22 14:54:31 +0300

    fix PR comments

--- web/client/components/data/featuregrid/FeatureEditorFallback.jsx
+++ web/client/components/data/featuregrid/FeatureEditorFallback.jsx
@@ -6,47 +6,28 @@
  * LICENSE file in the root directory of this source tree.
  */
 
-import React from 'react';
+import React, { useMemo } from 'react';
 import { connect } from 'react-redux';
 import { createSelector } from 'reselect';
-import ReactDock from 'react-dock';
 import { getDockSize } from '../../../selectors/featuregrid';
 import { mapLayoutValuesSelector } from '../../../selectors/maplayout';
-import BasicSpinner from '../../misc/spinners/BasicSpinner/BasicSpinner';
+import Loader from '../../misc/Loader';
 
-export const getDockProps = (open) => ({
-    dimMode: "none",
-    defaultSize: 0.35,
-    fluid: true,
-    isVisible: open,
-    maxDockSize: 0.7,
-    minDockSize: 0.1,
-    position: "bottom",
-    setDockSize: () => { },
-    zIndex: 1060
-});
+const FeatureEditorFallback = ({ size, dockStyle }) => {
+    const containerStyle = useMemo(() => {
+        return { height: `${size * 100}%`, ...dockStyle };
+    }, [size, dockStyle]);
+    return (
+        <div className="feature-editor-fallback-container" style={containerStyle}>
+            <Loader size={100} style={{ margin: '0 auto' }} />
+        </div>
+    );
+};
 
-const Dock = connect(createSelector(
+export default connect(createSelector(
     getDockSize,
     state => mapLayoutValuesSelector(state, { transform: true }),
     (size, dockStyle) => ({
         size,
         dockStyle
-    })
-)
-)(ReactDock);
-
-const FeatureEditorFallback = (props) => {
-    const dockProps = getDockProps(props.open);
-    return (
-        <Dock {...dockProps} onSizeChange={size => { props.onSizeChange(size, dockProps); }}>
-            <div className="feature-editor-fallback-container">
-                <BasicSpinner sSize="sp-big" />
-            </div>
-        </Dock>
-    );
-};
-
-export default connect(
-    createSelector([state => state?.featuregrid?.open], (open) => ({ open }))
-)(FeatureEditorFallback);
+    })))(FeatureEditorFallback);
