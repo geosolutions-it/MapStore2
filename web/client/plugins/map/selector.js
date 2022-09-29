5b2c2c65af11ebdbeac3d1165d21bb1cf5a97f15
stefano bovio <stefano.bovio@geosolutionsgroup.com>
2022-09-29 15:39:07 +0200

    #8576 No feedback loading while query 3D Tiles layers (Identify) (#8602) (#8632)

--- web/client/plugins/map/selector.js
+++ web/client/plugins/map/selector.js
@@ -1,5 +1,3 @@
-import { createStructuredSelector } from 'reselect';
-
 import { mapSelector, projectionDefsSelector, isMouseMoveCoordinatesActiveSelector } from '../../selectors/map';
 import { mapTypeSelector, isOpenlayers } from '../../selectors/maptype';
 import { layerSelectorWithMarkers } from '../../selectors/layers';
@@ -7,21 +5,38 @@ import { highlighedFeatures } from '../../selectors/highlight';
 import { securityTokenSelector } from '../../selectors/security';
 import { currentLocaleLanguageSelector } from '../../selectors/locale';
 import { isLocalizedLayerStylesEnabledSelector, localizedLayerStylesNameSelector } from '../../selectors/localizedLayerStyles';
+import { createShallowSelectorCreator } from '../../utils/ReselectUtils';
+import isEqual from 'lodash/isEqual';
 
 /**
  * Map state to props selector for Map plugin
  */
-export default createStructuredSelector({
-    projectionDefs: projectionDefsSelector,
-    map: mapSelector,
-    mapType: mapTypeSelector,
-    layers: layerSelectorWithMarkers,
-    features: highlighedFeatures,
-    loadingError: state => state.mapInitialConfig && state.mapInitialConfig.loadingError && state.mapInitialConfig.loadingError.data,
-    securityToken: securityTokenSelector,
-    elevationEnabled: isMouseMoveCoordinatesActiveSelector,
-    shouldLoadFont: isOpenlayers,
-    isLocalizedLayerStylesEnabled: isLocalizedLayerStylesEnabledSelector,
-    localizedLayerStylesName: localizedLayerStylesNameSelector,
-    currentLocaleLanguage: currentLocaleLanguageSelector
-});
+
+export default createShallowSelectorCreator(isEqual)(
+    projectionDefsSelector,
+    mapSelector,
+    mapTypeSelector,
+    layerSelectorWithMarkers,
+    highlighedFeatures,
+    state => state.mapInitialConfig && state.mapInitialConfig.loadingError && state.mapInitialConfig.loadingError.data,
+    securityTokenSelector,
+    isMouseMoveCoordinatesActiveSelector,
+    isOpenlayers,
+    isLocalizedLayerStylesEnabledSelector,
+    localizedLayerStylesNameSelector,
+    currentLocaleLanguageSelector,
+    (projectionDefs, map, mapType, layers, features, loadingError, securityToken, elevationEnabled, shouldLoadFont, isLocalizedLayerStylesEnabled, localizedLayerStylesName, currentLocaleLanguage) => ({
+        projectionDefs,
+        map,
+        mapType,
+        layers,
+        features,
+        loadingError,
+        securityToken,
+        elevationEnabled,
+        shouldLoadFont,
+        isLocalizedLayerStylesEnabled,
+        localizedLayerStylesName,
+        currentLocaleLanguage
+    })
+);
