e10735ad0d6473c845635c2ac685026cca4b79a8
David Quartey <42542676+DavidQuartz@users.noreply.github.com>
2022-10-03 09:37:33 +0000

    #8603 Error downloading filtered dataset, where filter is based on another layer  (#8641)

--- web/client/observables/wps/__tests__/common-test.js
+++ web/client/observables/wps/__tests__/common-test.js
@@ -9,7 +9,8 @@
 import expect from 'expect';
 
 import {
-    getWPSURL
+    getWPSURL,
+    cdata
 } from '../common';
 
 
@@ -40,5 +41,63 @@ describe('common WPS utils', () => {
         expect(/https:\/\/host.?\/service\/wps\?service=WPS&IDENTIFIER=gs%3AAggregate&REQUEST=DescribeProcess&version=1\.0\.0/.test(getWPSURL(url, options))).toEqual(true);
 
     });
-
+    it('test cdata wrapper on xml with no cdata', () => {
+        const filterString = `<wps:ComplexData mimeType="text/xml; subtype=filter/1.1">
+					<ogc:Filter
+						xmlns:ogc="http://www.test.net/ogc"
+						xmlns:gml="http://www.test.net/gml">
+						<ogc:And>
+							<ogc:Intersects>
+								<ogc:PropertyName>the_geom</ogc:PropertyName>
+								<ogc:Function name="collectGeometries">
+									<ogc:Function name="queryCollection">
+										<ogc:Literal>geonode:test_export_filter0</ogc:Literal>
+										<ogc:Literal>the_geom</ogc:Literal>
+									</ogc:Function>
+								</ogc:Function>
+							</ogc:Intersects>
+						</ogc:And>
+					</ogc:Filter>
+				</wps:ComplexData>`;
+        expect(cdata(filterString)).toEqual(`<![CDATA[<wps:ComplexData mimeType="text/xml; subtype=filter/1.1">
+					<ogc:Filter
+						xmlns:ogc="http://www.test.net/ogc"
+						xmlns:gml="http://www.test.net/gml">
+						<ogc:And>
+							<ogc:Intersects>
+								<ogc:PropertyName>the_geom</ogc:PropertyName>
+								<ogc:Function name="collectGeometries">
+									<ogc:Function name="queryCollection">
+										<ogc:Literal>geonode:test_export_filter0</ogc:Literal>
+										<ogc:Literal>the_geom</ogc:Literal>
+									</ogc:Function>
+								</ogc:Function>
+							</ogc:Intersects>
+						</ogc:And>
+					</ogc:Filter>
+				</wps:ComplexData>]]>`);
+    });
+    it('test cdata wrapper on xml with cdata', () => {
+        const filterString = `<wps:ComplexData mimeType="text/xml; subtype=filter/1.1">
+					<ogc:Filter
+						xmlns:ogc="http://www.test.net/ogc"
+						xmlns:gml="http://www.test.net/gml">
+						<ogc:And>
+							<ogc:Intersects>
+								<ogc:PropertyName>the_geom</ogc:PropertyName>
+								<ogc:Function name="collectGeometries">
+									<ogc:Function name="queryCollection">
+										<ogc:Literal>geonode:test_export_filter0</ogc:Literal>
+										<ogc:Literal>the_geom</ogc:Literal>
+                                        <ogc:Literal>
+											<![CDATA[INCLUDE]]>
+										</ogc:Literal>
+									</ogc:Function>
+								</ogc:Function>
+							</ogc:Intersects>
+						</ogc:And>
+					</ogc:Filter>
+				</wps:ComplexData>`;
+        expect(cdata(filterString)).toEqual(filterString);
+    });
 });
