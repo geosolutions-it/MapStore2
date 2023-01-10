fab93a90790f391e01cae26c4173dd36f7bd61e9
Lorenzo Natali <lorenzo.natali@geosolutionsgroup.com>
2023-01-10 17:32:18 +0100

    improved test to remove known servers

--- web/client/components/map/cesium/__tests__/TileProviderLayer-test.jsx
+++ web/client/components/map/cesium/__tests__/TileProviderLayer-test.jsx
@@ -9,7 +9,7 @@ describe('TileProviderLayer', () => {
             { url: 'http://{s}.test.com/{z}/{x}/{y}.png', data: {}, expected: 'http://{s}.test.com/{z}/{x}/{y}.png' },
             { url: 'http://{s}.test.com/{name}/{z}/{x}/{y}.png', data: {name: "test"}, expected: 'http://{s}.test.com/test/{z}/{x}/{y}.png' },
             { url: 'http://{s}.test.com/{name}.{variant}/{style}/{z}/{x}/{y}.png', data: {name: "name", variant: "variant", style: "style"}, expected: 'http://{s}.test.com/name.variant/style/{z}/{x}/{y}.png' },
-            { url: 'https://osm-prod.noncd.db.de:8100/styles/dbs-osm-railway/{z}/{x}/{y}.png?key=asdasdasdasdasdasdasd', data: {}, expected: 'https://osm-prod.noncd.db.de:8100/styles/dbs-osm-railway/{z}/{x}/{y}.png?key=asdasdasdasdasdasdasd'}
+            { url: 'https://very.long.domain.to.test.regex.speed:8100/long/path/{z}/{x}/{y}.png?key=LONG_KEY_TO_USE_TOO', data: {}, expected: 'https://very.long.domain.to.test.regex.speed:8100/long/path/{z}/{x}/{y}.png?key=LONG_KEY_TO_USE_TOO' }
         ];
         samples.forEach((sample) => {
             expect(template(sample.url, sample.data)).toBe(sample.expected);
