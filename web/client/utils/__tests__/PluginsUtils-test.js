83fb7c1fa9b24dab177c00bf48a83ddd875dc2a9
stefano bovio <stefano.bovio@geosolutionsgroup.com>
2023-02-03 17:08:02 +0100

    #8936 Contexts do not load on mobile for DEV,QA (#8949)
    * #8936 Contexts do not load on mobile for DEV,QA
    
    * Update web/client/utils/__tests__/PluginsUtils-test.js
    
    Co-authored-by: Lorenzo Natali <lorenzo.natali@geosolutionsgroup.com>
    
    ---------
    
    Co-authored-by: Lorenzo Natali <lorenzo.natali@geosolutionsgroup.com>

--- web/client/utils/__tests__/PluginsUtils-test.js
+++ web/client/utils/__tests__/PluginsUtils-test.js
@@ -634,4 +634,31 @@ describe('PluginsUtils', () => {
         const items3 = PluginsUtils.getPluginItems(defaultState, plugins, pluginsConfig, "Container3", "Container3", true, []);
         expect(items3.length).toBe(1);
     });
+    it('getPagePluginsConfig', () => {
+        // check default behaviour
+        expect(PluginsUtils.getPagePluginsConfig({
+            pluginsConfig: [{ name: 'Map' }]
+        })).toEqual([{ name: 'Map' }]);
+        // check default mode with pluginsConfig defined for desktop
+        expect(PluginsUtils.getPagePluginsConfig({
+            pluginsConfig: {
+                desktop: [{ name: 'Map' }]
+            }
+        })).toEqual([{ name: 'Map' }]);
+        // check when mode selected is not present, use the default one
+        expect(PluginsUtils.getPagePluginsConfig({
+            pluginsConfig: {
+                desktop: [{ name: 'Map' }]
+            },
+            mode: 'mobile'
+        })).toEqual([{ name: 'Map' }]);
+        // check if mobile mode is present, it is used instead of the default one
+        expect(PluginsUtils.getPagePluginsConfig({
+            pluginsConfig: {
+                desktop: [{ name: 'Map' }, { name: 'TOC' }],
+                mobile: [{ name: 'Map' }]
+            },
+            mode: 'mobile'
+        })).toEqual([{ name: 'Map' }]);
+    });
 });
