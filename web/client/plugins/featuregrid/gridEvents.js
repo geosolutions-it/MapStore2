f7683de9e7d4415fa7d8c66fb3407acda1420f95
mulondo <mulondo26@gmail.com>
2023-02-08 18:12:02 +0300

    revert to code

--- web/client/plugins/featuregrid/gridEvents.js
+++ web/client/plugins/featuregrid/gridEvents.js
@@ -14,19 +14,10 @@ export default {
     onAddFilter: (update = {}) => updateFilter(update),
     onTemporaryChanges: (v) => activateTemporaryChanges(v),
     onGridRowsUpdated: ({fromRow, toRow, updated}, rowGetter) => {
-
-        const updatedValue = {
-            Boolean: {Boolean: null},
-            String: {String: null}
-        };
-
         let features = range(fromRow, toRow).map(r => rowGetter(r)).filter(f =>
             Object.keys(updated || {}).filter(k => f.properties[k] !== updated[k]).length > 0
         );
-        const key = Object.keys(updated)[0];
-        const newValue = updated.String === '' || updated.Boolean === '' ? updatedValue[key] : updated;
-        const checkedFeatures = updated.String === '' || updated.Boolean === '' ? [] : features;
-        return featureModified(checkedFeatures, newValue);
+        return featureModified(features, updated);
     },
     onRowsToggled: (rows, rowGetter) => selectFeatures(rows.map(r => rowGetter(r.rowIdx)), false),
     onRowsSelected: (rows, rowGetter) => selectFeatures(rows.map(r => rowGetter(r.rowIdx)), true),
