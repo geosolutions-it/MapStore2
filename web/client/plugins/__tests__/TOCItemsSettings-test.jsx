4ec37822d1ce9640cc5475dac63102537c7b17ed
Lorenzo Natali <lorenzo.natali@geosolutionsgroup.com>
2023-04-20 18:36:45 +0200

    Fix unit tests

--- web/client/plugins/__tests__/TOCItemsSettings-test.jsx
+++ web/client/plugins/__tests__/TOCItemsSettings-test.jsx
@@ -73,7 +73,7 @@ describe('TOCItemsSettings Plugin', () => {
         ReactDOM.render(<Plugin />, document.getElementById("container"));
         expect(document.querySelector(SETTINGS_SELECTOR)).toExist();
         const tabIndexes = document.querySelectorAll(TAB_INDEX_SELECTOR);
-        expect(tabIndexes.length).toBe(3);
+        expect(tabIndexes.length).toBe(4);
         expect(tabIndexes[0].className).toBe("active"); // general tab active
         expect(document.querySelectorAll(`${TAB_CONTENT_SELECTOR} div.form-group`).length).toBe(4); // check content is general settings tab.
 
