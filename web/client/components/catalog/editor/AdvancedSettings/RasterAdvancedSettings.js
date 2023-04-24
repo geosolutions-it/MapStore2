fe5cda374ccacc8e23ba4780cd386f1610a5ac74
Landry Breuil <breuil@craig.fr>
2023-04-24 12:53:25 +0000

    add a new infoFormat selector in advanced raster settings (#9007)
    fetches capabilities upon open

--- web/client/components/catalog/editor/AdvancedSettings/RasterAdvancedSettings.js
+++ web/client/components/catalog/editor/AdvancedSettings/RasterAdvancedSettings.js
@@ -186,6 +186,30 @@ export default ({
             </Col >
         </FormGroup>
         <FormGroup style={advancedRasterSettingsStyles}>
+            <Col xs={6}>
+                <ControlLabel><Message msgId="infoFormatLbl" /></ControlLabel>
+            </Col >
+            <Col xs={6} style={{marginBottom: '5px', display: 'flex', alignItems: 'center' }}>
+                <div style={{ flex: 1 }}>
+                    <Select
+                        isLoading={props.formatsLoading}
+                        onOpen={() => onFormatOptionsFetch(service.url)}
+                        value={service && service.infoFormat}
+                        clearable
+                        options={props.formatsLoading ? [] : infoFormatOptions.map((format) => ({ value: format, label: format }))}
+                        onChange={event => onChangeServiceProperty("infoFormat", event && event.value)} />
+                </div>
+                <Button
+                    disabled={props.formatsLoading}
+                    tooltipId="catalog.format.refresh"
+                    className="square-button-md no-border"
+                    onClick={() => onFormatOptionsFetch(service.url, true)}
+                    key="format-refresh">
+                    <Glyphicon glyph="refresh" />
+                </Button>
+            </Col >
+        </FormGroup>
+        <FormGroup style={advancedRasterSettingsStyles}>
             <Col xs={6} >
                 <ControlLabel><Message msgId="layerProperties.wmsLayerTileSize" /></ControlLabel>
             </Col >
