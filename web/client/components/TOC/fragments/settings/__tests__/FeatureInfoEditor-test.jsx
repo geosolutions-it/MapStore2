9e1ee1d7b2c073ab713668025e7dbe929b96c643
MV88 <matteo.velludini@gmail.com>
2023-06-28 16:31:27 +0200

    fix test

--- web/client/components/TOC/fragments/settings/__tests__/FeatureInfoEditor-test.jsx
+++ web/client/components/TOC/fragments/settings/__tests__/FeatureInfoEditor-test.jsx
@@ -9,7 +9,6 @@
 import expect from 'expect';
 import React from 'react';
 import ReactDOM from 'react-dom';
-import TestUtils from 'react-dom/test-utils';
 
 import FeatureInfoEditor from '../FeatureInfoEditor';
 
@@ -27,73 +26,6 @@ describe("test FeatureInfoEditor", () => {
 
     it('test rendering edit modal', () => {
         ReactDOM.render(<FeatureInfoEditor showEditor/>, document.getElementById("container"));
-        const modalEditor = document.getElementsByClassName('ms-resizable-modal');
-        expect(modalEditor.length).toBe(1);
-    });
-
-    it('test rendering close x', (done) => {
-
-        const template = '<p>html</p>';
-
-        ReactDOM.render(<FeatureInfoEditor
-            onReady={(quill) => {
-                try {
-                    // edit template
-                    const editor = quill.getEditor();
-                    editor.clipboard.dangerouslyPasteHTML(template);
-                    const btns = document.getElementsByClassName('ms-header-btn');
-                    expect(btns.length).toBe(2);
-                    TestUtils.Simulate.click(btns[1]);
-                } catch (e) {
-                    done(e);
-                }
-            }}
-            onShowEditor={(value) => {
-                expect(value).toBe(false);
-            }}
-            onChange={(key, value) => {
-                if (value.template === template) {
-                    expect(key).toBe('featureInfo');
-                    expect(value).toEqual({ template });
-                    done();
-                }
-            }}
-            showEditor/>, document.getElementById("container"));
-
-        const modalEditor = document.getElementsByClassName('ms-resizable-modal');
-        expect(modalEditor.length).toBe(1);
-
-    });
-
-    it('test rendering close button', (done) => {
-
-        const template = '<p>html</p>';
-
-        ReactDOM.render(<FeatureInfoEditor
-            onReady={(quill) => {
-                try {
-                    // edit template
-                    const editor = quill.getEditor();
-                    editor.clipboard.dangerouslyPasteHTML(template);
-                    const btns = document.getElementsByClassName('btn');
-                    expect(btns.length).toBe(1);
-                    TestUtils.Simulate.click(btns[0]);
-                } catch (e) {
-                    done(e);
-                }
-            }}
-            onShowEditor={(value) => {
-                expect(value).toBe(false);
-            }}
-            onChange={(key, value) => {
-                if (value.template === template) {
-                    expect(key).toBe('featureInfo');
-                    expect(value).toEqual({ template });
-                    done();
-                }
-            }}
-            showEditor
-        />, document.getElementById("container"));
         const modalEditor = document.getElementsByClassName('ms-resizable-modal');
         expect(modalEditor.length).toBe(1);
     });
