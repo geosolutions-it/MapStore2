80b50b630c569a496af4e23f90a84e9c8edf5cb7
Lorenzo Natali <lorenzo.natali@geosolutionsgroup.com>
2023-07-26 17:37:51 +0200

    #9306 Support for CQL_FILTER in LayerDownload plugin

--- web/client/epics/layerdownload.js
+++ web/client/epics/layerdownload.js
@@ -65,7 +65,7 @@ import {
 
 import { getLayerWFSCapabilities, getXMLFeature } from '../observables/wfs';
 import { describeProcess } from '../observables/wps/describe';
-import { download, downloadWithAttributesFilter } from '../observables/wps/download';
+import { download, downloadRaster } from '../observables/wps/download';
 import { referenceOutputExtractor, makeOutputsExtractor, getExecutionStatus  } from '../observables/wps/execute';
 
 import { mergeFiltersToOGC } from '../utils/FilterUtils';
@@ -97,7 +97,10 @@ const DOWNLOAD_FORMATS_LOOKUP = {
 };
 
 const { getFeature: getFilterFeature, query, sortBy, propertyName } = requestBuilder({ wfsVersion: "1.1.0" });
-
+const getCQLFilterFromLayer = (layer = {}) => {
+    const params = layer?.params ?? {};
+    return params?.[Object.keys(params).find((k) => k?.toLowerCase() === "cql_filter")];
+};
 
 const hasOutputFormat = (data) => {
     const operation = get(data, "WFS_Capabilities.OperationsMetadata.Operation");
@@ -108,10 +111,11 @@ const hasOutputFormat = (data) => {
     return toPairs(pickedObj).map(([prop, value]) => ({ name: prop, label: value }));
 };
 
-const getWFSFeature = ({ url, filterObj = {}, layerFilter, downloadOptions = {}, options } = {}) => {
+const getWFSFeature = ({ url, filterObj = {}, layerFilter, layer, downloadOptions = {}, options } = {}) => {
     const { sortOptions, propertyNames } = options;
 
-    const data = mergeFiltersToOGC({ ogcVersion: '1.1.0', addXmlnsToRoot: true, xmlnsToAdd: ['xmlns:ogc="http://www.opengis.net/ogc"', 'xmlns:gml="http://www.opengis.net/gml"'] }, layerFilter, filterObj);
+    const cqlFilter = getCQLFilterFromLayer(layer);
+    const data = mergeFiltersToOGC({ ogcVersion: '1.1.0', addXmlnsToRoot: true, xmlnsToAdd: ['xmlns:ogc="http://www.opengis.net/ogc"', 'xmlns:gml="http://www.opengis.net/gml"'] }, layerFilter, filterObj, cqlFilter);
 
     return getXMLFeature(url, getFilterFeature(query(
         filterObj.featureTypeName, [...(sortOptions ? [sortBy(sortOptions.sortBy, sortOptions.sortOrder)] : []), ...(propertyNames ? [propertyName(propertyNames)] : []), ...(data ? castArray(data) : [])],
@@ -258,6 +262,7 @@ export const startFeatureExportDownload = (action$, store) =>
             url: action.url,
             downloadOptions: action.downloadOptions,
             filterObj: action.filterObj,
+            layer,
             layerFilter,
             options: {
                 pagination: !virtualScroll && get(action, "downloadOptions.singlePage") ? action.filterObj && action.filterObj.pagination : null,
@@ -277,6 +282,7 @@ export const startFeatureExportDownload = (action$, store) =>
                     url: action.url,
                     downloadOptions: action.downloadOptions,
                     filterObj: action.filterObj,
+                    layer,
                     layerFilter,
                     options: {
                         pagination: !virtualScroll && get(action, "downloadOptions.singlePage") ? action.filterObj && action.filterObj.pagination : null,
@@ -311,6 +317,7 @@ export const startFeatureExportDownload = (action$, store) =>
         const wpsFlow = () => {
             const isVectorLayer = !!layer.search?.url;
             const cropToROI = action.downloadOptions.cropDataSet && !!mapBbox && !!mapBbox.bounds;
+            const cqlFilter = getCQLFilterFromLayer(layer);
             const wpsDownloadOptions = {
                 layerName: layer.name,
                 outputFormat: action.downloadOptions.selectedFormat,
@@ -326,7 +333,7 @@ export const startFeatureExportDownload = (action$, store) =>
                             ogcVersion: '1.1.0',
                             addXmlnsToRoot: true,
                             xmlnsToAdd: ['xmlns:ogc="http://www.opengis.net/ogc"', 'xmlns:gml="http://www.opengis.net/gml"']
-                        }, layer.layerFilter, action.filterObj)
+                        }, layer.layerFilter, action.filterObj, cqlFilter)
                     }
                 } : undefined,
                 ROI: cropToROI ? {
@@ -346,7 +353,7 @@ export const startFeatureExportDownload = (action$, store) =>
                     } : {})
                 },
                 notifyDownloadEstimatorSuccess: true,
-                attribute: propertyNames
+                attribute: isVectorLayer && propertyNames ? propertyNames : undefined
             };
             const newResult = {
                 id: uuidv1(),
@@ -358,7 +365,7 @@ export const startFeatureExportDownload = (action$, store) =>
                 outputsExtractor: makeOutputsExtractor(referenceOutputExtractor)
             };
 
-            const executor = isVectorLayer && propertyNames ? downloadWithAttributesFilter : download;
+            const executor = isVectorLayer ? download : downloadRaster;
 
             return executor(action.url, wpsDownloadOptions, wpsExecuteOptions)
                 .takeUntil(action$.ofType(REMOVE_EXPORT_DATA_RESULT).filter(({id}) => id === newResult.id).take(1))
