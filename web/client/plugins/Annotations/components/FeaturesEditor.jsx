b238dd98291963cff36581676ad8eaea7fef864f
MV88 <matteo.velludini@gmail.com>
2023-10-24 17:07:27 +0200

    Fix #9650 forcing geometries tab to be selected when adding a new one

--- web/client/plugins/Annotations/components/FeaturesEditor.jsx
+++ web/client/plugins/Annotations/components/FeaturesEditor.jsx
@@ -167,6 +167,7 @@ export function FeaturesEditor({
     }
 
     function handleAddFeature(type, properties) {
+        onSelectTab('coordinates', selected);
         const id = uuid();
         handleOnChange((prevCollection) => {
             const newFeature = {
