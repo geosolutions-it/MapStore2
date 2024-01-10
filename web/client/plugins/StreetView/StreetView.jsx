2fa6ef2b984c3f22ae63ab4f92783edfad785468
Suren <dsuren1@gmail.com>
2024-01-10 21:52:59 +0530

    #9840: StreetView plugin ability to set parameters for the default size of panel (#9842) (#9863)
    (cherry picked from commit 0be639d77d0fb449f0ce182cd610d64c588e9683)

--- web/client/plugins/StreetView/StreetView.jsx
+++ web/client/plugins/StreetView/StreetView.jsx
@@ -20,14 +20,14 @@ import streetView from './reducers/streetview';
 import * as epics from './epics/streetView';
 import './css/style.css';
 
-const StreetViewPluginComponent = ({onMount, onUnmount, apiKey, useDataLayer, dataLayerConfig, panoramaOptions}) => {
+const StreetViewPluginComponent = ({onMount, onUnmount, apiKey, useDataLayer, dataLayerConfig, panoramaOptions, panelSize}) => {
     useEffect(() => {
         onMount({apiKey, useDataLayer, dataLayerConfig, panoramaOptions});
         return () => {
             onUnmount();
         };
     }, [apiKey, useDataLayer, dataLayerConfig, JSON.stringify(panoramaOptions ?? {})]);
-    return <StreetViewContainer />;
+    return <StreetViewContainer panelSize={panelSize} />;
 };
 
 const StreetViewPluginContainer = connect(() => ({}), {
@@ -49,6 +49,7 @@ const StreetViewPluginContainer = connect(() => ({}), {
  * @property {boolean} [cfg.useDataLayer=true] If true, adds to the map a layer for street view data availability when the plugin is turned on.
  * @property {object} [cfg.dataLayerConfig] configuration for the data layer. By default `{provider: 'custom', type: "tileprovider", url: "https://mts1.googleapis.com/vt?hl=en-US&lyrs=svv|cb_client:apiv3&style=40,18&x={x}&y={y}&z={z}"}`
  * @property {object} [cfg.panoramaOptions] options to configure the panorama. {@link https://developers.google.com/maps/documentation/javascript/reference/street-view#panoramaOptions|Reference for google maps API}
+ * @property {object} [cfg.panelSize] option to configure default street view modal panel size `width` and `height`. Example: `{"width": 500, "height": 500}`.
  * @class
  */
 export default createPlugin(
--- web/client/plugins/StreetView/components/EmptyStreetView.js
+++ web/client/plugins/StreetView/components/EmptyStreetView.js
@@ -3,6 +3,14 @@ import Message from '../../../components/I18N/Message';
 
 import EmptyView from '../../../components/misc/EmptyView';
 const EmptyStreetView = () => {
-    return <EmptyView iconFit={false} glyph="road" title={<Message msgId="streetView.emptyTitle"/>} description={<Message msgId="streetView.emptyDescription"/>} />;
+    return (
+        <EmptyView
+            style={{display: "flex", justifyContent: "center"}}
+            iconFit={false}
+            glyph="road"
+            title={<Message msgId="streetView.emptyTitle"/>}
+            description={<Message msgId="streetView.emptyDescription"/>}
+        />
+    );
 };
 export default EmptyStreetView;
