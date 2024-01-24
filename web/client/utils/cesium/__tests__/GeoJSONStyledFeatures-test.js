d62d8bcc48cd9f31cb56aac3dccafa4b2d5668e1
allyoucanmap <stefano.bovio@geosolutionsgroup.com>
2024-01-24 18:32:43 +0100

    #9905 Clamp to ground issue found with WFS layer

---
+++ web/client/utils/cesium/__tests__/GeoJSONStyledFeatures-test.js
@@ -0,0 +1,25 @@
+/*
+ * Copyright 2024, GeoSolutions Sas.
+ * All rights reserved.
+ *
+ * This source code is licensed under the BSD-style license found in the
+ * LICENSE file in the root directory of this source tree.
+*/
+import expect from 'expect';
+import GeoJSONStyledFeatures from '../GeoJSONStyledFeatures';
+
+describe('Test GeoJSONStyledFeatures', () => {
+    it('it should exclude invalid features', () => {
+        const styledFeatures = new GeoJSONStyledFeatures({
+            features: [{
+                type: 'Feature',
+                geometry: {
+                    type: 'Polygon',
+                    coordinates: [[[7, 41], [7, 41], [7, 41], [7, 41]]]
+                },
+                properties: {}
+            }]
+        });
+        expect(styledFeatures._features.length).toBe(0);
+    });
+});
