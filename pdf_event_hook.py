9a77a5fca1173caa36660d9ba4891c705200558b
allyoucanmap <stefano.bovio@geosolutionsgroup.com>
2024-02-23 17:30:52 +0100

    move icon to css as background url

--- pdf_event_hook.py
+++ pdf_event_hook.py
@@ -10,14 +10,8 @@ from mkdocs.structure.pages import Page
 def inject_link(html: str, href: str,
                 page: Page, logger: logging) -> str:
     """Adding PDF View button on navigation bar(using material theme)"""
-    def _img_tag(src):
-        _IMG = f'''<img src="{src}" alt="Export PDF" style="position: relative; top: -3px; margin-right: 4px; width: 20px; height: 25px;">'''
-        return BeautifulSoup(_IMG, 'html.parser')
-
-    def _get_relative_pah():
-        # get the relative path from root folder using the pdf href
-        parts = href.split('pdf/')
-        return parts[0]
+    def _icon_tag():
+        return BeautifulSoup('<span class="pdf-icon"></span>', 'html.parser')
 
     logger.info(f'(hook on inject_link: {page.title})')
     soup = BeautifulSoup(html, 'html.parser')
@@ -28,8 +22,7 @@ def inject_link(html: str, href: str,
     if export_pdf:
         a = soup.new_tag('a', href=href, title='PDF', target="_blank", **{'class': 'md-tabs__link'})
         # Append an img tag with a local image source
-        img_src = f'{_get_relative_pah()}theme/img/download-pdf-icon.png'
-        a.append(_img_tag(img_src))
+        a.append(_icon_tag())
         a.append("Export PDF")
         export_pdf.append(a)
         return str(soup)
@@ -39,7 +32,7 @@ def inject_link(html: str, href: str,
                 nav = soup.find('nav', class_='md-header__inner')
           if nav:
                 a = soup.new_tag('a', href=href, title='PDF', target="_blank", **{'class': 'md-header__button md-header-nav__button md-icon'})
-                a.append(_img_tag(img_src))
+                a.append(_icon_tag())
                 a.append("Export PDF")
                 nav.append(a)
                 return str(soup)
