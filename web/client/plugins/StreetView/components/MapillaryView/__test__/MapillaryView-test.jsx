d032ce5bf26ef4b9d854d821abf856be4da88a0b
mahmoudadel54 <mahmoodmadel@gmail.com>
2024-03-07 15:05:31 +0200

    #9978: Add 360 photo support with Mapillary Description: - edit in mapillary unit tests

--- web/client/plugins/StreetView/components/MapillaryView/__test__/MapillaryView-test.jsx
+++ web/client/plugins/StreetView/components/MapillaryView/__test__/MapillaryView-test.jsx
@@ -11,13 +11,10 @@ import expect from 'expect';
 
 import ReactDOM from 'react-dom';
 import MapillaryView from '../MapillaryView';
-import axios from 'axios';
-import MockAdapter from 'axios-mock-adapter';
 import { waitFor } from '@testing-library/react';
 
 describe('MapillaryView', () => {
     const testAPIKey = 'test';
-    let mockAxios;
     const mockProviderSettings = {
         ApiURL: "base/web/client/test-resources/mapillary/output/run_04/index.json"
     };
@@ -31,14 +28,11 @@ describe('MapillaryView', () => {
         }
     };
     beforeEach((done) => {
-        mockAxios = new MockAdapter(axios);
         document.body.innerHTML = '<div id="container"></div>';
         setTimeout(done);
     });
 
     afterEach((done) => {
-        mockAxios.restore();
-
         ReactDOM.unmountComponentAtNode(document.getElementById("container"));
         document.body.innerHTML = '';
         setTimeout(done);
@@ -59,58 +53,19 @@ describe('MapillaryView', () => {
         expect(viewer).toExist();
         expect(viewer.style.position).toExist();
     });
-    it('Test MapillaryView with location value', (done) => {
-        mockAxios = new MockAdapter(axios);
-        mockAxios.onGet().reply(200, {
-            "type": "FeatureCollection",
-            "features": [
-                {
-                    "type": "Feature",
-                    "geometry": {
-                        "type": "Point",
-                        "coordinates": [
-                            9.0327347,
-                            44.3834013,
-                            52.621
-                        ]
-                    },
-                    "properties": {
-                        "filename": "01916-1665045163-2022-10-06-08-32-43-889",
-                        "md5sum": "b44289b035343cc4eebd23c57ec36309",
-                        "filetype": "image",
-                        "MAPLatitude": 44.3834013,
-                        "MAPLongitude": 9.0327347,
-                        "MAPCaptureTime": "2022_10_06_10_32_43_000",
-                        "MAPAltitude": 52.621,
-                        "MAPCompassHeading": {
-                            "TrueHeading": 1.08,
-                            "MagneticHeading": 1.08
-                        },
-                        "MAPSequenceUUID": "0",
-                        "MAPDeviceMake": "NCTECH LTD",
-                        "MAPDeviceModel": "iSTAR Pulsar",
-                        "MAPOrientation": 1,
-                        "width": 11000,
-                        "height": 5500,
-                        "extension": ".jpg"
-                    }
-                }
-            ]
-        });
-        ReactDOM.render(<MapillaryView {...props} location={{properties: {imageId: "TEST_ID"}}} providerSettings={mockProviderSettings}/>, document.getElementById("container"));
+    it('Test displaying Mapillary viewer by associating location value', (done) => {
+        ReactDOM.render(<MapillaryView {...props} location={{properties: {imageId: "image01"}}} providerSettings={mockProviderSettings}/>, document.getElementById("container"));
         const div = emptyStreetView();
         expect(div).toExist();
         expect(div.style.display).toEqual('block');
         const viewer = mapillaryStreetView();
         expect(viewer).toExist();
         expect(viewer.style.position).toExist();
-        waitFor(()=>expect(document.querySelector('.mapillary-dom')).toExist(), {
-            timeout: 5000
-        })
+        waitFor(()=>expect(document.querySelector('.mapillary-dom')).toBeTruthy())
             .then(()=>{
                 let mapillaryDomElem = document.querySelector('.mapillary-dom');
                 expect(mapillaryDomElem).toExist();
-            }).catch(done)
-            .finally(done);
+                done();
+            }).catch(done);
     });
 });
