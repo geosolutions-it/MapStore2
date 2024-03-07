c097c664b136e2fc388657e6c8079a4d11ac42e8
David Göckede <david-alexander.goeckede-extern@deutschebahn.com>
2024-03-07 10:40:13 +0100

    Make Validator a wrapper for predicate
    In reality all a validator does is expose two functions that filter
    based on a format-dependent predicate/criterium.
    
    The only exception being String format, where both valid and invalid
    responses require need to be of type string, but this is likely a
    mistake.
    
    Instead of constructing them one by one we extract the criteria into the
    Predicate object and make the Validator a wrapper containing the
    previous array-based functions as well as the unterlying predicate
    itself.
    
    This way, the criterium is very clear and it is guaranteed that any
    response that is not valid is indeed invalid.
    
    It also makes it easier to expose the predicate for direct use so that
    one does not need to cast single responses to arrays and then check
    whether the remain in the filtered array.

--- web/client/utils/__tests__/FeatureInfoUtils-test.js
+++ web/client/utils/__tests__/FeatureInfoUtils-test.js
@@ -1,5 +1,5 @@
 import expect from 'expect';
-import {Parser, Validator} from '../FeatureInfoUtils';
+import {Parser, validator} from '../FeatureInfoUtils';
 
 describe('FeatureInfoUtils', () => {
     // **********************************
@@ -36,41 +36,28 @@ describe('FeatureInfoUtils', () => {
 
     });
     it('HTML Validator', () => {
-        // Default fetch all values
-        let results = Validator.HTML.getValidResponses([{response: emptyHTML}, {response: rowHTML}]);
-        expect(results.length).toBe(1);
+        const responseValidator = validator("HTML");
+        let valid;
 
-        let notValidResults = Validator.HTML.getNoValidResponses([{response: emptyHTML}, {response: rowHTML}]);
-        expect(notValidResults.length).toBe(1);
-        expect(notValidResults[0].response).toBe(emptyHTML);
+        // Default fetch all values
+        expect(responseValidator.isValidResponse({response: rowHTML})).toBe(true);
+        expect(responseValidator.isValidResponse({response: emptyHTML})).toBe(false);
 
         // test regex
         let validRegex = "<div[^>]*>[\\s\\S]*<\\/div>";
         let invalidRegex = "<table[^>]*>[\\s\\S]*<\\/table>";
 
-        let validRegexResults = Validator.HTML.getValidResponses([{response: emptyHTML}, {response: rowHTML, layerMetadata: {regex: validRegex }}]);
-        expect(validRegexResults.length).toBe(1);
-        expect(validRegexResults[0].response).toBe(rowHTML);
+        valid = responseValidator.isValidResponse({response: rowHTML, layerMetadata: {regex: validRegex }});
+        expect(valid).toBe(true);
 
-        validRegexResults = Validator.HTML.getValidResponses([{response: emptyHTML}, {response: rowHTML, layerMetadata: {regex: validRegex }}]);
-        expect(validRegexResults.length).toBe(1);
-
-        let invalidRegexResults = Validator.HTML.getValidResponses([{response: emptyHTML}, {response: rowHTML, layerMetadata: {regex: invalidRegex }}]);
-        expect(invalidRegexResults.length).toBe(0);
-
-        validRegexResults = Validator.HTML.getNoValidResponses([{response: emptyHTML}, {response: rowHTML, layerMetadata: {regex: validRegex }}]);
-        expect(validRegexResults.length).toBe(1); // only the empty is not valid
-
-        invalidRegexResults = Validator.HTML.getNoValidResponses([{response: emptyHTML}, {response: rowHTML, layerMetadata: {regex: invalidRegex }}]);
-        expect(invalidRegexResults.length).toBe(2); // both are not valid
-
-
+        valid = responseValidator.isValidResponse({response: rowHTML, layerMetadata: {regex: invalidRegex }});
+        expect(valid).toBe(false);
     });
 
     it('HTML valid html contained in xml',  () => {
         const htmlInXML =  '<?xml version="1.0" encoding="ISO-8859-1"?>' + rowHTML;
-        const validResults = Validator.HTML.getValidResponses([{response: htmlInXML}]);
-        expect(validResults.length).toBe(1);
+        const valid = validator("HTML").isValidResponse({response: htmlInXML});
+        expect(valid).toBe(true);
     });
 
     it("HTML should return invalid if html is not valid", () => {
@@ -91,32 +78,34 @@ describe('FeatureInfoUtils', () => {
             layers (AD.ADDRESSA) No soportada.]]>
            </ServiceException>
         </ServiceExceptionReport>`;
-        const validResults = Validator.HTML.getNoValidResponses([{response: inValidXML}]);
-        expect(validResults.length).toBe(1);
+        const valid = validator("HTML").isValidResponse({response: inValidXML});
+        expect(valid).toBe(false);
     });
 
     // **********************************
     // TEXT
     // **********************************
     const baseTextGFI = 'GetFeatureInfo results:\n';
-    const notValid = '';
     const validTEXT = baseTextGFI
         + '\n'
         + "Layer 'LimiteRegionale''\n"
         + "Feature 0:'\n"
         + "uuid = 'fc1132ee-cf89-4fb0-a25d-315bb3c34568''\n";
+    const notValid = '';
+    const noFeaturesFoundText = 'no features were found';
 
     it('TEXT Validator', () => {
-        let results = Validator.TEXT.getValidResponses([{response: notValid}, {response: validTEXT}]);
-        expect(results.length).toBe(1);
-        expect(results[0].response).toBe(validTEXT);
+        const responseValidator = validator("TEXT");
+        let valid;
 
-        results = Validator.TEXT.getValidResponses([{response: "no features were found"}, {response: validTEXT}]);
-        expect(results.length).toBe(1);
+        valid = responseValidator.isValidResponse({response: validTEXT});
+        expect(valid).toBe(true);
 
-        let notValidResults = Validator.TEXT.getNoValidResponses([{response: notValid}, {response: validTEXT}]);
-        expect(notValidResults.length).toBe(1);
-        expect(notValidResults[0].response).toBe(notValid);
+        valid = responseValidator.isValidResponse({response: notValid});
+        expect(valid).toBe(false);
+
+        valid = responseValidator.isValidResponse({response: noFeaturesFoundText});
+        expect(valid).toBe(false);
     });
 
     // **********************************
@@ -125,13 +114,14 @@ describe('FeatureInfoUtils', () => {
     const validJSON = {"type": "FeatureCollection", "totalFeatures": "unknown", "features": [{"type": "Feature", "id": "", "geometry": null, "properties": {"precip30min": 816}}], "crs": null};
     const emptyJSON = {"type": "FeatureCollection", "totalFeatures": "unknown", "features": [], "crs": null};
     it('PROPERTIES Validator', () => {
-        let results = Validator.JSON.getValidResponses([{response: validJSON}, {response: emptyJSON}]);
-        expect(results.length).toBe(1);
-        expect(results[0].response).toBe(validJSON);
+        const responseValidator = validator("JSON");
+        let valid;
 
-        let notValidResults = Validator.JSON.getNoValidResponses([{response: validJSON}, {response: emptyJSON}]);
-        expect(notValidResults.length).toBe(1);
-        expect(notValidResults[0].response).toBe(emptyJSON);
+        valid = responseValidator.isValidResponse({response: validJSON});
+        expect(valid).toBe(true);
+
+        valid = responseValidator.isValidResponse({response: emptyJSON});
+        expect(valid).toBe(false);
     });
 
 });
