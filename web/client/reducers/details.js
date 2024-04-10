238707c6f28ba5fb5633ab82e963def76f43c234
Matteo V <matteo.velludini@geosolutionsgroup.com>
2024-04-10 10:47:23 +0200

    Fix #10153 fix wrong reset of details (#10182)
    * Fix #10153 fix wrong reset of details
    
    * Update web/client/reducers/config.js
    
    * Update web/client/reducers/config.js
    
    * Update web/client/reducers/config.js
    
    ---------
    
    Co-authored-by: Suren <dsuren1@gmail.com>

--- web/client/reducers/details.js
+++ web/client/reducers/details.js
@@ -5,6 +5,7 @@
  * This source code is licensed under the BSD-style license found in the
  * LICENSE file in the root directory of this source tree.
  */
+import { LOCATION_CHANGE } from 'connected-react-router';
 
 import {
     UPDATE_DETAILS
@@ -14,6 +15,9 @@ const details = (state = {}, action) => {
     switch (action.type) {
     case UPDATE_DETAILS: {
         return {...state, detailsText: action.detailsText, id: action.id};
+    }
+    case LOCATION_CHANGE: {
+        return {...state, detailsText: ""};
     }
     default:
         return state;
