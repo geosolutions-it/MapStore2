6c207db4af5e5b84398becee7d1cf2b9000a828e
David Göckede <79367114+DavGoeck@users.noreply.github.com>
2024-04-19 14:32:22 +0200

    Refactor and simplify the format validator utility logic (#10039)
    * Extract info format logic into separate function
    
    * Extract validator logic into separate function
    
    * Use simple filter in place of reducer
    
    * Make Validator a wrapper for predicate
    
    ---------
    
    Co-authored-by: David Göckede <david-alexander.goeckede-extern@deutschebahn.com>

--- web/client/utils/__tests__/MapInfoUtils-test.js
+++ web/client/utils/__tests__/MapInfoUtils-test.js
@@ -638,12 +638,12 @@ describe('MapInfoUtils', () => {
 
         let validator = getValidator();
         let noValidResponses = validator.getNoValidResponses(response);
-        expect(noValidResponses.length).toBe(1);
+        expect(noValidResponses.length).toBe(3);
 
         // Validate format 'JSON'
         response.filter(r=> r !== undefined).forEach(res => {res.format = "JSON"; return res;});
         noValidResponses = validator.getNoValidResponses(response);
-        expect(noValidResponses.length).toBe(1);
+        expect(noValidResponses.length).toBe(2);
 
     });
 
