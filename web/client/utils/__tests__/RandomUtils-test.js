422a7b63941f1ef5ebbaf0c53f4f6a47e1634886
Matteo V <matteo.velludini@geosolutionsgroup.com>
2024-04-29 11:26:59 +0200

    Fix #10238 Change Math.random with an alternative solution  (#10245)
    * Fix #10238 Change Math.random with an alternative solution
    
    # Conflicts:
    #	web/client/components/map/cesium/plugins/WMSLayer.js
    #	web/client/components/style/vector/marker/SymbolLayout.jsx
    #	web/client/plugins/TOC/components/Legend.jsx
    #	web/client/utils/cesium/MathUtils.js
    
    * add tests
    
    * other replacement for master branch
    
    * fix require paths
    
    * fix paths
    
    * Fix test
    
    * change max number
    
    * Fix math utils position
    
    * fix path
    
    * fix test
    
    * Changed name into RandomUtils to avoid confusion
    
    * Update web/client/utils/__tests__/RandomUtils-test.js
    
    ---------
    
    Co-authored-by: Lorenzo Natali <lorenzo.natali@geosolutionsgroup.com>

---
+++ web/client/utils/__tests__/RandomUtils-test.js
@@ -0,0 +1,27 @@
+/*
+ * Copyright 2022, GeoSolutions Sas.
+ * All rights reserved.
+ *
+ * This source code is licensed under the BSD-style license found in the
+ * LICENSE file in the root directory of this source tree.
+*/
+import expect from 'expect';
+import {
+    randomFloat,
+    randomInt
+} from '../RandomUtils';
+
+describe('Test RandomUtils', () => {
+    it('randomInt', () => {
+        const result = randomInt();
+        expect(result).toBeGreaterThan(0);
+        expect(result).toBeLessThan(Number.MAX_SAFE_INTEGER);
+        expect(typeof result).toBe("number");
+    });
+    it('randomFloat', () => {
+        const result = randomFloat();
+        expect(result).toBeGreaterThan(0);
+        expect(result).toBeLessThan(1);
+        expect(typeof result).toBe("number");
+    });
+});
