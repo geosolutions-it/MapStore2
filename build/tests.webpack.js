4c99b276ca6a6918a070de1ca7d6ae941ed06cb9
mahmoudadel54 <mahmoodmadel@gmail.com>
2024-05-27 16:43:48 +0300

    fix FE unit test failure  #10281

--- build/tests.webpack.js
+++ build/tests.webpack.js
@@ -1,3 +1,3 @@
-var context = require.context('../web/client/components/catalog/editor/AdvancedSettings/__tests__', true, /(-test\.jsx?)|(-test-chrome\.jsx?)$/);
+var context = require.context('../web', true, /(-test\.jsx?)|(-test-chrome\.jsx?)$/);
 context.keys().forEach(context);
 module.exports = context;
