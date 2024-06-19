f0a5f0c5949a49cb2aceaee5d602d8ba2e158b7e
MV88 <matteo.velludini@gmail.com>
2024-06-19 12:20:48 +0200

    improved logic for misconfigured plugin

--- web/client/epics/geoProcessing.js
+++ web/client/epics/geoProcessing.js
@@ -486,7 +486,7 @@ export const runBufferProcessGPTEpic = (action$, store) => action$
         const counter = getCounter(layers, GPT_BUFFER_GROUP_ID);
         let misconfiguredLayers = [];
         if (!layerUrl) {
-            misconfiguredLayers.push(layer.name + " - " + layer.title);
+            misconfiguredLayers.push(layer.name === layer.title ? layer.name : layer.name + " - " + layer.title);
         }
         if (misconfiguredLayers.length) {
             return Rx.Observable.of(showErrorNotification({
@@ -678,6 +678,11 @@ export const runIntersectProcessGPTEpic = (action$, store) => action$
         const layer = getLayerFromIdSelector(state, layerId);
         const layers = availableLayersSelector(state);
         const layerUrl = wpsUrlSelector(state) || head(castArray(layer.url));
+
+        const intersectionLayerId = intersectionLayerIdSelector(state);
+        const intersectionLayer = getLayerFromIdSelector(state, intersectionLayerId);
+        const intersectionLayerUrl = wpsUrlSelector(state) || head(castArray(intersectionLayer.url));
+
         const sourceFeature = sourceFeatureSelector(state);
         const executeOptions = {};
         const intersectionFeature = intersectionFeatureSelector(state);
@@ -686,7 +691,10 @@ export const runIntersectProcessGPTEpic = (action$, store) => action$
         let intersectionFC$;
         let misconfiguredLayers = [];
         if (!layerUrl) {
-            misconfiguredLayers.push(layer.name + " - " + layer.title);
+            misconfiguredLayers.push(layer.name === layer.title ? layer.name : layer.name + " - " + layer.title);
+        }
+        if (!intersectionLayerUrl) {
+            misconfiguredLayers.push(intersectionLayer.name === intersectionLayer.title ? intersectionLayer.name : intersectionLayer.name + " - " + intersectionLayer.title);
         }
         if (misconfiguredLayers.length) {
             return Rx.Observable.of(showErrorNotification({
@@ -712,9 +720,7 @@ export const runIntersectProcessGPTEpic = (action$, store) => action$
         } else {
             sourceFC$ = Rx.Observable.of(sourceFeature.geometry);
         }
-        const intersectionLayerId = intersectionLayerIdSelector(state);
-        const intersectionLayer = getLayerFromIdSelector(state, intersectionLayerId);
-        const intersectionLayerUrl = wpsUrlSelector(state) || head(castArray(intersectionLayer.url));
+
         if (isEmpty(intersectionFeature)) {
             intersectionFC$ = executeProcess(
                 intersectionLayerUrl,
@@ -862,7 +868,7 @@ export const runIntersectProcessGPTEpic = (action$, store) => action$
                     })
                     .catch((e) => {
                         logError(e);
-                        let misconfiguredLayers = [];
+                        misconfiguredLayers = [];
                         if (!layerUrl) {
                             misconfiguredLayers.push(layer.name + " - " + layer.title);
                         }
