8e2d4433427ce5b7cf7628c8d51f724f29d2bf18
Matteo V. <matteo.velludini@geosolutionsgroup.com>
2024-10-25 09:30:51 +0200

    Fix #10626 adding possibilities for better updating store (#10627)

--- web/client/actions/layers.js
+++ web/client/actions/layers.js
@@ -36,7 +36,19 @@ export const FILTER_LAYERS = 'LAYERS:FILTER_LAYERS';
 export const SHOW_LAYER_METADATA = 'LAYERS:SHOW_LAYER_METADATA';
 export const HIDE_LAYER_METADATA = 'LAYERS:HIDE_LAYER_METADATA';
 export const UPDATE_SETTINGS_PARAMS = 'LAYERS:UPDATE_SETTINGS_PARAMS';
+export const REPLACE_LAYERS = 'LAYERS:REPLACE_LAYERS';
 
+/**
+ * full replacement of layers in layers state
+ * @param {object[]} layers the new layers to replace in the state
+ * @returns
+ */
+export function replaceLayers(layers) {
+    return {
+        type: REPLACE_LAYERS,
+        layers
+    };
+}
 export function showSettings(node, nodeType, options) {
     return {
         type: SHOW_SETTINGS,
