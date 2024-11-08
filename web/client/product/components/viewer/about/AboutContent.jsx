1771a369a6d6f397191a12747eb6866f3c92c4db
mahmoud adel <58145645+mahmoudadel54@users.noreply.github.com>
2024-11-08 17:40:34 +0200

    #10660: Enhance About plugin to show/hide version/content sections (#10661)

--- web/client/product/components/viewer/about/AboutContent.jsx
+++ web/client/product/components/viewer/about/AboutContent.jsx
@@ -14,12 +14,12 @@ import msLogo from '../../../assets/img/mapstore-logo-0.20.png';
 class About extends React.Component {
     render() {
         return (
-            <div style={{
+            <div className="about-content-section" style={{
                 backgroundImage: 'url("' + msLogo + '")',
                 backgroundRepeat: 'no-repeat',
                 backgroundPosition: 'center'
             }}>
-                <h1>MapStore</h1>
+                <h1 style={{marginTop: 0}}>MapStore</h1>
                 <p>
                     <I18N.Message msgId="about_p0-0"/> <a href="http://openlayers.org/">OpenLayers</a> <I18N.Message msgId="about_p0-1"/> <a href="http://leafletjs.com/">Leaflet</a>.
                 </p>
