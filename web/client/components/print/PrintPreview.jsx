c12b5eab1eaf960a1b34b78e0316ce48c3f160f3
MV88 <matteo.velludini@gmail.com>
2024-12-03 10:53:25 +0100

    Fix #10692 print bug when tryng to print

--- web/client/components/print/PrintPreview.jsx
+++ web/client/components/print/PrintPreview.jsx
@@ -11,9 +11,15 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 import { Glyphicon } from 'react-bootstrap';
-import { Document, Page } from 'react-pdf';
+import { pdfjs, Document, Page } from 'react-pdf';
 import Button from '../misc/Button';
+import 'react-pdf/dist/Page/TextLayer.css';
+import 'react-pdf/dist/Page/AnnotationLayer.css';
 
+pdfjs.GlobalWorkerOptions.workerSrc = new URL(
+    'pdfjs-dist/build/pdf.worker.min.js',
+    import.meta.url
+).toString();
 
 class PrintPreview extends React.Component {
     static propTypes = {
