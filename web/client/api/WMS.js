58dd73d1bbf7c45ef46ee5566a991cbf3ee768d3
Suren <dsuren1@gmail.com>
2025-01-10 13:18:03 +0530

    #10236: Fix - Legend filter incompatibility with style editing

--- web/client/api/WMS.js
+++ web/client/api/WMS.js
@@ -321,30 +321,14 @@ export const getSupportedFormat = (url, includeGFIFormats = false) => {
         .catch(() => includeGFIFormats ? { imageFormats: [], infoFormats: [] } : []);
 };
 
-let layerLegendJsonData = {};
 export const getJsonWMSLegend = (url) => {
-    let request;
-
-    // enables caching of the JSON legend for a specified duration,
-    // while providing the possibility of re-fetching the legend data in case of external modifications
-    const cached = layerLegendJsonData[url];
-    if (cached && new Date().getTime() < cached.timestamp + (ConfigUtils.getConfigProp('cacheExpire') || 60) * 1000) {
-        request = () => Promise.resolve(cached.data);
-    } else {
-        request = () => axios.get(url).then((response) => {
+    return axios.get(url)
+        .then((response) => {
             if (typeof response?.data === 'string' && response.data.includes("Exception")) {
                 throw new Error("Faild to get json legend");
             }
-            layerLegendJsonData[url] = {
-                timestamp: new Date().getTime(),
-                data: response?.data?.Legend
-            };
             return response?.data?.Legend || [];
-        });
-    }
-    return request().then((data) => data).catch(err => {
-        throw err;
-    });
+        }).catch(err => { throw err; });
 };
 
 const Api = {
