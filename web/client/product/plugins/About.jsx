b7d934389b9d0979f573c8e34319d47ae1b2cd94
Anup Dahal <adahal39@gmail.com>
2025-03-06 20:48:33 +0545

    change in component name : AboutNavBarButton

--- web/client/product/plugins/About.jsx
+++ web/client/product/plugins/About.jsx
@@ -35,7 +35,7 @@ const About = connect((state) => ({
     onClose: toggleControl.bind(null, 'about', null)
 })(AboutComp);
 
-const AboutFooterButton = connect(() => ({}), { onClick: toggleControl.bind(null, 'about', null) })(({ component, onClick }) => {
+const AboutNavBarButton = connect(() => ({}), { onClick: toggleControl.bind(null, 'about', null) })(({ component, onClick }) => {
     const Component = component;
     return (
         <Component
@@ -92,7 +92,7 @@ export default {
                 doNotHide: true,
                 priority: 3,
                 position: 0,
-                Component: AboutFooterButton
+                Component: AboutNavBarButton
             }
         }),
     reducers: {
