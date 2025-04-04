c16b05dbd4a2d6438a1d9c137211782c3ce5a61d
mahmoud adel <58145645+mahmoudadel54@users.noreply.github.com>
2025-04-04 16:28:59 +0200

    #10983: handle add new X twitter icon instead of old icon in Share-->Social (#10988)
    Description:
    - add new x icon of x.com [twitter] in share panel

--- web/client/components/share/ShareSocials.jsx
+++ web/client/components/share/ShareSocials.jsx
@@ -32,7 +32,14 @@ const {
 
 // icons of the social network
 const FacebookIcon = generateShareIcon('facebook');
-const TwitterIcon = generateShareIcon('twitter');
+const TwitterXIcon = ({ color = "#000000", size = 32, round = true }) => (
+    <div style={{width: size, height: size}}>
+        <svg viewBox="0 0 64 64" fill="white" width={size} height={size} className="social-icon social-icon--twitter">
+            {round && <circle cx={size} cy={size} r="31" fill={color} />}
+            {/* X icon svg path */}
+            <path d="M 41.116 18.375 h 4.962 l -10.8405 12.39 l 12.753 16.86 H 38.005 l -7.821 -10.2255 L 21.235 47.625 H 16.27 l 11.595 -13.2525 L 15.631 18.375 H 25.87 l 7.0695 9.3465 z m -1.7415 26.28 h 2.7495 L 24.376 21.189 H 21.4255 z" fill={'white'} />
+        </svg>
+    </div>);
 const LinkedinIcon = generateShareIcon('linkedin');
 
 class ShareSocials extends React.Component {
@@ -84,7 +91,7 @@ class ShareSocials extends React.Component {
                             url={this.props.shareUrl}
                             title={title}
                             className="share-twitter">
-                            <TwitterIcon
+                            <TwitterXIcon
                                 size={32}
                                 round />
                         </TwitterShareButton>
