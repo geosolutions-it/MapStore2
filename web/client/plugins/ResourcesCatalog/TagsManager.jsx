13808029a3e0de686a00c961c138dced315d06cb
rowheat02 <rohit.jack02@gmail.com>
2025-06-30 19:42:40 +0545

    fix: requested changes

--- web/client/plugins/ResourcesCatalog/TagsManager.jsx
+++ web/client/plugins/ResourcesCatalog/TagsManager.jsx
@@ -5,34 +5,10 @@
  * This source code is licensed under the BSD-style license found in the
  * LICENSE file in the root directory of this source tree.
  */
-import React from 'react';
+
 import { createPlugin } from "../../utils/PluginsUtils";
 
 import TagsManager from './containers/TagsManager';
-import PropTypes from 'prop-types';
-
-
-function TagsManagerMenuItem({
-    itemComponent
-}, context) {
-    const ItemComponent = itemComponent;
-    if (ItemComponent) {
-        return (
-            <ItemComponent
-                onClick={() => {
-                    context.router.history.push("/manager/tagsmanager");
-                }}
-                msgId="resourcesCatalog.manageTags"
-                glyph="tags"
-            />
-        );
-    }
-    return null;
-}
-
-TagsManagerMenuItem.contextTypes = {
-    router: PropTypes.object
-};
 
 
 /**
@@ -44,16 +20,6 @@ TagsManagerMenuItem.contextTypes = {
 export default createPlugin('TagsManager', {
     component: () => null,
     containers: {
-        ManagerMenu: {
-            Component: TagsManagerMenuItem,
-            position: 5
-        },
-        Login: {
-            target: 'manager-menu',
-            Component: TagsManagerMenuItem,
-            position: 5,
-            glyph: 'tags'
-        },
         Manager: {
             name: 'tagsmanager',
             position: 3,
