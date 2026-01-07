fca3ec141da18ff7d42c377420f197983384a20b
Suren <dsuren1@gmail.com>
2026-01-07 12:36:06 +0530

    #11730 - Enable FeatureEditor grid in advanced Map editor

--- web/client/plugins/mapEditor/DefaultConfiguration.js
+++ web/client/plugins/mapEditor/DefaultConfiguration.js
@@ -169,6 +169,7 @@ export default {
                 "containerClassName": "map-editor-search-config"
             }
         },
-        "FeedbackMask"
+        "FeedbackMask",
+        "FeatureEditor"
     ]
 };
