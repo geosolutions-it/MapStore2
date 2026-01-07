5dd0b1ecaecb1f6bd383088e9bf63927a2f52ccd
stefanocudini <stefano.cudini@gmail.com>
2026-01-07 12:27:06 +0100

    update flatgeobuf 4.4.0 using readMetadata

--- web/client/api/FlatGeobuf.js
+++ web/client/api/FlatGeobuf.js
@@ -7,8 +7,6 @@
  */
 import axios from '../libs/ajax';
 import isEmpty from 'lodash/isEmpty';
-import { extend, createEmpty } from 'ol/extent';
-import { GeoJSON } from 'ol/format';
 
 /**
  * constants of FlatGeobuf format
@@ -18,6 +16,7 @@ export const FGB_LAYER_TYPE = 'flatgeobuf';
 export const FGB_VERSION = '3.0.1'; // supported format version
 
 export const getFlatGeobufGeojson = () => import('flatgeobuf/lib/mjs/geojson').then(mod => mod);
+export const getFlatGeobufGeneric = () => import('flatgeobuf/lib/mjs/generic').then(mod => mod);  // implement readMetadata()
 export const getFlatGeobufOl = () => import('flatgeobuf/lib/mjs/ol').then(mod => mod);
 
 function getFormat(url) {
@@ -28,7 +27,9 @@ function getFormat(url) {
 
 function getTitleFromUrl(url) {
     const parts = url.split('/');
-    return parts[parts.length - 2];
+    const filename = parts[parts.length - 1];
+    const nameNoExt = filename.split('.').slice(0, -1).join('.');
+    return nameNoExt || filename;
 }
 
 function extractCapabilities({url}) {
@@ -45,56 +46,25 @@ function extractCapabilities({url}) {
 // copy and paste in catalog for testing: https://flatgeobuf.org/test/data/countries.fgb
 //
 export const getCapabilities = (url) => {
-    return getFlatGeobufGeojson().then(async flatgeobuf => { // load lib dynamically
+    return getFlatGeobufGeneric().then(async flatgeobuf => { // load lib dynamically
         return axios.get(url, {
-            adapter: config => {
-                return fetch(config.url); // use fetch adapter to get a stream
+            adapter: async config => {
+                // fetch(config.url); // use fetch adapter to get a stream
+                return await flatgeobuf.readMetadata(config.url); // readMetadata accept also request headers as param
             }
-        }).then(async({body}) => {
+        }).then(async(metadata) => {
 
-            const features = [];
-            let metadata = {};
-            let rect; // if undefined read all features
-
-            // ///// TODO  replace .deserialize() with new method .readMetadata()
-            // when available in flatgeobuf lib > v4.4.5
-            // just merged here: https://github.com/flatgeobuf/flatgeobuf/commit/15f137cdf30495dd84afda6159e537df39d5309c
-
-            /**
-             * flatgeobuf.deserialize() return a AsyncGenerator
-             */
-            for await (let feature of flatgeobuf.deserialize(
-                body,
-                rect,
-                function HeaderMetaFn(meta) {
-                    // sample of metadata structure /web/client/test-resources/flatgeobuf/UScounties_subset.metadata.json
-                    const crs = `${meta?.crs?.org}:${meta?.crs?.code}`;
-                    const title = !isEmpty(meta?.title) ? meta.title : getTitleFromUrl(url);
-                    metadata = {
-                        ...metadata,
-                        title,
-                        crs
-                    };
-                })
-            ) {
-                features.push(new GeoJSON().readFeature(feature));
-            }
-
-            // TODO simplify using new method readMetadata(url) when available in flatgeobuf lib > v4.4.5
-            // that return envelope
-
-            const totExtent = features.reduce((extent, feature) => {
-                return extend(extent, feature.getGeometry().getExtent());
-            }, createEmpty());
+            // const metadata = await flatgeobuf.readMetadata(url);
+            metadata.title = !isEmpty(metadata?.title) ? metadata.title : getTitleFromUrl(url);
 
             const bbox = {
                 bounds: {
-                    minx: totExtent[0],
-                    miny: totExtent[1],
-                    maxx: totExtent[2],
-                    maxy: totExtent[3]
+                    minx: metadata.envelope[0],
+                    miny: metadata.envelope[1],
+                    maxx: metadata.envelope[2],
+                    maxy: metadata.envelope[3]
                 },
-                crs: metadata.crs || 'EPSG:4326'
+                crs: metadata.crs ? `${metadata.crs.org}:${metadata.crs.code}` : 'EPSG:4326'
             };
 
             const capabilities = extractCapabilities({flatgeobuf, url});
