5196b21015b1e7a225fe2d225d85d2b9f1e18b5c
rowheat02 <rohit.jack02@gmail.com>
2026-01-23 21:09:48 +0545

    improve force plug mesassge

--- web/client/components/widgets/builder/wizard/common/interactions/InteractionsRow.jsx
+++ web/client/components/widgets/builder/wizard/common/interactions/InteractionsRow.jsx
@@ -138,7 +138,7 @@ const InteractionsRow = ({item, target, interactions, sourceWidgetId, interactio
                     />
                 )}
             </FlexBox>
-            <InteractionConfiguration item={item} show={showConfiguration} configuration={configuration} setConfiguration={handleConfigurationChange} setPlugged={handlePlugToggle} />
+            <InteractionConfiguration item={item} show={showConfiguration} configuration={configuration} setConfiguration={handleConfigurationChange} setPlugged={handlePlugToggle} target={target} />
             {hasChildren && expanded && (
                 <FlexBox component="ul" column gap="xs">
                     {item.children?.map((child, idx) => (
