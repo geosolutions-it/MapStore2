8473d445bc8fd5101b95caf906eebd2e4a5e7264
Lorenzo Natali <lorenzo.natali@geosolutionsgroup.com>
2026-02-11 11:10:08 +0100

    Migration to filtrex rules system
    - Support for filtrex syntax for rules
    - removed scalebox template for display property
    - minor improvements and cleanup

--- web/client/plugins/print/Option.jsx
+++ web/client/plugins/print/Option.jsx
@@ -1,6 +1,5 @@
 import React, {useEffect} from "react";
 import PropTypes from "prop-types";
-import {handleExpression} from "../../utils/PluginsUtils";
 import { getMessageById } from '../../utils/LocaleUtils';
 
 import PrintOptionComp from "../../components/print/PrintOption";
@@ -13,7 +12,7 @@ export const Option = (props, context) => {
     useEffect(() => {
         if (additionalProperty) actions?.addParameter(property, get(spec, fullProperty) ?? false);
     }, []);
-    return handleExpression({}, {...props}, "{" + enabled + "}") ? (<PrintOptionComp checked={!!get(spec, fullProperty)}
+    return  enabled ? (<PrintOptionComp checked={!!get(spec, fullProperty)}
         label={getMessageById(context.messages, label)}
         onChange={v => onChangeParameter(fullProperty, v)}/>) : null;
 };
