2e3796eff29a3dad5591fc75933e2ad28885bd9a
Matteo V. <matteo.velludini@geosolutionsgroup.com>
2026-02-13 17:50:47 +0100

    backport c027_2025.01.xx - Fix Rulemanager edit of old rules (#11977)
    * Fix Rulemanager edit of old rules
    
    * fix lint
    
    * fix lint
    
    * lint fix
    
    Signed-off-by: MV88 <matteo.velludini@gmail.com>
    
    * clean up
    
    ---------
    
    Signed-off-by: MV88 <matteo.velludini@gmail.com>

--- web/client/components/manager/rulesmanager/ruleseditor/EditMain.jsx
+++ web/client/components/manager/rulesmanager/ruleseditor/EditMain.jsx
@@ -5,19 +5,34 @@
 * This source code is licensed under the BSD-style license found in the
 * LICENSE file in the root directory of this source tree.
 */
-import React from 'react';
+import React, {useEffect} from 'react';
 
 import { Grid, Row, Col, Glyphicon } from 'react-bootstrap';
+import { connect } from "react-redux";
+import { error } from "../../../../actions/notifications";
+import { storeGSInstancesDDList } from "../../../../actions/rulesmanager";
 import Selectors from './attributeselectors';
 import Message from '../../../I18N/Message';
 import Api from '../../../../api/geoserver/GeoFence';
 
 
-export default ({rule = {}, setOption = () => {}, active = true, gsInstancesList = []}) => {
+const EditMain = ({rule = {}, setOption = () => {}, active = true, gsInstancesList = [], handleStoreGSInstancesDDList, onError}) => {
     const {grant, layer, workspace} = rule;
     const showInfo = grant !== "DENY" && layer && !workspace;
     const isStandAloneGeofence = Api.getRuleServiceType() === 'geofence';
     let rulesSelectors = [];
+    useEffect(() => {
+        if (isStandAloneGeofence) {
+            Api.getGSInstancesForDD().then(response => {
+                handleStoreGSInstancesDDList(response.data);
+            }).catch(() => {
+                onError({
+                    title: "rulesmanager.errorTitle",
+                    message: "rulesmanager.errorLoadingGSInstances"
+                });
+            });
+        }
+    }, []);
     if (isStandAloneGeofence) {
         // adding this condition to only render selectors if:
         //   - An instance is selected AND instances list is loaded (to resolve URL for workspace/layer fetch)
@@ -53,7 +68,7 @@ export default ({rule = {}, setOption = () => {}, active = true, gsInstancesList
             <Selectors.Access key="access" selected={rule.grant} workspace={rule.workspace} setOption={setOption}/>];
     }
     return (
-        <Grid className="ms-rule-editor" fluid style={{width: '100%', display: active ? 'block' : 'none'}}>
+        <Grid className="ms-rule-editor edit-main" fluid style={{width: '100%', display: active ? 'block' : 'none'}}>
             {rulesSelectors}
             {showInfo && (<Row>
                 <Col xs={12}>
@@ -68,3 +83,8 @@ export default ({rule = {}, setOption = () => {}, active = true, gsInstancesList
         </Grid>
     );
 };
+
+export default connect(null, (dispatch) => ({
+    handleStoreGSInstancesDDList: (instances)=> dispatch(storeGSInstancesDDList(instances)),
+    onError: (...args) => dispatch(error(...args))
+}))(EditMain);
