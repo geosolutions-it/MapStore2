4dbcde82a0e9c3b9d92c15a26b36027765fce30d
Suren <dsuren1@gmail.com>
2026-03-02 19:20:01 +0530

    #12026 - Fix scale and zoom handling on projection change

--- web/client/plugins/print/MapPreview.jsx
+++ web/client/plugins/print/MapPreview.jsx
@@ -9,14 +9,11 @@ import { assign } from "lodash";
 export const MapPreview = ({mapSize, layout, layoutName, resolutions, useFixedScales,
     localizedLayerStylesEnv, mapPreviewOptions, mapType,
     map, capabilities, onRefresh, validation = {valid: true}, editScale, printMap, printSpec, ...rest}) => {
-    // scales need to be reviewed
     let printSpecState = printSpec && assign({}, printSpec, printMap || {});
     const selectedPrintProjection = (printSpecState && printSpecState?.params?.projection) || (printSpecState && printSpecState?.projection) || (printMap && printMap.projection) || 'EPSG:3857';
-    // need to be reviewed
-    const scales = editScale ? getScales(
-        selectedPrintProjection,
-        map && map.mapOptions && map.mapOptions.view && map.mapOptions.view.DPI || null
-    ) : capabilities.scales.slice(0).reverse().map((scale) => parseFloat(scale.value)) || [];
+    const printCapabilitiesScales = capabilities.scales.slice(0).reverse().map((scale) => parseFloat(scale.value)) || [];
+    const projectedScales = getScales(selectedPrintProjection, map && map.mapOptions && map.mapOptions.view && map.mapOptions.view.DPI || null);
+    const scales = editScale ? projectedScales : useFixedScales ? printCapabilitiesScales : projectedScales;
     return validation.valid ? (
         <MapPreviewComp
             {...rest}
