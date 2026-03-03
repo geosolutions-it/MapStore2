13d2f57fdd5d4e25095d7c2382fce249603d1b9f
Benjamin Grenard <bgr@arxit.com>
2026-03-03 12:07:26 +0100

    add test for utils

--- web/client/plugins/LayersSelection/utils/_tests_/LayersSelection-test.js
+++ web/client/plugins/LayersSelection/utils/_tests_/LayersSelection-test.js
@@ -1,9 +1,32 @@
-import {makeCrsValid } from '../LayersSelection';
+import { makeCrsValid, arcgisToGeoJSON } from '../LayersSelection';
+import expect from 'expect';
+import { getLayerFromRecord } from '../../../../api/catalog/ArcGIS';
 
 describe('LayersSelection Utils', () => {
 
-    describe('make Crs Valid', () => {
+    describe('Crs is valid should return same Crs', () => {
         // TODO
         // makeCrsValid
+        const mockCRS = 'EPSG:2056';
+        const crs = makeCrsValid(mockCRS);
+        expect(crs).toEqual(mockCRS);
+    });
+
+    describe('CRS number should be convert as valid CRS ', () => {
+        // TODO
+        // makeCrsValid
+        const mockCRS = '2056';
+        const CRSValid = 'EPSG:2056';
+        const crs = makeCrsValid(mockCRS);
+        expect(crs).toEqual(CRSValid);
+    });
+
+    describe('CRS number should not be convert as default if it is valid ', () => {
+        // TODO
+        // makeCrsValid
+        const mockCRS = '2056';
+        const defaultCRS = 'EPSG:4326';
+        const crs = makeCrsValid(mockCRS);
+        expect(crs).toNotEqual(defaultCRS);
     });
 });
