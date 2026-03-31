c5483f999e4357124c507a32c3cc2cc3d6d38fe1
stefanocudini <stefano.cudini@gmail.com>
2026-03-31 14:52:52 +0200

    fix flatgeobuf params

--- web/client/components/map/openlayers/plugins/FlatGeobufLayer.js
+++ web/client/components/map/openlayers/plugins/FlatGeobufLayer.js
@@ -49,8 +49,8 @@ const getFlatGeobufStyle = (layer, options, map) => {
 
 const createLoader = (source, options, strategy) => (extent, resolution, projection) => {
     getFlatGeobufOl().then(flatgeobuf => {
-        const { headers } = getRequestConfigurationByUrl(options.url, options?.security?.sourceId);
-        const secureUrl = updateUrlParams(options.url, options.params);
+        const { headers, params } = getRequestConfigurationByUrl(options.url, options?.security?.sourceId);
+        const secureUrl = updateUrlParams(options.url, { ...options.params, ...params});
         const loader = flatgeobuf.createLoader(source, secureUrl, 'EPSG:4326', strategy, true, headers);
         source.setLoader(loader);
         loader(extent, resolution, projection); // force load at creation(needed for flatgeobuf only)
