bcddcdf88bb114a42bc630313c1d235bdff1daad
Wolfgang Kaltz <wolfgang.kaltz@ruag.ch>
2026-04-27 16:25:56 +0200

    restore copyright notice

--- web/client/api/Nominatim.js
+++ web/client/api/Nominatim.js
@@ -1,4 +1,7 @@
 /**
+ * Copyright 2015-2026, GeoSolutions Sas.
+ * All rights reserved.
+ *
  * This source code is licensed under the BSD-style license found in the
  * LICENSE file in the root directory of this source tree.
  */
