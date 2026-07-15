d79ef37315af7393296962591ad1a12d2d9666d9
Binabh <binabhdevkota@gmail.com>
2026-07-15 19:59:46 +0545

    #12612 Catalog: Clearing level search bar does not refresh the list (#12625)
    * Refactor search reset to refetch results in catalog search
    
    * Improve Search clearing by clearing layer selections on reset
    
    * Clear layer selections when submitting a search
    
    (cherry picked from commit 58c8473bb5425e853d909615093697afdb1a5da3)

--- web/client/components/catalog/datasets/Catalog.jsx
+++ web/client/components/catalog/datasets/Catalog.jsx
@@ -284,6 +284,13 @@ const Catalog = ({
         search({ searchText, filters, sort: newSort, start: searchOptions?.startPosition });
     };
 
+    const onReset = () => {
+        onChangeText('', { skipAutoSearch: true });
+        setFilters({});
+        clearSelection?.();
+        search({ searchText: '', filters: {}, sort });
+    };
+
     const hasActiveFilters = useMemo(() => serviceCapabilities.filterSupport && Object.keys(filters).length > 0, [serviceCapabilities.filterSupport, filters]);
 
     const searchInputNode = (
@@ -295,7 +302,7 @@ const Catalog = ({
             }}
             enableFilters={serviceCapabilities.filterSupport}
             onToggleFilters={() => setShowFilters(!showFilters)}
-            onResetFilters={() => onFilterChange({}, true)}
+            onReset={onReset}
             includeSearchButton={includeSearchButton}
             onShowSecurityModal={onShowSecurityModal}
             onSetProtectedServices={onSetProtectedServices}
