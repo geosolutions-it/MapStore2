43b73628256825aad395533798613d5e1e04dfd2
Binabh <binabhdevkota@gmail.com>
2026-07-17 21:58:29 +0545

    Fixed #12579 - Fixed terrain switch and black on load issues (#12581) (#12683)

--- web/client/components/map/cesium/plugins/TerrainLayer.js
+++ web/client/components/map/cesium/plugins/TerrainLayer.js
@@ -46,7 +46,6 @@ function cesiumIonOptionsMapping(config) {
 }
 
 const createLayer = (config, map) => {
-    map.terrainProvider = undefined;
     let terrainProvider;
     let terrain;
     let url;
@@ -94,12 +93,29 @@ const createLayer = (config, map) => {
         add: () => {
             if (terrainProvider) {
                 terrain = new Cesium.Terrain(terrainProvider);
+                // if the terrain provider fails to load (e.g. unreachable server or CORS error)
+                // fallback to the default ellipsoid terrain to avoid a completely black scene
+                terrain.errorEvent.addEventListener((error) => {
+                    console.error('Error while loading the terrain provider, falling back to the ellipsoid terrain', error);
+                    if (map._msCurrentTerrain === terrain) {
+                        terrain = new Cesium.Terrain(new Cesium.EllipsoidTerrainProvider());
+                        map.scene.setTerrain(terrain);
+                        map._msCurrentTerrain = terrain;
+                    }
+                });
                 map.scene.setTerrain(terrain);
+                map._msCurrentTerrain = terrain;
             }
         },
         remove: () => {
-            terrain = new Cesium.Terrain(new Cesium.EllipsoidTerrainProvider());
-            map.scene.setTerrain(terrain);
+            // reset the scene terrain only if this layer is the one currently applied,
+            // removing a deselected or hidden terrain layer must not override
+            // a terrain that has just been activated (e.g. switching terrain from the background selector)
+            if (terrain && map._msCurrentTerrain === terrain) {
+                terrain = new Cesium.Terrain(new Cesium.EllipsoidTerrainProvider());
+                map.scene.setTerrain(terrain);
+                map._msCurrentTerrain = terrain;
+            }
         }
     };
 };
