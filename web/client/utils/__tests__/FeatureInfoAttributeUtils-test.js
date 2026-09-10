daaf6b4e9b8a3c23355508dd1f8d46b74a937ffa
rowheat02 <rohit.jack02@gmail.com>
2026-09-10 12:55:08 +0545

    Add configurable FeatureInfo for media type aliases

--- web/client/utils/__tests__/FeatureInfoAttributeUtils-test.js
+++ web/client/utils/__tests__/FeatureInfoAttributeUtils-test.js
@@ -4,8 +4,11 @@ import {
     getDisplayTypeFromMediaType,
     resolveAttributeDisplayType
 } from '../FeatureInfoAttributeUtils';
+import ConfigUtils from '../ConfigUtils';
 
 describe('FeatureInfoAttributeUtils', () => {
+    afterEach(() => ConfigUtils.removeConfigProp('featureInfoMediaTypeAliases'));
+
     it('normalizes configured media types and MIME types', () => {
         expect(getDisplayTypeFromMediaType(' VIDEO ')).toBe('video');
         expect(getDisplayTypeFromMediaType('image/jpeg')).toBe('image');
@@ -33,5 +36,37 @@ describe('FeatureInfoAttributeUtils', () => {
             attribute: { displayType: 'media', mediaTypeAttribute: 'mimeType' },
             mediaTypeValue: 'audio/mpeg'
         })).toBe('audio');
+    });
+
+    it('resolves configured media type aliases', () => {
+        ConfigUtils.setConfigProp('featureInfoMediaTypeAliases', {
+            panorama: ['PAN', 'PANO', '360'],
+            image: ['IMG', 'FOTO'],
+            video: ['VID']
+        });
+
+        expect(resolveAttributeDisplayType({
+            value: 'https://example.com/resource.jpg',
+            attribute: { displayType: 'media', mediaTypeAttribute: 'mediaType' },
+            mediaTypeValue: ' pano '
+        })).toBe('panorama');
+        expect(resolveAttributeDisplayType({
+            value: 'https://example.com/resource.jpg',
+            attribute: { displayType: 'media', mediaTypeAttribute: 'mediaType' },
+            mediaTypeValue: 360
+        })).toBe('panorama');
+    });
+
+    it('ignores aliases with unsupported display types', () => {
+        ConfigUtils.setConfigProp('featureInfoMediaTypeAliases', {
+            unsupported: ['PAN'],
+            image: ['IMG']
+        });
+
+        expect(resolveAttributeDisplayType({
+            value: 'https://example.com/resource.jpg',
+            attribute: { displayType: 'media' },
+            mediaTypeValue: 'PAN'
+        })).toBe('image');
     });
 });
